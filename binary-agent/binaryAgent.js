88defee6ff2ee05b1938b21202e0c5257c861d1e
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-07-18 04:15:31 +0530

    Solve binary-agent

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,14 @@
+const convertFromBinaryToString = (charInBinary) => {
+  const charCode = parseInt(charInBinary, 2).toString(10);
+  const letterFromCharCode = String.fromCharCode(charCode);
+  return letterFromCharCode;
+};
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(stringInBinary) {
+  const arrayContainingCharInBinaryFormat = stringInBinary.split(' ');
+  const arrayOfChars = arrayContainingCharInBinaryFormat.map(convertFromBinaryToString);
+  const sentence = arrayOfChars.join('');
+  return sentence;
 }
 
 export {
