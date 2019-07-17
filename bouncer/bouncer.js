a600620371560c00e06e16be87a272df53baa1dc
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-18 04:21:41 +0530

    [WIP]: Completed the bouncer problem.

--- bouncer/bouncer.js
+++ bouncer/bouncer.js
@@ -1,8 +1,15 @@
-
-function bouncer(...args) {
-  return args;
+function bouncer(arrayToBeBounced) {
+  if (!arrayToBeBounced)
+    throw new Error(
+      "The variable cannot be processed as it is: " + arrayToBeBounced
+    );
+  const bouncedArray = arrayToBeBounced.filter( iThArrayValue => !checkIfValueIsFalsy(iThArrayValue))
+  return bouncedArray;
 }
-
-export {
-  bouncer,
-};
+function checkIfValueIsFalsy(value){
+  if(value === '' || value === null || value === undefined || value === 0 ||  value === false || value+"" === "NaN" )
+  return true
+  else
+  return false
+}
+export { bouncer };
