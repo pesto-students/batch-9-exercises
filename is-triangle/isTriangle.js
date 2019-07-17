70d6705f1bc0ee67a0c7f4effd09b171db67793c
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-18 02:06:24 +0530

    not completed is triangle implementation

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,9 @@
 
-function isTriangle(...args) {
-  return args;
+function isTriangle(...sides) {
+  const b1 = sides[0] < (sides[1] + [sides[2]]);
+  const b2 = sides[1] < (sides[0] + [sides[2]]);
+  const b3 = sides[2] < (sides[0] + [sides[1]]);
+  return (b1 && b2 && b3)
 }
 
 export {
