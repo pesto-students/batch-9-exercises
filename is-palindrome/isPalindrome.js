63c7fc0356783ce5328151a54ba3efad280d7ece
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-18 04:55:01 +0530

    [WIP]: Completed with checking palindrome question

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,8 +1,24 @@
+function isPalindrome(randomString) {
+  if (typeof randomString !== "number" && typeof randomString !== "string")
+    {
+      throw new Error(
+      "The value passed (" +
+        randomString +
+        ") is not of valid type, its type is:" +
+        typeof randomString +
+        " but it should be of type either string or number."
+    );
+  }
 
-function isPalindrome(...args) {
-  return args;
+  let regExToRemoveUnwanted = /[\W_]/g;
+  let lowerCasedCleanString = String(randomString)
+    .toLowerCase()
+    .replace(regExToRemoveUnwanted, "");
+  let reversedLowerCasedCleanString = lowerCasedCleanString
+    .split("")
+    .reverse()
+    .join("");
+  return lowerCasedCleanString === reversedLowerCasedCleanString;
 }
 
-export {
-  isPalindrome,
-};
+export { isPalindrome };
