395f6622dd1902da758abe405807e800dba021f4
Kanav Arora <kanav_arora@statusbrew.com>
2019-07-18 04:54:46 +0530

    Solve Large Of Four

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,10 @@
+const largerNumberInArray = (numberArray) => {
+  const sortedNumberArray = numberArray.sort((a, b) => b - a);
+  return sortedNumberArray[0];
+};
 
-function largeOfFour(...args) {
-  return args;
+function largeOfFour(numberArrays) {
+  return numberArrays.map(numberArray => largerNumberInArray(numberArray));
 }
 
 export {
