12aefb088a7f324d03e7e39a889ec2f83fb381ba
Kanav Arora <kanav_arora@statusbrew.com>
2019-07-18 02:05:23 +0530

    WIP Apply Operator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,24 @@
-
-function applyOperator(...args) {
-  return args;
+const sum = numbers => numbers.reduce((a, b) => a + b);
+const subtract = numbers => numbers.reduce((a, b) => a - b);
+const multiply = numbers => numbers.reduce((a, b) => a * b);
+const divide = numbers => numbers.reduce((a, b) => a / b);
+const mod = numbers => numbers.reduce((a, b) => a % b);
+function applyOperator(operator, ...numbers) {
+  let result;
+  if (operator === '+') {
+    result = sum(numbers);
+  } else if (operator === '-') {
+    result = subtract(numbers);
+  } else if (operator === '*') {
+    result = multiply(numbers);
+  } else if (operator === '/') {
+    result = divide(numbers);
+  } else if (operator === '%') {
+    result = mod(numbers);
+  } else {
+    throw new Error('Operator Not Supported');
+  }
+  return result;
 }
 
 export {
