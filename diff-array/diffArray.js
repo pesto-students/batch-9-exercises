8062deb84d11c4ddcffc7581fa6a8a034bec7ddf
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-18 04:34:22 +0530

    [WIP]: Completed with finding the symmetric differnce

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,8 +1,15 @@
+function diffArray(set1Array, set2Array) {
+  const requiredSymmetricSet = [];
 
-function diffArray(...args) {
-  return args;
+  set1Array.forEach(val => {
+    if (set2Array.indexOf(val) < 0) requiredSymmetricSet.push(val);
+  });
+
+  set2Array.forEach(val => {
+    if (set1Array.indexOf(val) < 0) requiredSymmetricSet.push(val);
+  });
+
+  return requiredSymmetricSet;
 }
 
-export {
-  diffArray,
-};
+export { diffArray };
