0c183be29c651a804b81700752aada92cc9d2727
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-18 04:18:42 +0530

    Write binaryAgent.js

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,19 @@
+function binaryToDecimal(number) {
+  let decimal = 0;
+  for (let i = 0; i < number.length; i += 1) {
+    const power = number.length - i - 1;
+    decimal += (number[i] * (2 ** power));
+  }
+  return decimal;
+}
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(string) {
+  const input = string.split(' ');
+  let sentence = '';
+  for (let i = 0; i < input.length; i += 1) {
+    sentence += String.fromCharCode(binaryToDecimal(input[i]));
+  }
+  return sentence;
 }
 
 export {
