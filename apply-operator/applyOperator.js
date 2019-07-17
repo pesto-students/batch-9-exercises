fc3dd308a315cb43736e4ff500c664afe7e7dd33
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-18 00:54:41 +0530

    Solve applyOperator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,43 @@
 
-function applyOperator(...args) {
-  return args;
+function applyOperator(operator, ...operands) {
+  if (operator === undefined) {
+    throw new Error('No operator found!');
+  }
+  if (!operands.length) {
+    return 0;
+  }
+  switch (operator) {
+    case '+': return operands.reduce((acc, val) => {
+      const res = acc + val;
+      return res;
+    }, 0);
+    case '-': return operands.reduce((acc, val) => {
+      const res = acc - val;
+      return res;
+    }, 0);
+    case '*': return operands.reduce((acc, val, index) => {
+      if (index === 0) {
+        return val;
+      }
+      const res = acc * val;
+      return res;
+    }, 0);
+    case '/': return operands.reduce((acc, val, index) => {
+      if (index === 0) {
+        return val;
+      }
+      const res = acc / val;
+      return res;
+    }, 0);
+    case '%': return operands.reduce((acc, val, index) => {
+      if (index === 0) {
+        return val;
+      }
+      const res = acc % val;
+      return res;
+    }, 0);
+    default: return null;
+  }
 }
 
 export {
