8ed1001e7da88e6db41f61cdd7f68ec22bc5aca4
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-18 04:29:58 +0530

    Solve sumAll

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,14 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll(range) {
+  let [lowerBound, upperBound] = range;
+  let sumOfRange = 0;
+  if (lowerBound > upperBound) {
+    [lowerBound, upperBound] = [upperBound, lowerBound];
+  }
+  for (let i = lowerBound; i <= upperBound; i += 1) {
+    sumOfRange += i;
+  }
+  return sumOfRange;
 }
 
 export {
