93c7f29bc2bf85512b19a608d5f2a0d12d62d7a4
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-18 02:09:11 +0530

    Solve counterFactory

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,17 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  // let counter = 0;
+  return {
+    counter: 0,
+    increment() {
+      this.counter += 1;
+      return this.counter;
+    },
+    decrement() {
+      this.counter -= 1;
+      return this.counter;
+    },
+  };
 }
 
 export {
