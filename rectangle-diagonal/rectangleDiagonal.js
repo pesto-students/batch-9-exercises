987e7ac6d6095233f880d36efb7d1dd71479ea50
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-18 01:03:30 +0530

    Completed rectangleDiagonal

--- rectangle-diagonal/rectangleDiagonal.js
+++ rectangle-diagonal/rectangleDiagonal.js
@@ -1,6 +1,8 @@
+const square = x => x ** 2;
 
-function rectangleDiagonal(...args) {
-  return args;
+function rectangleDiagonal(length, width) {
+  const diagonal = Math.sqrt(square(length) + square(width));
+  return diagonal;
 }
 
 export {
