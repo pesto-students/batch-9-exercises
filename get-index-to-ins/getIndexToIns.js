00547049ef24f27d7e077debb6ed366454497e28
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-18 04:57:52 +0530

    Two problems done

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,15 @@
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(inputArray, numberToInsert) {
+  inputArray.sort();
+  let indexToReturn;
+  for (let i = 0; i < inputArray.length; i += 1) {
+    if (numberToInsert < inputArray[i]) {
+      indexToReturn = inputArray.indexOf(inputArray[i]) - 1;
+    } else if (numberToInsert === inputArray[i]) {
+      indexToReturn = inputArray.indexOf(inputArray[i]) - 2;
+    }
+  }
+  return indexToReturn;
 }
 
 export {
--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,8 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(inputString) {
+  const newString = inputString.match(/[a-zA-Z0-9]+/g).join('').toLowerCase();
+  console.log(newString);
+  return newString === newString.split('').reverse().join('');
 }
 
 export {
