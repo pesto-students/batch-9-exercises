e71e427e75eed962faa41afeb128e364361d9872
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-18 18:23:36 +0530

    [WIP] Solve the large of four array problem

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,8 +1,17 @@
-
-function largeOfFour(...args) {
-  return args;
+function largeOfFour(arrayList) {
+  let result = []
+  let listSize = arrayList.length
+  for(let i = 0 ; i < listSize ; i++ ) {
+    result.push(findLargest(arrayList[i]))
+  }
+  return result;
 }
-
-export {
-  largeOfFour,
-};
+function findLargest(array) {
+  let largest = -Infinity;
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] >largest)
+    largest=array[i]
+  }
+  return largest
+}
+export { largeOfFour };
