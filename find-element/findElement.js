613b7c92ccc627a1272e8e24ad7bada4625ff78d
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-19 00:41:32 +0530

    Write findElement.js

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,6 +1,13 @@
 
 function findElement(...args) {
-  return args;
+  const array = args[0];
+  const test = args[1];
+  for (const member of array) {
+    if (test(member)) {
+      return member;
+    }
+  }
+  return undefined;
 }
 
 export {
