8300d24258c57267c6c0aabd9f602eff2c8e6569
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-19 05:15:18 +0530

    solve squareNumbersArray

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,8 +1,11 @@
-
 function squareNumbersArray(...args) {
-  return args;
+  const isNumberArray = [...args].every(Number);
+
+  if (!isNumberArray) {
+    throw new Error("My custom error");
+  }
+
+  return [...args].map(element => element ** 2);
 }
 
-export {
-  squareNumbersArray,
-};
+export { squareNumbersArray };
