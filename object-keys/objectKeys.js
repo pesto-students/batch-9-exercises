bc065b0130ca4482738b8fca5d77b4d7d49ef8cb
Kanav Arora <arorakanav11@gmail.com>
2019-07-19 00:51:31 +0530

    Solve Object Keys

--- object-keys/objectKeys.js
+++ object-keys/objectKeys.js
@@ -1,6 +1,12 @@
 
-function objectKeys(...args) {
-  return args;
+function objectKeys(obj) {
+  let keys = [];
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      keys = [...keys, key];
+    }
+  }
+  return keys;
 }
 
 export {
