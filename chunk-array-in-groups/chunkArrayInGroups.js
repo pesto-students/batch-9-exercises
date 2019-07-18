f3708b604269cf8b662f805a880ef4d5d02a004c
Kanav Arora <arorakanav11@gmail.com>
2019-07-19 02:02:15 +0530

    Solve Chunk Array Groups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,12 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(array, chunkSize) {
+  const groupArray = [];
+  let operatingIndex = 0;
+  while (operatingIndex < array.length) {
+    groupArray.push(array.slice(operatingIndex, operatingIndex + chunkSize));
+    operatingIndex += chunkSize;
+  }
+  return groupArray;
 }
 
 export {
