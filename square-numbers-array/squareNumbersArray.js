6eebce92aac477ecface9691fd32b5f15449f7b7
Kanav Arora <arorakanav11@gmail.com>
2019-07-19 05:10:05 +0530

    WIP Square Numbers Array

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,12 @@
 
-function squareNumbersArray(...args) {
-  return args;
+
+const isNotNumberArray = array => array.some(Number.isNaN);
+const square = number => number ** 2;
+function squareNumbersArray(...array) {
+  if (isNotNumberArray(array)) {
+    throw Error('My custom error');
+  }
+  return array.map(square);
 }
 
 export {
