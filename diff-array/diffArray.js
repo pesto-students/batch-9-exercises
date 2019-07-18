294eda8e3ed15a0dd88f245863388ac377931c44
jayamathan <jayamathanskj03@gmail.com>
2019-07-18 20:35:26 +0530

    solve Diff Array

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,8 @@
-
-function diffArray(...args) {
-  return args;
+const unique = (array1, array2) => array1.filter(item => !array2.includes(item));
+function diffArray(array1, array2) {
+  const uniqueArray1 = unique(array1, array2);
+  const uniqueArray2 = unique(array2, array1);
+  return [...uniqueArray1, ...uniqueArray2];
 }
 
 export {
