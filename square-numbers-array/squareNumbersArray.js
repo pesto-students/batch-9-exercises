01a1dfce7f5aade8c06cdce64048c028d7944d17
avinash360 <avithakur86@gmail.com>
2019-07-19 05:01:23 +0530

    solve: squareNumbersArray test

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,8 +1,13 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function isArrayNumeric(array) {
+  return array.every(Number);
 }
-
+function squareNumbersArray(...args) {
+  if (!isArrayNumeric([...args])) {
+    throw Error('Array contains non-numeric values');
+  }
+  return [...args].map(el => Math.pow(el, 2));
+}
 export {
   squareNumbersArray,
 };
