1da406c096ec92082c6f2af78930671ff7413264
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-19 00:33:46 +0530

    Solve objectInvert

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,6 +1,12 @@
 
-function objectInvert(...args) {
-  return args;
+function objectInvert(object) {
+  const invertedObject = {};
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      invertedObject[object[key]] = key;
+    }
+  }
+  return invertedObject;
 }
 
 export {
