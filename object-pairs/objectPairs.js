c9cedf3f941ddef1ec00210c2d8070536855d19d
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-19 00:39:44 +0530

    Solve objectPairs

--- object-pairs/objectPairs.js
+++ object-pairs/objectPairs.js
@@ -1,6 +1,12 @@
 
-function objectPairs(...args) {
-  return args;
+function objectPairs(object) {
+  const pairs = [];
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      pairs.push([key, object[key]]);
+    }
+  }
+  return pairs;
 }
 
 export {
