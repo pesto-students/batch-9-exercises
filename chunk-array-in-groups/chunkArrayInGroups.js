cb42ceb1c3af72d71dba1b33f0f0d15d2743e23c
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-19 00:44:47 +0530

    solve chunkArrayInGroups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,22 +1,15 @@
 // const array = ["a", "b", "c", "d"];
 // const splitSize = 2;
 function chunkArrayInGroups(array, splitSize) {
-  // let splitIndex = splitSize - 1;
   let groupOfChunkedArray = [];
   for (
     let splitIndex = 0;
     splitIndex <= array.length - 1;
     splitIndex += splitSize
   ) {
-    // console.log(array.splice(splitIndex, splitIndex + splitSize - 1));
-    groupOfChunkedArray = groupOfChunkedArray.concat(
-      array.splice(splitIndex, splitIndex + splitSize - 1)
-    );
+    groupOfChunkedArray.push(array.slice(splitIndex, splitIndex + splitSize));
   }
-  console.log(groupOfChunkedArray);
   return groupOfChunkedArray;
 }
-
-// console.log(chunkArrayInGroups(array, splitSize));
 
 export { chunkArrayInGroups };
