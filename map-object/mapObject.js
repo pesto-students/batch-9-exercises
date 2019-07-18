c9088e80a9cc5d854f80ac708af3b63020257b19
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-19 01:04:40 +0530

    Write mapObject.js

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,15 @@
+const performOperation = (object, property, task) => task(object[property]);
 
 function mapObject(...args) {
-  return args;
+  const object = args[0];
+  const task = args[1];
+  const newObject = {};
+  for (const property in object) {
+    if (typeof property === 'string') {
+      newObject[property] = performOperation(object, property, task);
+    }
+  }
+  return newObject;
 }
 
 export {
