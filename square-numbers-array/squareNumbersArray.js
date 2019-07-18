40f7523c097f57fc733b64dcff5e486d3708d791
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-19 05:14:10 +0530

    Write squareNumbersArray.js

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,7 +1,10 @@
-
 function squareNumbersArray(...args) {
-  return args;
+  if (!args.every(Number)) {
+    throw Error('My custom error');
+  }
+  return args.map(number => number ** 2);
 }
+
 
 export {
   squareNumbersArray,
