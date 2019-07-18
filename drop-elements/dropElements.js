bc1179da6d39ffe5229c952a8736f0072b5aa94f
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-19 00:37:47 +0530

    Write dropElements.js

--- drop-elements/dropElements.js
+++ drop-elements/dropElements.js
@@ -1,6 +1,9 @@
 
 function dropElements(...args) {
-  return args;
+  const array = args[0];
+  const condition = args[1];
+  const newArray = array.filter(x => condition(x));
+  return newArray;
 }
 
 export {
