232cd988a4569f30bc31b103eff1327e5430aa4a
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-07-19 00:56:19 +0530

    Solve object-invert

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,8 +1,12 @@
-
-function objectInvert(...args) {
-  return args;
+function objectInvert(object) {
+  const invertedObject = {};
+  for (const key in object) {
+    if ({}.hasOwnProperty.call(object, key)) {
+      const value = object[key];
+      invertedObject[value] = key;
+    }
+  }
+  return invertedObject;
 }
 
-export {
-  objectInvert,
-};
+export { objectInvert };
