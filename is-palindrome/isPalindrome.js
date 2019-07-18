27e1ee761d269464664c4f77e017ba369fc5d28f
avinash360 <avithakur86@gmail.com>
2019-07-18 18:24:06 +0530

    solve: isPalindrome test

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,11 @@
+function trimSpecialChars(text) {
+  // this regular expression trims special characters and allows only alphanumeric characters
+  return `${text}`.toLowerCase().replace(/[^a-z0-9]+/g, '');
+}
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(value) {
+  const string = trimSpecialChars(value);
+  return string.split("").reverse().join("") === string;
 }
 
 export {
