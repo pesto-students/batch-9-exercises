89e32e028fe0355ab58d4a9f6a7745b65fc5607c
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-18 18:17:27 +0530

    Solve isPalindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,12 +1,22 @@
+function isLowerCaseChar(char) {
+  const charCode = char.charCodeAt(0);
+  return charCode >= 97 && charCode <= 123;
+}
+
+function isNumber(char) {
+  const charCode = char.charCodeAt(0);
+  return charCode >= 48 && charCode <= 57;
+}
 
 function isPalindrome(sequence) {
   if (typeof sequence === 'string') {
-    const regex = /\W|_/g;
-    const filteredSequence = sequence.replace(regex, '');
-    return filteredSequence.toLowerCase().split('').reverse().join('') === filteredSequence.toLowerCase();
+    const filteredArraySequence = sequence.toLowerCase().split('')
+      .filter(char => isLowerCaseChar(char) || isNumber(char));
+    return filteredArraySequence.join('') === filteredArraySequence.reverse().join('');
   }
   return Number(sequence.toString().split('').reverse().join('')) === sequence;
 }
+
 
 export {
   isPalindrome,
