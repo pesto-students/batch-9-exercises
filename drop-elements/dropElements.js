3271ce8dddcf023766fc7e6422392e0bbae48c4f
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-19 00:17:40 +0530

    [WIP] Complete Day 4 Segement 1

--- drop-elements/dropElements.js
+++ drop-elements/dropElements.js
@@ -1,6 +1,16 @@
 
-function dropElements(...args) {
-  return args;
+function dropElements(array,predicate) {
+  'use strict';
+  if ( ! ((typeof predicate === 'Function' || typeof predicate === 'function') ) )
+      throw new TypeError("Expected predicated to be function \n Received:"+predicate);
+  let result = [] 
+  
+  for(let i = 0 ; i < array.length ; i++) {
+    if(predicate(array[i]))
+    result.push(array[i])
+  }
+
+  return result
 }
 
 export {
