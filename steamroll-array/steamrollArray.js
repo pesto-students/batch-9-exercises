a448672c57297b520b84ed7b5db6707422c685ff
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-18 20:54:28 +0530

    Solve SteamRoll array

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,20 @@
+let flatArray = [];
 
-function steamrollArray(...args) {
-  return args;
+function getFlatArray(inputArray) {
+  for (let i = 0; i < inputArray.length; i += 1) {
+    if (Array.isArray(inputArray[i])) {
+      getFlatArray(inputArray[i]);
+    } else {
+      flatArray.push(inputArray[i]);
+    }
+  }
+  return flatArray;
+}
+
+function steamrollArray(inputArray) {
+  const tempFlatArray = getFlatArray(inputArray);
+  flatArray = [];
+  return tempFlatArray;
 }
 
 export {
