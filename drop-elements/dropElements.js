09b65c9845aa96ed880906e639e15c0baefc60ab
Rajeet <rajeetdel@gmail.com>
2019-07-19 00:58:03 +0530

    DropElement in WIP

--- drop-elements/dropElements.js
+++ drop-elements/dropElements.js
@@ -1,6 +1,20 @@
 
-function dropElements(...args) {
-  return args;
+function dropElements(array1,retain) {
+  const finalArray = array1;
+
+  array1.forEach((e1)=>
+    {
+      if(e1!==retain)
+        {
+          finalArray.shift();
+        }
+      /*if (finalArray.indexOf(retain) <= -1) 
+        {
+        finalArray.shift();
+        }*/
+    });
+
+  return finalArray;
 }
 
 export {
