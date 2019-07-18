8cb46d5632046c215ad7095d7551f6a3e403a7a6
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-18 20:15:05 +0530

    solve largeOfFour

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,8 +1,10 @@
-
-function largeOfFour(...args) {
-  return args;
+function largeOfFour([firstArray, secondArray, thirdArray, fourthArray]) {
+  const resultArray = [];
+  resultArray.push(firstArray.sort((a, b) => a - b)[firstArray.length - 1]);
+  resultArray.push(secondArray.sort((a, b) => a - b)[secondArray.length - 1]);
+  resultArray.push(thirdArray.sort((a, b) => a - b)[thirdArray.length - 1]);
+  resultArray.push(fourthArray.sort((a, b) => a - b)[fourthArray.length - 1]);
+  return resultArray;
 }
 
-export {
-  largeOfFour,
-};
+export { largeOfFour };
