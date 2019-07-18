f74a83486d13ac2c16de38c6ccd935ab67f68a96
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-18 17:28:43 +0530

    solve binaryAgent

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,8 +1,17 @@
+function binaryAgent(stringInBinary) {
+  if (typeof stringInBinary !== "string") {
+    throw Error("Argument: expected string, received " + typeof stringInBinary);
+  }
 
-function binaryAgent(...args) {
-  return args;
+  if (stringInBinary === "") {
+    throw Error("Argument can't be an empty string");
+  }
+  return stringInBinary
+    .split(" ")
+    .reduce(
+      (string, word) => string + String.fromCharCode(parseInt(word, 2)),
+      ""
+    );
 }
 
-export {
-  binaryAgent,
-};
+export { binaryAgent };
