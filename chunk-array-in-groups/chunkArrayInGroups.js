cc0425a84a4d42cd0105b30b36a5e0f66e306654
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-18 23:25:29 +0530

    Solve chunkArrayInGroups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,12 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(array, size) {
+  const chunkedArray = [];
+  let index = 0;
+  while (index < array.length) {
+    chunkedArray.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunkedArray;
 }
 
 export {
