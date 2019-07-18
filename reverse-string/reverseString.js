13a743b04a3e7d17f9e0e5ff29ecf4c157590d11
jayamathan <jayamathanskj03@gmail.com>
2019-07-18 23:21:31 +0530

    solve reverse string

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,8 @@
-
-function reverseString(...args) {
-  return args;
+function reverseString(string) {
+  let revString = '';
+  // eslint-disable-next-line no-return-assign
+  string.split('').forEach(char => revString = char + revString);
+  return revString;
 }
 
 export {
