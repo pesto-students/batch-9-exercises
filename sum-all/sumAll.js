3ab3e699002e20c7bc43d4a1476821721fe1a650
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-18 20:20:50 +0530

    [WIP] Complete the sum all problem

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,8 +1,14 @@
+function sumAll(range) {
 
-function sumAll(...args) {
-  return args;
+  const upperBound = range[0] > range[1] ? range[0] : range[1];
+  const lowerBound = range[0] < range[1] ? range[0] : range[1];
+  
+  const requiredSum = sumOneToN(upperBound)-sumOneToN(lowerBound-1) 
+
+  return requiredSum
 }
 
-export {
-  sumAll,
-};
+function sumOneToN (n) {
+  return (n * (n+1)) / 2;
+}
+export { sumAll };
