a07f6aff27e3a65b9c947e27c73a3700514910df
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-18 17:37:47 +0530

    Solve steamRollArray

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,8 +1,8 @@
-
-function steamrollArray(...args) {
-  return args;
+function steamrollArray(nestedArray) {
+  return nestedArray.reduce(
+    (acc, val) => acc.concat(Array.isArray(val) ? steamrollArray(val) : val),
+    [],
+  );
 }
 
-export {
-  steamrollArray,
-};
+export { steamrollArray };
