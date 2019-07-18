d2ea7ae3c15ac77b9a5996ad10b21a6548d78dc8
Rajeet <rajeetdel@gmail.com>
2019-07-19 04:49:13 +0530

    A

--- drop-elements/dropElements.js
+++ drop-elements/dropElements.js
@@ -1,20 +1,22 @@
 
 function dropElements(array1,retain) {
-  const finalArray = array1;
+ // const finalArray = array1;
 
-  array1.forEach((e1)=>
+return array1.filter(retain);//can be used predicate
+ /* array1.forEach((e1)=>
     {
-      if(e1!==retain)
+      if(e1<retain)
+      //if(e1!==retain)
         {
           finalArray.shift();
-        }
+        }//use filtering
       /*if (finalArray.indexOf(retain) <= -1) 
         {
         finalArray.shift();
-        }*/
-    });
+        }
+    });*/
 
-  return finalArray;
+ // return finalArray;
 }
 
 export {
