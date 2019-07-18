c810ef5f9c42d116235da7be2fab1e5192c1974e
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-19 01:04:02 +0530

    solve objectDefaults

--- object-defaults/objectDefaults.js
+++ object-defaults/objectDefaults.js
@@ -1,8 +1,19 @@
+function objectDefaults(firstObject, secondObject) {
+  const newObject = {};
 
-function objectDefaults(...args) {
-  return args;
+  const firstObjectKeys = Object.keys(firstObject);
+  const secondObjectKeys = Object.keys(secondObject);
+  firstObjectKeys.forEach(key => {
+    newObject.key = firstObject[key];
+  });
+
+  secondObjectKeys.forEach(key => {
+    const newObjectKeys = Object.keys(newObject);
+    if (newObjectKeys.findIndex(key) === -1) {
+      newObject[key] = secondObject[key];
+    }
+  });
+  return newObject;
 }
 
-export {
-  objectDefaults,
-};
+export { objectDefaults };
