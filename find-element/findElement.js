84dac97b5eeaa6d6bd77a95fd1325422f58ddb92
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-19 00:22:37 +0530

    [WIP] Complete the find element problem.

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,6 +1,14 @@
 
-function findElement(...args) {
-  return args;
+function findElement(array, predicate) {
+  let result
+  for(let i = 0 ; i < array.length ; i++) {
+    if(predicate(array[i]))
+    {
+      result = array[i]
+      break
+    }
+  }
+  return result;
 }
 
 export {
