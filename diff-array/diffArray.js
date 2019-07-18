d30733d036cb3e36269fe265c484caf011101c98
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-18 23:24:10 +0530

    Write diffArray.js

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,14 @@
 
 function diffArray(...args) {
-  return args;
+  const firstArray = args[0];
+  const secondArray = args[1];
+  const differenceArray = [];
+  for (const element in firstArray) {
+    if (secondArray.indexOf(element) === -1) {
+      differenceArray.push(element);
+    }
+  }
+  return differenceArray;
 }
 
 export {
