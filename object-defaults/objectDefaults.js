52c304999ca0b463922fa1356c14ef94d597e4d9
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-19 00:36:48 +0530

    [WIP] Complete the default properties adding method.

--- object-defaults/objectDefaults.js
+++ object-defaults/objectDefaults.js
@@ -1,6 +1,15 @@
 
-function objectDefaults(...args) {
-  return args;
+function objectDefaults(obj, defaultProperties) {
+  let newObject = {}
+  for(let i in obj){
+    newObject[i] = obj[i]
+  }
+
+  for(let j in defaultProperties) {
+    if(!obj[j])
+    newObject[j] = defaultProperties[j]
+  }
+  return newObject;
 }
 
 export {
