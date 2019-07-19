11bd2ec5f4b7c9cf2c386f0493ef83fb4bc318e2
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-20 00:20:37 +0530

    [WIP] Solve initial test cases of days in month problem. Add new test cases.

--- days-in-month/daysInMonth.test.js
+++ days-in-month/daysInMonth.test.js
@@ -4,5 +4,12 @@ describe('countDaysInMonth', () => {
   test('should return correct number of days', () => {
     expect(daysInMonth(2014, 4)).toBe(30);
     expect(daysInMonth(2016, 2)).toBe(29);
+    expect(daysInMonth(2018, 2)).toBe(28);
+  });
+
+  test('should throw error when incorrect arguments are there', () => {
+    expect(daysInMonth(2014, 40)).toThrow();
+    expect(daysInMonth(2016, 770)).toThrow();
+    expect(daysInMonth(2018, 2770)).toThrow();
   });
 });
