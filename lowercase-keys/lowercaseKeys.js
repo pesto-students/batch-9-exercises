3a9fd643446591163438ba467f8dd8e53472f4d3
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-20 00:40:36 +0530

    [WIP] Partial complete lowercasekeys.

--- lowercase-keys/lowercaseKeys.js
+++ lowercase-keys/lowercaseKeys.js
@@ -1,7 +1,15 @@
+function copyObjPropertyToLowerCaseKey(accumulator, currentKey) {
 
-function lowercaseKeys(...args) {
-  return args;
+  const keyProperties = Object.getOwnPropertyDescriptors(this[currentKey]);
+  Object.defineProperty(accumulator, (currentKey).toLowerCase(), keyProperties);
+  return accumulator;
 }
+function lowercaseKeys(obj) {
+  const objKeys = Object.getOwnPropertyNames(obj);
+
+  return objKeys.reduce(copyObjPropertyToLowerCaseKey.bind(obj), {});
+}
+
 
 export {
   lowercaseKeys,
