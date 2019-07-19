3e02b06b5182c54dfa6f51eee86e3875ab360b84
avinash360 <avithakur86@gmail.com>
2019-07-19 21:03:26 +0530

    solve: limitFunctionCallCount test

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,11 @@
-
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(cb, cbInvokeLimit) {
+  return function (...args) {
+    if (cbInvokeLimit > 0) {
+      cbInvokeLimit -= 1;
+      return cb(...args);
+    }
+    return null;
+  };
 }
 
 export {
