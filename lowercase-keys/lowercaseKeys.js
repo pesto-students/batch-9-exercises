f11a4a7fb35139f261e9f0a3b6874cffb4fe9f9d
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 00:51:07 +0530

    Solve Lowercase Kays

--- lowercase-keys/lowercaseKeys.js
+++ lowercase-keys/lowercaseKeys.js
@@ -1,6 +1,11 @@
 
-function lowercaseKeys(...args) {
-  return args;
+function lowercaseKeys(obj) {
+  return Object.keys(obj)
+    .reduce((result, key) => {
+      const descriptor = Object.getOwnPropertyDescriptors(obj, key);
+      Object.defineProperty(result, key.toLocaleLowerCase(), descriptor[key]);
+      return result;
+    }, {});
 }
 
 export {
