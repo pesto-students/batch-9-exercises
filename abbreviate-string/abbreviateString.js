f24dcad2a2c9ab37bbb35d7a29bd05b935155835
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-20 00:03:59 +0530

    Write abbreviateString.js

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,13 @@
+const wordAbbreviated = (word) => {
+  const result = word.charAt(0).toUpperCase();
+  return result;
+};
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(string) {
+  const wordsArray = string.split(' ');
+  const lastWord = wordsArray[wordsArray.length - 1];
+  const abbreviatedString = `${wordsArray[0]} ${wordAbbreviated(lastWord)}.`;
+  return abbreviatedString;
 }
 
 export {
