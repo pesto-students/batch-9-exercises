fe56f9ee3fccb40a7ff6342a95c0dabad92c033d
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-19 23:59:38 +0530

    solve abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,8 +1,9 @@
-
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(string) {
+  if (typeof string !== "string") {
+    throw new Error("Expected string in argument, received: " + typeof string);
+  }
+  const words = string.split(" ");
+  return `${words[0]} ${words[words.length - 1].charAt(0).toUpperCase()}.`;
 }
 
-export {
-  abbreviateString,
-};
+export { abbreviateString };
