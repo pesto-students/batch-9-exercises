f4c600cd3a59e3df7ed5278c62433575d59a67b3
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-19 23:56:07 +0530

    Solve abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,12 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(string) {
+  if (typeof string !== 'string') {
+    throw new TypeError(`Expected argument to be of type String. Received: ${typeof string}`);
+  }
+  const words = string.split(' ');
+  const firstWord = words[0];
+  const lastWordLetter = words[words.length - 1][0];
+  return `${firstWord} ${lastWordLetter.toUpperCase()}.`;
 }
 
 export {
