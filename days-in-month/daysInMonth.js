a87ebdcfb220dcc8ac4058ecd99f7a9e4bbbb646
avinash360 <avithakur86@gmail.com>
2019-07-20 00:02:29 +0530

    solve: daysInMonth test

--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,8 +1,12 @@
-
-function daysInMonth(...args) {
-  return args;
+function getPreviousDay(date) {
+  const previousDay = new Date(date.setDate(date.getDate() - 1));
+  return previousDay;
 }
-
+function daysInMonth(year, month) {
+  const date = new Date(year, month);
+  const lastDayOfMonth = getPreviousDay(date);
+  return lastDayOfMonth.getDate();
+}
 export {
   daysInMonth,
 };
