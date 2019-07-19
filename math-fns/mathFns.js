6af20051967a514f593f1f39dbc2eaf5e4c6b858
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 01:29:32 +0530

    Solve Math Fns

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -1,8 +1,10 @@
-
-function mathFns(...args) {
-  return args;
-}
+const sqrt = number => Math.sqrt(number);
+const round = number => Math.round(number);
+// eslint-disable-next-line no-restricted-properties
+const power = (number, pow) => Math.pow(number, pow);
 
 export {
-  mathFns,
+  sqrt,
+  round,
+  power,
 };
