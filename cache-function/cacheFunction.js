94681c3c4d9ef43a1e1e6753b93ce97e495b8956
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-19 17:56:54 +0530

    [WIP] Pass two test cases for cache function.

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,6 +1,13 @@
 
-function cacheFunction(...args) {
-  return args;
+function cacheFunction(randomFn) {
+  const memomize = {
+
+  };
+  function callback(args) {
+    if (!memomize[String(args)]) { memomize[String(args)] = randomFn(args); }
+    return memomize[String(args)];
+  }
+  return callback;
 }
 
 export {
