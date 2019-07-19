4a4ff85f7b2eae829f78460313002c7ccb17461d
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 00:17:54 +0530

    WIP Round To

--- round-to/roundTo.js
+++ round-to/roundTo.js
@@ -1,8 +1,20 @@
+const roundToSharedFunctions = {
+  down: (number, decimalPlaces) => {
+    const factorOf10 = 10 ** decimalPlaces;
+    return Math.floor(number * factorOf10) / factorOf10;
+  },
+  up: (number, decimalPlaces) => {
+    const factorOf10 = 10 ** decimalPlaces;
+    return Math.ceil(number * factorOf10) / factorOf10;
+  },
+};
 
-function roundTo(...args) {
-  return args;
+function roundTo(number, decimalPlaces) {
+  const factorOf10 = 10 ** decimalPlaces;
+  return Math.round(number * factorOf10) / factorOf10;
 }
 
+Object.setPrototypeOf(roundTo, roundToSharedFunctions);
 export {
   roundTo,
 };
