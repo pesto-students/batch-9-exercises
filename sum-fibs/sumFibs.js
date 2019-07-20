ce96370ce736ac981e6e3de1bac19d307e940818
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-20 22:35:23 +0530

    [WIP] Complete the sum fib problem.

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,17 @@
+const fibMemo = { 0: 0, 1: 1 };
+function getFib(n) {
+  if (!fibMemo[n] && fibMemo[n] !== 0) { fibMemo[n] = getFib(n - 1) + getFib(n - 2); }
+  return fibMemo[n];
+}
 
-function sumFibs(...args) {
-  return args;
+function sumFibs(num) {
+  let sum = 0;
+  for (let i = 1; sum <= num; i += 1) {
+    const currentFib = getFib(i);
+    // eslint-disable-next-line no-bitwise
+    if (((currentFib % 2) === 1) && currentFib <= num) { sum += currentFib; }
+  }
+  return sum;
 }
 
 export {
