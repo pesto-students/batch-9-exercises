8540f4118294a02ebed1e3eb4ecc58fee5e52ec5
avinash360 <avithakur86@gmail.com>
2019-07-21 04:51:05 +0530

    solve: convertToRoman test

--- convert-to-roman/convertToRoman.js
+++ convert-to-roman/convertToRoman.js
@@ -1,6 +1,15 @@
 
-function convertToRoman(...args) {
-  return args;
+function convertToRoman(number) {
+  let result = '';
+  const numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const romanNumbers = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  for (let i = 0; i <= numbers.length; i++) {
+    while (number % numbers[i] < number) {
+      result += romanNumbers[i];
+      number -= numbers[i];
+    }
+  }
+  return result;
 }
 
 export {
