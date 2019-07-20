f814e2478612cf38c106364192b44574e3203a1e
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 21:04:52 +0530

    Solve Sum Primes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,30 @@
+const isPrime = (number) => {
+  let result = true;
+  if (number <= 1) {
+    result = false;
+  } else {
+    for (let i = 2, s = Math.sqrt(number); i <= s; i += 1) {
+      if (number % i === 0) {
+        result = false;
+      }
+    }
+  }
+  return result;
+};
 
-function sumPrimes(...args) {
-  return args;
+const getPrimeList = (upperLimit) => {
+  const array = [...Array(upperLimit + 1).keys()];
+  return array.reduce((result, val) => {
+    if (isPrime(val)) {
+      return [...result, val];
+    }
+    return result;
+  }, []);
+};
+const sumList = list => list.reduce((result, number) => result + number, 0);
+
+function sumPrimes(upperLimit) {
+  return sumList(getPrimeList(upperLimit));
 }
 
 export {
