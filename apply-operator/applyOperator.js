0ec2b9d1af12c0587fec13503480c5853d8de29b
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-21 04:23:21 +0530

    solve applyOperator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,30 @@
 
-function applyOperator(...args) {
-  return args;
+function applyOperator(operator, ...operands) {
+  let result;
+  switch (operator) {
+    case '/':
+      result = operands.reduce((res, operand) => res / operand, 1);
+      break;
+
+    case '*':
+      result = operands.reduce((res, operand) => res * operand, 1);
+      break;
+
+    case '+':
+      result = operands.reduce((res, operand) => res + operand, 0);
+      break;
+
+    case '-':
+      result = operands.reduce((res, operand) => res - operand, 0);
+      break;
+
+    case '%':
+      result = operands.reduce((res, operand) => res % operand);
+      break;
+    default:
+      throw Error('Invalid argument, first argument should be one of these oprators { /  * +  - % }');
+  }
+  return result;
 }
 
 export {
