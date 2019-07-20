0f0bdada3a138e5c473139402d8bfd67b8f7b6fc
avinash360 <avithakur86@gmail.com>
2019-07-21 02:17:02 +0530

    solve: rot13 test

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,8 +1,21 @@
-
-function rot13(...args) {
-  return args;
+function rot13(string) {
+  const lowerLimit = 65;
+  const upperLimit = 90;
+  const charDistance = 13;
+  let newString = '';
+  for (let i = 0; i < string.length; i += 1) {
+    const code = string.charCodeAt(i);
+    if (code >= lowerLimit && code <= upperLimit) {
+      const charCode = code + charDistance;
+      const extra = charCode - upperLimit;
+      const newCharCode = extra > 0 ? lowerLimit + extra - 1 : charCode;
+      newString += String.fromCharCode(newCharCode);
+    } else {
+      newString += string[i];
+    }
+  }
+  return newString;
 }
-
 export {
   rot13,
 };
