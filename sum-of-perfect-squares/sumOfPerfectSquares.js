ae70004576bcdd794dbf5374a2fd8f7d8bd396e5
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 22:15:00 +0530

    Solve Sum Of Perfect Squares

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,6 +1,23 @@
+function sumOfPerfectSquares(number) {
+  if (number <= 3) {
+    return number;
+  }
 
-function sumOfPerfectSquares(...args) {
-  return args;
+  let result = number;
+
+  for (let i = 1; i < number + 1; i += 1) {
+    // get squared number
+    const temp = i * i;
+    if (temp > number) {
+      break;
+    } else {
+      // get whatever is lower:
+      // 1 - the current minimum, or
+      // 2 - the minimum after considering the next perfect square
+      result = Math.min(result, 1 + sumOfPerfectSquares(number - temp));
+    }
+  }
+  return result;
 }
 
 export {
