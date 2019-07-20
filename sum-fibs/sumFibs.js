ddf6cf0ed9ff0ec44f7853e6dbf6d3ce82f13ed9
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-21 04:50:19 +0530

    solve sumFib

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,8 +1,21 @@
-
-function sumFibs(...args) {
-  return args;
+function generateFibonnaciSeries(number) {
+  const series = [1, 1];
+  while (series[series.length - 1] + series[series.length - 2] < number) {
+    series.push(series[series.length - 1] + series[series.length - 2]);
+  }
+  return series;
 }
 
-export {
-  sumFibs,
-};
+function sumFibs(number) {
+  const fibonnaciSeries = generateFibonnaciSeries(number);
+  const oddNumberedFibonnaciSeries = fibonnaciSeries.filter(
+    element => element % 2 !== 0
+  );
+  const sumOfOddNumberedFibonnaciSeries = oddNumberedFibonnaciSeries.reduce(
+    (sum, element) => sum + element,
+    0
+  );
+  return sumOfOddNumberedFibonnaciSeries;
+}
+
+export { sumFibs };
