1ec389be37ab983f8979f957ee5fe735b95e9cc4
jayamathan <jayamathanskj03@gmail.com>
2019-07-20 23:08:02 +0530

    solve sumprime

--- rectangle-diagonal/rectangleDiagonal.js
+++ rectangle-diagonal/rectangleDiagonal.js
@@ -1,6 +1,6 @@
-
+/* eslint-disable no-restricted-properties */
+const square = number => Math.pow(number, 2);
 function rectangleDiagonal(length, width) {
-  const square = number => number * number;
   const diagonal = Math.sqrt(square(length) + square(width));
   return diagonal;
 }
