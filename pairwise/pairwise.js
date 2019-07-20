cb8f316e75995e1453c7b415dd1b82a8f16937fd
avinash360 <avithakur86@gmail.com>
2019-07-21 03:14:51 +0530

    solve: pairWise test

--- pairwise/pairwise.js
+++ pairwise/pairwise.js
@@ -1,6 +1,19 @@
+const sum = first => second => first + second;
 
-function pairwise(...args) {
-  return args;
+function pairwise(array, target) {
+  let total = 0;
+  let used = [];
+  for (let i = 0; i < array.length; i++) {
+    const firstNum = sum(array[i]);
+    for (let j = i + 1; j < array.length; j++) {
+      const sum = firstNum(array[j]);
+      if (sum === target && !used.includes(i) && !used.includes(j)) {
+        total = total + i + j;
+        used.push(i, j);
+      }
+    }
+  }
+  return total;
 }
 
 export {
