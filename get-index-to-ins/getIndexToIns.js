e95f090a7e83b63b47afd33bc976b6b38ebedc5f
avinash360 <avithakur86@gmail.com>
2019-07-21 02:28:10 +0530

    solve: getIndexToIns test

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,8 +1,13 @@
+const sortAscending = (a, b) => a - b;
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(array, number) {
+  const sortedArray = array.sort(sortAscending);
+  let index = sortedArray.findIndex(element => element >= number);
+  if (index < 0) {
+    index = array.length;
+  }
+  return index;
 }
-
 export {
   getIndexToIns,
 };
