9d124a54f12e5ee9a94dec3f28c80e8a778f464c
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-21 00:18:49 +0530

    [WIP] Complete the rot13 problem.

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,8 +1,17 @@
 
-function rot13(...args) {
-  return args;
+function decodeCharcter(char) {
+  const asciiValue = char.charCodeAt(0);
+  // eslint-disable-next-line max-len
+  const isEnglishAlphabet = (asciiValue >= 97 && asciiValue <= 122) || (asciiValue >= 65 && asciiValue <= 90);
+  if (!isEnglishAlphabet) { return char; }
+  const decodedAsciiValue = (((asciiValue - 65) + 13) % 26) + 65;
+  return String.fromCharCode(decodedAsciiValue);
 }
-
+function rot13(encodedString) {
+  const encodedCharArr = encodedString.split('');
+  const decodedString = encodedCharArr.map(decodeCharcter).join('');
+  return decodedString;
+}
 export {
   rot13,
 };
