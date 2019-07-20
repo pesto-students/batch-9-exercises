669d0d48e9c3b96269bae52ad5b1f25cdd8292c6
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-21 03:44:41 +0530

    solve counterFactory

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,20 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+
+  let count = 0;
+
+  function incrementCount() {
+    return ++count;
+  }
+
+  function decrementCount() {
+    return --count;
+  }
+
+  return {
+    increment: incrementCount,
+    decrement: decrementCount,
+  }
 }
 
 export {
