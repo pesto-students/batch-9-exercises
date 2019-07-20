94a076ee4560848eeabf1fb968e1655ea0b7d91f
jayamathan <jayamathanskj03@gmail.com>
2019-07-20 20:38:33 +0530

    solve abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,13 @@
+/* eslint-disable no-undef */
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(inputString) {
+  if (typeof inputString !== 'string') {
+    throw new TypeError(`Expected string input. Received ${typeof inputString}`);
+  }
+  const splitString = inputString.split(' ');
+  const firstWord = splitString[0];
+  const lastwordFirstletter = splitString[splitString.length - 1][0];
+  return `${firstWord} ${lastwordFirstletter.toUpperCase()}.`;
 }
 
 export {
