4ed56b440a690f9fff319f09d5eac894c3da8fa3
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 23:51:17 +0530

    Solve Telephone Check

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,6 +1,8 @@
 
-function telephoneCheck(...args) {
-  return args;
+function telephoneCheck(telephoneNumber) {
+  // src: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s02.html
+  const usaTelephoneNumberRegex = /^(?:\+?1[- ]?)?\(([0-9]{3})\)[- ]?([0-9]{3})[- ]([0-9]{4})$/;
+  return usaTelephoneNumberRegex.test(telephoneNumber);
 }
 
 export {
