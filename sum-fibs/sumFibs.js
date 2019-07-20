e35e811bc3a424a191f292c8495a8e9aa6256f90
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-21 01:24:43 +0530

    Solve sumFibs

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,18 @@
 
-function sumFibs(...args) {
-  return args;
+function sumFibs(limit) {
+  let a = 1;
+  let b = 1;
+  let c = a + b;
+  let sum = c;
+  while (c <= limit) {
+    [a, b] = [b, c];
+    c = a + b;
+    // eslint-disable-next-line no-bitwise
+    if (c <= limit && Boolean(c & 1)) {
+      sum += c;
+    }
+  }
+  return sum;
 }
 
 export {
