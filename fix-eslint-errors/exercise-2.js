0359330d70734c87c43d609a81ce71bcc4d2cb11
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 06:24:13 +0530

    Fix EsLint Exercise 2

---
+++ fix-eslint-errors/exercise-2.js
@@ -0,0 +1,73 @@
+function noop() {}
+
+if (typeof console === 'undefined') {
+  window.console = {
+    warn: noop,
+    error: noop,
+  };
+}
+
+// avoid info messages during test
+window.console.info = noop;
+
+let asserted;
+
+function createCompareFn(spy) {
+  const hasWarned = (msg) => {
+    let count = spy.calls.count();
+    let args;
+    let result = null;
+    function containsMsg(arg) {
+      return arg.toString().indexOf(msg) > -1;
+    }
+    while (count) {
+      args = spy.calls.argsFor(count);
+      if (args.some(containsMsg)) {
+        result = true;
+      }
+      count -= 1;
+    }
+    return result;
+  };
+
+  return {
+    compare: (msg) => {
+      asserted = asserted.concat(msg);
+      const warned = Array.isArray(msg)
+        ? msg.some(hasWarned)
+        : hasWarned(msg);
+      return {
+        pass: warned,
+        message: warned
+          ? `Expected message "${msg}" not to have been warned`
+          : `Expected message "${msg}" to have been warned`,
+      };
+    },
+  };
+}
+
+// define custom matcher for warnings
+beforeEach(() => {
+  asserted = [];
+  spyOn(console, 'warn');
+  spyOn(console, 'error');
+  jasmine.addMatchers({
+    toHaveBeenWarned: () => createCompareFn(window.console.error),
+    toHaveBeenTipped: () => createCompareFn(window.console.warn),
+  });
+});
+
+afterEach((done) => {
+  const warned = msg => asserted.some(assertedMsg => msg.toString().indexOf(assertedMsg) > -1);
+  let count = window.console.error.calls.count();
+  let args;
+  while (count) {
+    args = window.console.error.calls.argsFor(count);
+    if (!warned(args[0])) {
+      done.fail(`Unexpected console.error message: ${args[0]}`);
+      return;
+    }
+    count -= 1;
+  }
+  done();
+});
