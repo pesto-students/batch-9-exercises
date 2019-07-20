88184e718dad679135838661bb991a7905eb5338
jayamathan <jayamathanskj03@gmail.com>
2019-07-20 23:58:38 +0530

    WIP Fibonacci

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,20 @@
+/* eslint-disable no-undef */
+const isFibonacci = (number) => {
+  const fib = [0, 1];
+  for (i = 2; i <= number; i += 1) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  return fib;
+};
 
-function sumFibs(...args) {
-  return args;
+function sumFibs(number) {
+  let fibSum = 0;
+  isFibonacci(number).forEach((el) => {
+    if (el % 2 !== 0) {
+      fibSum += el;
+    }
+  });
+  return fibSum;
 }
 
 export {
