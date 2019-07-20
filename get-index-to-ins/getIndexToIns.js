6e7e96b010ce94315181b79966a23ff15ccf9057
Kanav Arora <arorakanav11@gmail.com>
2019-07-21 00:21:47 +0530

    Solve Index To Insert

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,14 @@
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(array, numberToInsert) {
+  const sortedArray = array.sort((a, b) => a - b);
+
+  for (let index = 0; index < sortedArray.length; index += 1) {
+    if (numberToInsert <= sortedArray[index]) {
+      return index;
+    }
+  }
+
+  return sortedArray.length;
 }
 
 export {
