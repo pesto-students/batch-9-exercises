e028d06547059c010dca3946e3d76b80a0311929
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-21 05:26:48 +0530

    solve sumPrimes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,8 +1,28 @@
-
-function sumPrimes(...args) {
-  return args;
+function isPrime(num) {
+  const squareroot = Math.floor(Math.sqrt(num));
+  for (let i = 2; i <= squareroot; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-export {
-  sumPrimes,
-};
+function sumPrimes(number) {
+  const primeNumbers = [2];
+  for (let i = 3; i <= number; i += 2) {
+    if (isPrime(i)) {
+      primeNumbers.push(i);
+    }
+  }
+  console.log(primeNumbers);
+  const primeNumbersSum = primeNumbers.reduce(
+    (sum, elememt) => sum + elememt,
+    0
+  );
+
+  return primeNumbersSum;
+}
+
+export { sumPrimes };
+// console.log(sumPrimes(10));
