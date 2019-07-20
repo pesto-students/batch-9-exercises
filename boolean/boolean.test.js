c90701b8b8c17943336404a62e2defefcda6e6db
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-20 20:37:12 +0530

    [WIP] Add test case to fail the boolean problem.

--- boolean/boolean.test.js
+++ boolean/boolean.test.js
@@ -4,5 +4,9 @@ describe('check for boolean', () => {
   test('should return a boolean', () => {
     expect(typeof boolean(true)).toBe('boolean');
     expect(boolean('')).toBe(false);
+    expect(boolean(null)).toBe(false);
+  });
+  test('should throw an error', () => {
+    expect(() => boolean()).toThrow();
   });
 });
