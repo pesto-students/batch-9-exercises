0047ae5106e0be163b247a999633e0cecf049a72
Kanav Arora <arorakanav11@gmail.com>
2019-07-21 01:36:50 +0530

    Solve Pair Wise

--- pairwise/pairwise.js
+++ pairwise/pairwise.js
@@ -1,6 +1,20 @@
+function getPairIndices(array, sum) {
+  let usedIndexes = [];
 
-function pairwise(...args) {
-  return args;
+  for (let index1 = 0; index1 < array.length; index1 += 1) {
+    for (let index2 = index1 + 1; index2 < array.length; index2 += 1) {
+      if (array[index1] + array[index2] === sum
+        && !usedIndexes.includes(index1)
+        && !usedIndexes.includes(index2)) {
+        usedIndexes = [...usedIndexes, index1, index2];
+      }
+    }
+  }
+  return usedIndexes;
+}
+function pairwise(array, sum) {
+  const sumIndices = getPairIndices(array, sum).reduce((result, number) => result + number, 0);
+  return sumIndices;
 }
 
 export {
