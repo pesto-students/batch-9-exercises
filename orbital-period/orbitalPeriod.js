2257e018290d600ba1551c7645951670a37f9140
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-21 02:55:36 +0530

    [WIP] Complete the orbital period problem.

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,7 +1,27 @@
+/* eslint-disable max-len */
+/* eslint-disable no-underscore-dangle */
 
-function orbitalPeriod(...args) {
-  return args;
+const GM = 398600.4418; // in km3s-2 (The gravitational parameter for a body GM is the gravitational constant G multiplied by the mass of the body.)
+const earthRadius = 6367.4447; // in km
+const _2Pi = 2 * Math.PI;
+
+/**
+ *
+ * Formula used to calculate the elements’ average
+ * altitude into their orbital periods (in seconds)
+ * is taken from here: https://en.wikipedia.org/wiki/Orbital_period
+ *
+ */
+function getOribitalPeriod(element) {
+  const dividend = ((earthRadius + element.avgAlt) ** 3);
+  const partialResult = Math.sqrt(dividend / GM);
+  const orbPeriod = Math.round(_2Pi * partialResult);
+  return { name: element.name, orbitalPeriod: orbPeriod };
 }
+function orbitalPeriod(elements) {
+  return elements.map(getOribitalPeriod);
+}
+
 
 export {
   orbitalPeriod,
