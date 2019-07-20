03e3d3b92c6a17d2308f90e32089013866268b6a
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 23:47:33 +0530

    Solve Longest word In String

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,6 +1,13 @@
 
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(string) {
+  const words = string.split(' ');
+  let longestWordLength = 0;
+  words.forEach((word) => {
+    if (word.length > longestWordLength) {
+      longestWordLength = word.length;
+    }
+  });
+  return longestWordLength;
 }
 
 export {
