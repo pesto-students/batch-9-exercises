a561cc3c620b8177a612f7065f3dac1004e30c93
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 20:16:51 +0530

    Solve Permutation Without Repetition

--- permutate-without-repetitions/permutateWithoutRepetitions.js
+++ permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -1,6 +1,23 @@
+function permutateWithoutRepetitions(permutationOptions) {
+  if (permutationOptions.length === 1) {
+    return [permutationOptions];
+  }
 
-function permutateWithoutRepetitions(...args) {
-  return args;
+  const permutations = [];
+  const smallerPermutations = permutateWithoutRepetitions(permutationOptions.slice(1));
+  const firstOption = permutationOptions[0];
+
+  for (let permIndex = 0; permIndex < smallerPermutations.length; permIndex += 1) {
+    const smallerPermutation = smallerPermutations[permIndex];
+    // Insert first option into every possible position of smallerPermutation.
+    for (let positionIndex = 0; positionIndex <= smallerPermutation.length; positionIndex += 1) {
+      const permutationPrefix = smallerPermutation.slice(0, positionIndex);
+      const permutationSuffix = smallerPermutation.slice(positionIndex);
+      permutations.push(permutationPrefix.concat([firstOption], permutationSuffix));
+    }
+  }
+
+  return permutations;
 }
 
 export {
