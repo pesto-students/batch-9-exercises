938c40ec4c535ee86b08919914519e13612bc6d6
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-21 04:40:51 +0530

    Solve binaryAgent

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,9 @@
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(binarySentence) {
+  if (typeof binarySentence !== 'string') {
+    throw Error(`Expected argument to be of type string. Received: ${typeof binarySentence}`);
+  }
+  return binarySentence.split(' ').reduce((sentence, word) => sentence + String.fromCharCode(parseInt(word, 2)), '');
 }
 
 export {
