b8ad2ba301002e5c50532676d60f8d581ace8f20
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-20 19:54:17 +0530

    Write applyOperator.js

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,26 @@
 
-function applyOperator(...args) {
-  return args;
+function applyOperator(operator, ...args) {
+  let result = 0;
+  switch (operator) {
+    case '+':
+      result = args.reduce((accumulator, value) => accumulator + value, 0);
+      break;
+    case '-':
+      result = args.reduce((accumulator, value) => accumulator - value, 0);
+      break;
+    case '*':
+      result = args.reduce((accumulator, value) => accumulator * value, 1);
+      break;
+    case '/':
+      result = args.reduce((accumulator, value) => accumulator / value, 1);
+      break;
+    case '%':
+      result = args.reduce((accumulator, value) => accumulator % value);
+      break;
+    default:
+      throw Error(`Expected: An arithmetic Operator, Received: ${args}`);
+  }
+  return result;
 }
 
 export {
