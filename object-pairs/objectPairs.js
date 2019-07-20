1008ecc00653ce0e4e3632944221493151a27123
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-21 03:57:34 +0530

    [WIP] Complete the object pair problem.

--- object-pairs/objectPairs.js
+++ object-pairs/objectPairs.js
@@ -1,11 +1,7 @@
-function objectPairs(obj) {
-  let result = [];
-  let i = 0;
-  for (let key in obj) {
-    result[i] = [key, obj[key]];
-  }
 
-  return result
+
+function objectPairs(obj) {
+  return Object.keys(obj).map(key => [key, obj[key]]);
 }
 
 export { objectPairs };
