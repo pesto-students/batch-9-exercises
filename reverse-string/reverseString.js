b6fc77c5735226816e55446d3e7bf3cc7e92552e
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 23:44:33 +0530

    Solve Reverse String

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,11 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(string) {
+  const lastIndexOfString = string.length - 1;
+  let newString = '';
+  for (let i = lastIndexOfString; i >= 0; i -= 1) {
+    newString += string[i];
+  }
+  return newString;
 }
 
 export {
