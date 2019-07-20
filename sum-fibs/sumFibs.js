c5369deae458bc562b3a9046d36182e58b4dec98
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-21 03:06:41 +0530

    Write sumFibs.js

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,21 @@
+const generateNextFibonacci = (initial, next) => {
+  const newFib = initial + next;
+  return newFib;
+};
 
-function sumFibs(...args) {
-  return args;
+function sumFibs(limit) {
+  let first = 1;
+  let second = 1;
+  let sumFib = 1;
+  while (second <= limit) {
+    if (second % 2 !== 0) {
+      sumFib += second;
+    }
+    const temp = second;
+    second = generateNextFibonacci(first, second);
+    first = temp;
+  }
+  return sumFib;
 }
 
 export {
