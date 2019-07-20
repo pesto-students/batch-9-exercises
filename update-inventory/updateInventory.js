e236a935fe3f65f7da483010a0d86e28de915dbb
Kanav Arora <arorakanav11@gmail.com>
2019-07-21 04:15:16 +0530

    Solve Update Inventory

--- update-inventory/updateInventory.js
+++ update-inventory/updateInventory.js
@@ -1,6 +1,18 @@
 
-function updateInventory(...args) {
-  return args;
+const getUpdateInventoryObj = allInventory => allInventory
+  .reduce((updatedInventory, [itemQuantity, itemName]) => ({
+    ...updatedInventory,
+    [itemName]: (updatedInventory[itemName] || 0) + itemQuantity,
+  }), {});
+
+
+function updateInventory(currentInventory, newInventory) {
+  const allInventory = [...currentInventory, ...newInventory];
+  const updatedInventoryObj = getUpdateInventoryObj(allInventory);
+  // console.log('updatedInventoryObj', updatedInventoryObj);
+  return Object.keys(updatedInventoryObj)
+    .sort()
+    .map(key => [updatedInventoryObj[key], key]);
 }
 
 export {
