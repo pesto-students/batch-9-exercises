e62ce1fa996b1a7bc1698131436189c4dea6fb65
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-21 04:54:19 +0530

    Complete get-index-to-ins

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,8 +1,7 @@
 
-function getIndexToIns(...args) {
-  const [array, number] = [...args];
+function getIndexToIns(array, number) {
   let position = array.length;
-  const sortedArray = array.sort();
+  const sortedArray = array.sort((a, b) => a - b);
   for (const member of sortedArray) {
     if (number <= member) {
       position = sortedArray.indexOf(member);
