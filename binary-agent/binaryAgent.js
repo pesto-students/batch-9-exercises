697acf4c84b85707d58dd578f2acb93162671e0a
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-20 22:52:22 +0530

    [WIP] Complete the binary agent problem.

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,15 @@
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(str) {
+  const newBin = str.split(' ');
+  const binCode = [];
+
+  for (let i = 0; i < newBin.length; i += 1) {
+    const asciiCharCode = parseInt(newBin[i], 2);
+    const asciiValue = String.fromCharCode(asciiCharCode);
+    binCode.push(asciiValue);
+  }
+  const englishString = binCode.join('');
+  return englishString;
 }
 
 export {
