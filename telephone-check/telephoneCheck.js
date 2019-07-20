7b5ce24dfe5c729970219b4f67b624785145b22c
avinash360 <avithakur86@gmail.com>
2019-07-21 01:47:44 +0530

    solve: telephoneCheck test

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,8 +1,15 @@
-
-function telephoneCheck(...args) {
-  return args;
+function areBracketsBalanced(string, types) {
+  const openingCount = string.split('').reduce((total, element) => (element === types.opening ? total + 1 : total), 0);
+  const closingCount = string.split('').reduce((total, element) => (element === types.closing ? total + 1 : total), 0);
+  return openingCount === closingCount;
 }
-
+function telephoneCheck(string) {
+  if (!areBracketsBalanced(string, { opening: '(', closing: ')' })) {
+    return false;
+  }
+  const phoneRegex = /^[1]?[-\s\.]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return phoneRegex.test(string);
+}
 export {
   telephoneCheck,
 };
