81954b1000305b00094c3c4a2b3e79f20e299dba
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-20 22:58:17 +0530

    [WIP] Complete number to roman problem.

--- convert-to-roman/convertToRoman.js
+++ convert-to-roman/convertToRoman.js
@@ -1,6 +1,23 @@
+/* eslint-disable no-param-reassign */
 
-function convertToRoman(...args) {
-  return args;
+function convertToRoman(num) {
+  const roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+  const arabic = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  let result = '';
+
+  while (num >= 4000) {
+    result += 'M';
+    num -= 1000;
+  }
+
+  for (let i = arabic.length - 1; num > 0; i -= 1) {
+    while (num >= arabic[i]) {
+      result += roman[i];
+      num -= arabic[i];
+    }
+  }
+
+  return result;
 }
 
 export {
