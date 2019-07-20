127b89b370c659d71640d5364cd783758d4e8315
Kanav Arora <arorakanav11@gmail.com>
2019-07-20 21:33:11 +0530

    Solve Sum Fibs

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,26 +1,21 @@
 
-// eslint-disable-next-line consistent-return
-const getFibonacciSeries = (number) => {
-  const series = [0, 1];
-  const seriesLength = series.length();
-  const lastNumber = series[seriesLength - 1];
-  const secondLastMNumber = series[seriesLength - 2];
-  if (series[seriesLength] >= number) {
-    return series;
-  // eslint-disable-next-line no-else-return
-  } else {
-    series.push(lastNumber + secondLastMNumber);
-    getFibonacciSeries(number);
-    return series;
+function calculateFib(upperLimit, fibArray) {
+  const [secondLatNum, lastNum] = fibArray.slice(fibArray.length - 2);
+  const nextNum = secondLatNum + lastNum;
+  if (upperLimit >= nextNum) {
+    return calculateFib(upperLimit, [...fibArray, nextNum]);
   }
-};
+  return fibArray;
+}
 
 const isOdd = num => num % 2 !== 0;
 
 const sum = numbers => numbers.reduce((result, value) => result + value, 0);
 
-function sumFibs(limit) {
-  return sum(getFibonacciSeries(limit).filter(isOdd));
+function sumFibs(upperLimit) {
+  const fibArray = [0, 1];
+  const oddFibonacciSeries = calculateFib(upperLimit, fibArray).filter(isOdd);
+  return sum(oddFibonacciSeries);
 }
 
 export {
