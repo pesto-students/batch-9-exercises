0433dd6a81a4fd414d33fcdd5c137b15845fa2ea
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-20 20:46:09 +0530

    [WIP] Added the test cases for the boolean problem.

--- boolean/boolean.js
+++ boolean/boolean.js
@@ -1,7 +1,10 @@
 
 function boolean(val) {
-  if (val) return true;
-  return false;
+  switch (arguments.length) {
+    case 1: if (val) { return true; }
+      return false;
+    default: throw Error(`Arguments length Not Valid. \n Expected Argumnets:1 \n Receievd Arguments:${arguments.length}`);
+  }
 }
 
 export {
