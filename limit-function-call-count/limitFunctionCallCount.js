3339170f16366752b5f7306c9751cba876ad9488
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-21 01:31:37 +0530

    Write limitFunctionCallCount.js

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,14 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(callBack, callCount) {
+  let count = 0;
+  return function callCountLimiter(...args) {
+    let returnValue = null;
+    if (count < callCount) {
+      count += 1;
+      returnValue = callBack(...args);
+    }
+    return returnValue;
+  };
 }
 
 export {
