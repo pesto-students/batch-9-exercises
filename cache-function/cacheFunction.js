5a4d4f8fcae0a683ee63855ae237b4642a58f461
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-21 01:11:37 +0530

    Write cacheFunction.js

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,6 +1,13 @@
-
-function cacheFunction(...args) {
-  return args;
+function cacheFunction(callBack) {
+  const cache = {};
+  return function callBackHandler(args) {
+    if (!Object.prototype.hasOwnProperty.call(cache, args)) {
+      const callBackReturn = callBack(args);
+      cache[args] = callBackReturn;
+      return callBackReturn;
+    }
+    return cache[args];
+  };
 }
 
 export {
