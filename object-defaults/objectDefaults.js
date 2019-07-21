f545a224d81abdd7d414659ac1ac41d9b64058ba
jayamathan <jayamathanskj03@gmail.com>
2019-07-22 00:17:23 +0530

    user password

--- object-defaults/objectDefaults.js
+++ object-defaults/objectDefaults.js
@@ -1,6 +1,12 @@
 
-function objectDefaults(...args) {
-  return args;
+function objectDefaults(obj, defaultObj) {
+  const allObjectsProperties = [...Object.getOwnPropertyNames(obj),
+    ...Object.getOwnPropertyNames(defaultObj)];
+  return allObjectsProperties
+    .reduce((resultObj, key) => ({
+      ...resultObj,
+      [key]: (obj[key] || defaultObj[key]),
+    }), {});
 }
 
 export {
