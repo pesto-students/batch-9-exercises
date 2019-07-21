2a6edb18e3913ac37c615386033129d1c8c23693
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-21 17:30:46 +0530

    Solve orbitalPeriod

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,6 +1,18 @@
+const calculateOrbitalPeriod = (height) => {
+  const productOfGravitationConstantAndMassOfSatellite = 398600.4418;
+  const earthRadius = 6367.4447;
+  const distanceFromCore = earthRadius + height;
+  return Math.round(((2 * Math.PI * (distanceFromCore ** 1.5))
+    / (productOfGravitationConstantAndMassOfSatellite ** 0.5)));
+};
 
-function orbitalPeriod(...args) {
-  return args;
+function orbitalPeriod(satellites) {
+  return satellites.map((satellite) => {
+    const satelliteWithOrbitalPeriod = {};
+    satelliteWithOrbitalPeriod.name = satellite.name;
+    satelliteWithOrbitalPeriod.orbitalPeriod = calculateOrbitalPeriod(satellite.avgAlt);
+    return satelliteWithOrbitalPeriod;
+  });
 }
 
 export {
