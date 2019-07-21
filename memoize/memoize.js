734416f0b2c07398efbf5a2cc739a725624e6105
jayamathan <jayamathanskj03@gmail.com>
2019-07-22 02:56:18 +0530

    solve curry&Memoize

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,19 @@
+/* eslint-disable prefer-const */
 
-function memoize(...args) {
-  return args;
+function memoize(fn) {
+  let cache = {};
+
+  return (...args) => {
+    let result;
+    const key = args.toString();
+    if (key in cache) {
+      result = cache[key];
+    } else {
+      result = fn(...args);
+      cache[key] = result;
+    }
+    return result;
+  };
 }
 
 export {
