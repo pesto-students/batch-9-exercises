128944ee0b4568410da2249b24553ea96d250fe1
jayamathan <jayamathanskj03@gmail.com>
2019-07-22 03:06:07 +0530

    solve requestValidator

--- request-validator/requestValidator.js
+++ request-validator/requestValidator.js
@@ -1,6 +1,23 @@
+const validMethods = ['GET', 'POST', 'DELETE'];
+const CheckValidMethods = method => validMethods.includes(method);
 
-function requestValidator(...args) {
-  return args;
+const checkValidURL = (uri) => {
+  const uriRegex = /^([a-z]+:\/\/)?([a-z.]+[^ ]+[a-z.]+)+$/;
+  return uriRegex.test(uri);
+};
+
+function requestValidator(request) {
+  if (!CheckValidMethods(request.method)) {
+    throw Error('Invalid request header: Invalid Method');
+  }
+  if (!checkValidURL(request.uri)) {
+    throw Error('Invalid request header: Invalid URI');
+  }
+
+  if (request.message === undefined) {
+    throw Error('Invalid request header: Invalid Message');
+  }
+  return request;
 }
 
 export {
