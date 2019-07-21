d1af2946fcdc93470fe78d242e363acce8908b92
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-21 06:51:25 +0530

    Complete longest-word-in-string

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,6 +1,8 @@
-
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(string) {
+  const words = string.split(' ');
+  words.sort((a, b) => (a.length - b.length));
+  const sizeOfLongestWord = words.slice(-1).toString().length;
+  return sizeOfLongestWord;
 }
 
 export {
