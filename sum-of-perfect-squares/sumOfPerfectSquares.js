991da90aa0722a4b0f1129205be60256a667f103
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-21 06:07:30 +0530

    solve sumOfPerfectSquare

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,8 +1,15 @@
-
-function sumOfPerfectSquares(...args) {
-  return args;
+function sumOfPerfectSquares(number) {
+  if (Math.sqrt(number) - Math.floor(Math.floor(number)) === 0) return 1;
+  if (number <= 3) return number;
+  // (1*1 + 1*1 + ....number)
+  let result = number;
+  for (let x = 1; x <= number; x++) {
+    let temp = x * x;
+    if (temp > number) {
+      break;
+    } else result = Math.min(result, 1 + sumOfPerfectSquares(number - temp));
+  }
+  return result;
 }
 
-export {
-  sumOfPerfectSquares,
-};
+export { sumOfPerfectSquares };
