b1d89b7d6488b9c68baba1236dc182628381e5f8
jayamathan <jayamathanskj03@gmail.com>
2019-07-21 07:08:50 +0530

    solve Palindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,11 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(input) {
+  const string = input.toString();
+  const lowerCaseString = string.toLowerCase();
+  const alphaNumericRegex = /[^0-9a-z]/g;
+  const alphaNumericString = lowerCaseString.replace(alphaNumericRegex, '');
+  const reverseAlphaNumericString = alphaNumericString.split('').reverse().join('');
+  return reverseAlphaNumericString === alphaNumericString;
 }
 
 export {
