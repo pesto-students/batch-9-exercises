b8dfffcba18280b0965ea469ee1209dc37c8543b
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-21 05:47:43 +0530

    solve threeSum

--- three-sum/threeSum.js
+++ three-sum/threeSum.js
@@ -1,8 +1,29 @@
+function threeSum(array, target) {
+  if (!Array.isArray(array)) {
+    throw TypeError('Expected array as first argument, found ' + typeof array);
+  }
 
-function threeSum(...args) {
-  return args;
+  if (array.length < 3) {
+    throw Error('The array should contain atleast three numbers');
+  }
+
+  if (typeof target !== 'number') {
+    throw TypeError(
+      'Second argument should be of type number, received  ' + typeof input
+    );
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      for (let k = i + 2; k < array.length; k++) {
+        if (array[i] + array[j] + array[k] === target) {
+          return [array[i], array[j], array[k]];
+        }
+      }
+    }
+  }
+
+  return null;
 }
 
-export {
-  threeSum,
-};
+export { threeSum };
