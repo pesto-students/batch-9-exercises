f304b433945405c5ac4ea87d7be64c1c7a50b4ee
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-21 17:47:44 +0530

    Solve smallestCommons

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,6 +1,22 @@
 
-function smallestCommons(...args) {
-  return args;
+function getGCD(num1, num2) {
+  return num2 === 0 ? num1 : getGCD(num2, num1 % num2);
+}
+
+function getLCM(num1, num2) {
+  return (num1 * num2) / getGCD(num1, num2);
+}
+function smallestCommons(range) {
+  let [lowerBound, upperBound] = range;
+  if (lowerBound > upperBound) {
+    [lowerBound, upperBound] = [upperBound, lowerBound];
+  }
+  let smallestCommonMultiple = lowerBound;
+  while (lowerBound <= upperBound) {
+    smallestCommonMultiple = getLCM(smallestCommonMultiple, lowerBound);
+    lowerBound += 1;
+  }
+  return smallestCommonMultiple;
 }
 
 export {
