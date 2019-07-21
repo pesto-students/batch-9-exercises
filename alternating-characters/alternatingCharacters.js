320ec109e67bd1f5d7d3c703ed52ee3a98c0aa4d
Kanav Arora <arorakanav11@gmail.com>
2019-07-21 07:30:49 +0530

    Solve Alternating Character

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,7 +1,18 @@
+const getAlternatingCharacterCount = chars => chars.reduce((count, char, index) => {
+  const nextIndex = index + 1;
+  if (char === chars[nextIndex]) {
+    return count + 1;
+  }
+  return count;
+}, 0);
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(strings) {
+  return strings.map((string) => {
+    const chars = string.split('');
+    return getAlternatingCharacterCount(chars);
+  });
 }
+
 
 export {
   alternatingCharacters,
