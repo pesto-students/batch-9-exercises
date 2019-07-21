07c7f31279a863dc90aa36e1102b39e83247b540
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-21 06:27:54 +0530

    [WIP] Complete the cache function.

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -3,10 +3,12 @@ function cacheFunction(randomFn) {
   const memomize = {
 
   };
-  function callback(args) {
-    if (!memomize[String(args)]) { memomize[String(args)] = randomFn(args); }
-    return memomize[String(args)];
-  }
+  const callback = (args) => {
+    if (!(Object.prototype.hasOwnProperty.call(memomize, args))) {
+      memomize[args] = randomFn(args);
+    }
+    return memomize[args];
+  };
   return callback;
 }
 
