501d603766521b0a846195876fafe3a0669ec743
jayamathan <jayamathanskj03@gmail.com>
2019-07-21 23:10:57 +0530

    solve orbitalperiod

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,6 +1,19 @@
+const calculateOrbitalPeriod = (averageAltitude) => {
+  const earthGravity = 398600.4418; // in km3s-2
+  const earthRadius = 6367.4447; // in km
+  const distanceFromCore = earthRadius + averageAltitude;
+  return (2 * Math.PI * (distanceFromCore ** 1.5)) / (earthGravity ** 0.5);
+};
 
-function orbitalPeriod(...args) {
-  return args;
+function orbitalPeriodObj(element) {
+  return {
+    name: element.name,
+    orbitalPeriod: Math.round(calculateOrbitalPeriod(element.avgAlt)),
+  };
+}
+
+function orbitalPeriod(elements) {
+  return elements.map(orbitalPeriodObj);
 }
 
 export {
