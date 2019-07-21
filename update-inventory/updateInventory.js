ec5f37f4b5b0ee1114e3e0a1e816e079ab025966
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-21 17:56:25 +0530

    Solve updateInventory

--- update-inventory/updateInventory.js
+++ update-inventory/updateInventory.js
@@ -1,6 +1,13 @@
 
-function updateInventory(...args) {
-  return args;
+function updateInventory(currentInventory, addedInventory) {
+  const updatedInventoryObject = [...currentInventory, ...addedInventory]
+    .reduce((updatedInventory, [itemQuantity, itemName]) => ({
+      ...updatedInventory,
+      [itemName]: (updatedInventory[itemName] || 0) + itemQuantity,
+    }), {});
+  return Object.keys(updatedInventoryObject)
+    .sort()
+    .map(key => [updatedInventoryObject[key], key]);
 }
 
 export {
