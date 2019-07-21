5bffe07681506482ade5b2aa4b0413fd190ee2ea
jayamathan <jayamathanskj03@gmail.com>
2019-07-21 19:07:31 +0530

    WIP telephone check

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,6 +1,11 @@
 
-function telephoneCheck(...args) {
-  return args;
+function telephoneCheck(string) {
+  let result = false;
+  const telephoneNumber = string.replace(/[^\d]/g, '');
+  if (telephoneNumber.length === 10 || (telephoneNumber[0] === '1')) {
+    result = true;
+  }
+  return result;
 }
 
 export {
