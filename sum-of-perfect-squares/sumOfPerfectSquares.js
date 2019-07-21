b19c19764894bb8af9904b6468655739b97b4689
jayamathan <jayamathanskj03@gmail.com>
2019-07-21 20:20:59 +0530

    solve perfect squares

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,6 +1,18 @@
 
-function sumOfPerfectSquares(...args) {
-  return args;
+function sumOfPerfectSquares(number) {
+  let result = number;
+  if (number <= 3) {
+    return number;
+  }
+  for (let i = 1; i < number + 1; i += 1) {
+    const temp = i * i;
+    if (temp > number) {
+      break;
+    } else {
+      result = Math.min(result, 1 + sumOfPerfectSquares(number - temp));
+    }
+  }
+  return result;
 }
 
 export {
