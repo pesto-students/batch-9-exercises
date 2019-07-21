5cabd05b3fc5eda762d20ec62260952364fe3275
jayamathan <jayamathanskj03@gmail.com>
2019-07-21 21:36:51 +0530

    solve rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,7 +1,26 @@
+const charCodes = stringArray => stringArray.map(char => char.charCodeAt());
 
-function rot13(...args) {
-  return args;
+
+const CHAR_CODE_A = 'A'.charCodeAt();
+const CHAR_CODE_Z = 'Z'.charCodeAt();
+const isAlphabet = charCode => charCode >= CHAR_CODE_A && charCode <= CHAR_CODE_Z;
+const decipherCodesArray = stringArray => charCodes(stringArray)
+  .map((charCode) => {
+    if (isAlphabet(charCode)) {
+      const decipherAlphabetPostion = ((charCode - CHAR_CODE_A + 13) % 26);
+      return CHAR_CODE_A + decipherAlphabetPostion;
+    }
+    return charCode;
+  });
+
+const charArray = codesArray => String.fromCharCode(...codesArray);
+
+function rot13(string) {
+  const decipherCodes = decipherCodesArray(string.split(''));
+
+  return charArray(decipherCodes);
 }
+
 
 export {
   rot13,
