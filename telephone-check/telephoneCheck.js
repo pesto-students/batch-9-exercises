e6e7878636f37c367d81c72d28ab0dcc02642e19
jayamathan <jayamathanskj03@gmail.com>
2019-07-21 19:09:56 +0530

    telephone check

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,11 +1,7 @@
 
-function telephoneCheck(string) {
-  let result = false;
-  const telephoneNumber = string.replace(/[^\d]/g, '');
-  if (telephoneNumber.length === 10 || (telephoneNumber[0] === '1')) {
-    result = true;
-  }
-  return result;
+function telephoneCheck(phoneNumber) {
+  const usaTelephoneNumberRegex = /^(?:\+?1[- ]?)?\(([0-9]{3})\)[- ]?([0-9]{3})[- ]([0-9]{4})$/;
+  return usaTelephoneNumberRegex.test(phoneNumber);
 }
 
 export {
