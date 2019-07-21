575d04e20af54a301ac2f08827b898bd9a9936dc
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-21 06:35:37 +0530

    solve sumAll

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,8 +1,10 @@
-
-function sumAll(...args) {
-  return args;
+function sumAll(array) {
+  const sortedArray = array.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = sortedArray[0]; i <= sortedArray[1]; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
-export {
-  sumAll,
-};
+export { sumAll };
