07449a1ce57c5124d190caf1a12e81cb385c669f
avinash360 <avithakur86@gmail.com>
2019-07-23 00:58:19 +0530

    solve: createStudent test

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,6 +1,20 @@
 
-function createStudent(...args) {
-  return args;
+function createStudent(likes = { likesES2015: true, likesJavaScript: true }) {
+  const { likesES2015, likesJavaScript } = likes;
+  if (likes.hasOwnProperty('likesES2015') && likes.hasOwnProperty('likesJavaScript')) {
+    if (likesES2015 && likesJavaScript) {
+      return 'The student likes JavaScript and ES2015';
+    }
+  } else {
+    if (likes.hasOwnProperty('likesES2015') && !likesES2015) {
+      return 'The student likes JavaScript!';
+    }
+
+    if (likes.hasOwnProperty('likesJavaScript') && !likesJavaScript) {
+      return 'The student likes ES2015!';
+    }
+  }
+  return 'The student does not like much...';
 }
 
 export {
