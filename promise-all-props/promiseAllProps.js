9e96cbdb656b3433789bf9e6b23ad9e6cb216aaf
Kanav Arora <arorakanav11@gmail.com>
2019-07-23 04:55:31 +0530

    Solve Promise All Props

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,8 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(obj) {
+  const keys = Object.getOwnPropertyNames(obj);
+  const values = keys.map(key => obj[key]);
+  return Promise.all(...values).then(obj);
 }
 
 export {
