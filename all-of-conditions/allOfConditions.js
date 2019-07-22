9a4df546c47d96057ccd198f021cf143cb09d7e6
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-23 00:36:23 +0530

    pass two test for allOfConditions

--- all-of-conditions/allOfConditions.js
+++ all-of-conditions/allOfConditions.js
@@ -1,6 +1,18 @@
 
-function allOfConditions(...args) {
-  return args;
+function allOfConditions(...callbacks) {
+  const [firstFunction, secondFunction] = callbacks;
+  return (...args) => {
+    new Promise((resolve, reject) => {
+      const result = firstFunction(args);
+      if (result) {
+        resolve();
+      } else {
+        reject(new Error('Promise rejected.'));
+      }
+    }).then(() => secondFunction(...args))
+      // eslint-disable-next-line no-console
+      .catch(err => console.error(err));
+  };
 }
 
 export {
