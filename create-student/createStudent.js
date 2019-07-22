4a48dc527019d8ad2e565e336827d71f1a7cab05
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-23 00:54:03 +0530

    solve createStudent

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,8 +1,42 @@
+// const defaultObject = {
+//   likesES2015: true,
+//   likesJavaScript: true
+// };
+function createStudent({ likesES2015 = true, likesJavaScript = true } = {}) {
+  let outputString = '';
+  if (likesES2015 && likesJavaScript) {
+    outputString = 'The student likes JavaScript and ES2015';
+  }
 
-function createStudent(...args) {
-  return args;
+  if (likesJavaScript && likesES2015 === false) {
+    outputString = 'The student likes JavaScript!';
+  }
+
+  if (likesJavaScript === false && likesES2015) {
+    outputString = 'The student likes ES2015!';
+  }
+
+  if (likesJavaScript === false && likesES2015 === false) {
+    outputString = 'The student does not like much...';
+  }
+  return outputString;
+  // const object = { ...defaultObject, ...obj };
+  // let outputString = '';
+  // if (object.likesES2015 && object.likesJavaScript) {
+  //   outputString = 'The student likes JavaScript and ES2015';
+  // }
+
+  // if (object.likesJavaScript && object.likesES2015 === false) {
+  //   outputString = 'The student likes JavaScript!';
+  // }
+
+  // if (object.likesJavaScript === false && object.likesES2015) {
+  //   outputString = 'The student likes ES2015!';
+  // }
+
+  // if (object.likesJavaScript === false && object.likesES2015 === false) {
+  //   outputString = 'The student does not like much...';
+  // }
 }
 
-export {
-  createStudent,
-};
+export { createStudent };
