504b2ac76a3ff2bca183f67f13da9ccb2f080c6f
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-23 04:22:42 +0530

    [WIP]Complete triple-and-filter

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,6 +1,10 @@
+const applyGivenConditionFn = condition => array => () => array.map(value => condition(value));
 
-function tripleAndFilter(...args) {
-  return args;
+function tripleAndFilter(array) {
+  const isDivisibleBy5 = applyGivenConditionFn(x => x % 5 === 0);
+  const tripleTheFunction = applyGivenConditionFn(x => x * 3);
+  const tripleDivisibleBy5 = isDivisibleBy5(tripleTheFunction(array)());
+  return tripleDivisibleBy5();
 }
 
 export {
