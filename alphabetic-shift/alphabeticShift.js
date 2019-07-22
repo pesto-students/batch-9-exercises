24347eaea553796bacfc17e49b48ad724dcad1f6
jayamathan <jayamathanskj03@gmail.com>
2019-07-23 04:30:03 +0530

    solve alphabeticShift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,12 @@
+function alphabeticShift(string) {
+  let result = '';
+  let charcode = 0;
 
-function alphabeticShift(...args) {
-  return args;
+  for (let i = 0; i < string.length; i += 1) {
+    charcode = (string[i].charCodeAt()) + 1;
+    result += String.fromCharCode(charcode);
+  }
+  return result;
 }
 
 export {
