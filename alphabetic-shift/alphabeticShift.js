38448bc2d692bb45887b08ef8493434f9fc67247
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-23 04:47:21 +0530

    Complete alphabetical-shift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,21 @@
+const asciiCode = letter => letter.charCodeAt(0);
 
-function alphabeticShift(...args) {
-  return args;
+const nextLetter = (letter) => {
+  let nextLetterAscii = 0;
+  if (asciiCode(letter) === 90) {
+    nextLetterAscii = 65;
+  } else if (asciiCode(letter) === 122) {
+    nextLetterAscii = 97;
+  } else {
+    nextLetterAscii = 1 + letter.charCodeAt(0);
+  }
+  return String.fromCharCode(nextLetterAscii);
+};
+
+function alphabeticShift(string) {
+  const letters = string.split('');
+  const alphabeticShiftedLetters = letters.map(letter => nextLetter(letter));
+  return alphabeticShiftedLetters.join('');
 }
 
 export {
