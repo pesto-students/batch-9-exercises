2fcd7de5b167be716ccb1892871d8030bc0e6403
Rajeet <rajeetdel@gmail.com>
2019-07-23 01:00:10 +0530

    double-odd-number is wip

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,13 @@
 
 function doubleOddNumbers(...args) {
-  return args;
+  function checkOdd(num) {
+    return num%2!=0;
+  }
+
+  return Array.prototype.reduce.call(args.filter(checkOdd), function(a) {
+      return 2*a;
+  }, 0);
+
 }
 
 export {
