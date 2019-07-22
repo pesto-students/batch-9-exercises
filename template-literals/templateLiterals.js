91f2d8ef5db81ff1d040dfcb108780214f99b79f
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-23 01:00:46 +0530

    Complete template-literals

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,23 @@
+const playerNamesString = (namesArray) => {
+  let sentenceString = '';
+  if (!namesArray.length) {
+    sentenceString = 'Rooney, Ronaldo, Messi, Pogba';
+  } else {
+    namesArray.forEach((name) => {
+      sentenceString += `${name}, `;
+    });
+    sentenceString = sentenceString.slice(0, -2);
+  }
+  return sentenceString;
+};
 
-function templateLiterals(...args) {
-  return args;
+function templateLiterals(...names) {
+  let numberOfPlayers = 4;
+  if (names.length) {
+    numberOfPlayers = names.length;
+  }
+  const sentence = `There are ${numberOfPlayers} people on the football team. Their names are ${playerNamesString(names)}.`;
+  return sentence;
 }
 
 export {
