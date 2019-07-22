a41f14f09f6e2de02c688b3cb5a11b90633899f6
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-23 05:03:16 +0530

    Solve alphabetic-shift.

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,18 @@
 
-function alphabeticShift(...args) {
-  return args;
+const shift = (alphabet) => {
+  const charCode = alphabet.charCodeAt();
+  if (charCode === 90) {
+    return String.fromCharCode(65);
+  }
+  if (charCode === 122) {
+    return String.fromCharCode(97);
+  }
+  return String.fromCharCode(alphabet.charCodeAt() + 1);
+};
+
+function alphabeticShift(string) {
+  const alphabets = string.split('');
+  return alphabets.map(shift).join('');
 }
 
 export {
