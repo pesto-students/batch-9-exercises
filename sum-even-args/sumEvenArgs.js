8fbb1a1e4ee2558e2a5a1ee9d37619a1090e5e61
Kanav Arora <arorakanav11@gmail.com>
2019-07-23 00:11:24 +0530

    Solve Sum Even Args

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,12 @@
+const isEven = number => number % 2 === 0;
+const filterEven = numbers => numbers.filter(isEven);
+const sum = numbers => numbers.reduce((result, num) => result + num, 0);
+
+const compose = (...fns) => arg => fns.reduce((result, fn) => fn(result), arg);
 
 function sumEvenArgs(...args) {
-  return args;
+  const composed = compose(filterEven, sum);
+  return composed(args);
 }
 
 export {
