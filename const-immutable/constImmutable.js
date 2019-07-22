6bccb528bba3720afdec3ca94c373fb900b3dd1e
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-23 00:15:22 +0530

    [WIP] Partially Solve the Const Immutable issue.

--- const-immutable/constImmutable.js
+++ const-immutable/constImmutable.js
@@ -1,6 +1,16 @@
-
-function constImmutable(...args) {
-  return args;
+function makeObjectPropertyImmutable(property, value) {
+  Object.defineProperty(this, [property], {
+    value,
+    writable: false,
+  });
+}
+function constImmutable() {
+  const account = {
+    username: 'pesto',
+  };
+  makeObjectPropertyImmutable.call(account, 'password', 'initialPassword');
+  account.password = 's3cret';
+  return account.password;
 }
 
 export {
