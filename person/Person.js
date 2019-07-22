56f138da19b6863aa096404cb63d152fa1602007
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-23 05:15:15 +0530

    [WIP] Complete the Person problem.

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,20 @@
 
-function person(...args) {
-  return args;
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  addDobDigits() {
+    const dobArray = this.dateOfBirth.split('/');
+    const digitArrayInString = dobArray.join('').split('');
+    // eslint-disable-next-line max-len
+    const addedSum = digitArrayInString.reduce((accumulator, numberInString) => accumulator + parseInt(numberInString, 10), 0);
+    return addedSum;
+  }
 }
 
 export {
-  person,
+  Person,
 };
