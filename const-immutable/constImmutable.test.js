cf287ed29f3f2b66012be6f7e66040cd2c1112ba
avinash360 <avithakur86@gmail.com>
2019-07-23 00:36:37 +0530

    solve: constImmutable test

--- const-immutable/constImmutable.test.js
+++ const-immutable/constImmutable.test.js
@@ -1,7 +1,13 @@
 import { constImmutable } from './constImmutable';
 
+const account = {
+  username: 'pesto',
+  password: 'initialPassword',
+};
+
 describe('constImmutable', () => {
   test('const object should have immutable properties', () => {
-    expect(constImmutable()).toBe('initialPassword');
+    expect(Object.isFrozen(constImmutable())).toBe(true);
+    expect((constImmutable(account))).toEqual(account);
   });
 });
