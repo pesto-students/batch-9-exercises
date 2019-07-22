dd69a21074f6c9b1a656fa5173488d6135a6fd1b
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-23 02:10:59 +0530

    [WIP] Complete sum even args and add 1 test case.

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,14 @@
-
-function sumEvenArgs(...args) {
-  return args;
+function isEvenNumber(num) {
+  return (num % 2) === 0;
+}
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+function sumEvenArgs(...numbers) {
+  const initialSum = 0;
+  const evenIndexNumbers = numbers.filter((number, index) => isEvenNumber(index + 1));
+  const requiredSum = evenIndexNumbers.reduce(addNumbers, initialSum);
+  return requiredSum;
 }
 
 export {
