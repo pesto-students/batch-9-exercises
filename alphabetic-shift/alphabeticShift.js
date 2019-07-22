76af8884cb6b44e3004e830a64bde8cf590dac21
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-23 04:41:32 +0530

    solve aplhabeticShift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,8 +1,22 @@
+const charCodeOfZ = 90;
+const charCodeOfz = 122;
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(string) {
+  return string
+    .split('')
+    .map(character => {
+      let charCode = character.charCodeAt(0);
+      let incrementedCharCode = 0;
+      if (charCode === charCodeOfz) {
+        incrementedCharCode = 97;
+      } else if (charCode === charCodeOfZ) {
+        incrementedCharCode = 65;
+      } else {
+        incrementedCharCode = charCode + 1;
+      }
+      return String.fromCharCode(incrementedCharCode);
+    })
+    .join('');
 }
 
-export {
-  alphabeticShift,
-};
+export { alphabeticShift };
