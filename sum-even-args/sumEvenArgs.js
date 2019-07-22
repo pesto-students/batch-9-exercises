6ae70cfaec66a84fd5b5495dd7dfd2ef9e3d9dbd
jayamathan <jayamathanskj03@gmail.com>
2019-07-23 00:04:52 +0530

    solve sum-even-args

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,5 +1,13 @@
-function sumEvenArgs(...args) {
-  return args;
+const isEven = number => number % 2 === 0;
+
+function sumEvenArgs(...numbers) {
+  let result = 0;
+  for (const number of numbers) {
+    if (isEven(number)) {
+      result += number;
+    }
+  }
+  return result;
 }
 
 export {
