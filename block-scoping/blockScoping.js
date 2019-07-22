392fc158979c80e47f63504b12bcb222eac9a24d
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-23 00:05:28 +0530

    [WIP] Complete Day 6 Segment 1.

--- block-scoping/blockScoping.js
+++ block-scoping/blockScoping.js
@@ -1,6 +1,15 @@
 
-function blockScoping(...args) {
-  return args;
+function blockScoping(n) {
+  const callbacks = [];
+  switch (arguments.length) {
+    case 1:
+      for (let i = 0; i <= 10; i += 1) {
+        callbacks.push(() => i);
+      }
+      return callbacks[n]();
+
+    default: throw new Error(`Expected Number of Arguments:0 \n Received number of argumnets:${arguments.length}`);
+  }
 }
 
 export {
