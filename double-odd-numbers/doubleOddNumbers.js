388a14f17d85d27869b6ee063e322be36de55b55
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-23 01:53:48 +0530

    Solve double odd numbers.

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,12 @@
 
-function doubleOddNumbers(...args) {
-  return args;
+const isOdd = number => number % 2 === 1;
+
+const oddNumbers = array => array.filter(isOdd);
+
+const double = number => number * 2;
+
+function doubleOddNumbers(array) {
+  return oddNumbers(array).map(double);
 }
 
 export {
