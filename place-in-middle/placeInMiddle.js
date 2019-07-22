e1603976fd0b93d559450af4c635c9b5153772fc
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-23 00:34:57 +0530

    Solve place-in-middle.

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,9 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(originalArray, arrayToPush) {
+  const sliceIndex = Math.floor(originalArray.length / 2);
+  const slice1 = originalArray.slice(0, sliceIndex);
+  const slice2 = originalArray.slice(sliceIndex);
+  return [...slice1, ...arrayToPush, ...slice2];
 }
 
 export {
