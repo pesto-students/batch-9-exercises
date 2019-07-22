168ab7b64e9ca4bd973f4da3386e1f1efc108d1e
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-23 02:07:35 +0530

    Solve create-student.

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,6 +1,16 @@
 
-function createStudent(...args) {
-  return args;
+function createStudent(object = {}) {
+  const { likesES2015 = true, likesJavaScript = true } = object;
+  if (likesES2015 && likesJavaScript) {
+    return 'The student likes JavaScript and ES2015';
+  }
+  if (likesES2015) {
+    return 'The student likes ES2015!';
+  }
+  if (likesJavaScript) {
+    return 'The student likes JavaScript!';
+  }
+  return 'The student does not like much...';
 }
 
 export {
