aad6edc521aa3d1cc7c2443f961013ddc889c569
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-23 01:08:05 +0530

    Solve doubleOddNumbers

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,15 @@
+const doubleNumbers = number => number * 2;
 
-function doubleOddNumbers(...args) {
-  return args;
+function splat(fn) {
+  return list => Array.prototype.map.call(list, fn);
+}
+
+// eslint-disable-next-line no-bitwise
+const filterOddNumbers = list => list.filter(num => num & 1);
+
+function doubleOddNumbers(list) {
+  const oddNumbers = filterOddNumbers(list);
+  return splat(doubleNumbers)(oddNumbers);
 }
 
 export {
