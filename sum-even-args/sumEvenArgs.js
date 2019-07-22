d402aba4de84f57b91407261defafa08ff6bf68b
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-23 00:11:25 +0530

    Complete sum-even-args

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,16 @@
+const sumFn = condition => (array) => {
+  let sumValue = 0;
+  array.forEach((value) => {
+    if (condition(value)) {
+      sumValue += value;
+    }
+  });
+  return sumValue;
+};
 
 function sumEvenArgs(...args) {
-  return args;
+  const sumEven = sumFn(x => x % 2 === 0);
+  return sumEven(args);
 }
 
 export {
