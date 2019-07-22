5a23c91dc8cd603d00f7c1df7b7801d6aa975b89
Rajeet <rajeetdel@gmail.com>
2019-07-23 00:51:26 +0530

    Sum-Even-Args is done

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,14 @@
 
 function sumEvenArgs(...args) {
-  return args;
+
+  function checkEven(num) {
+    return num%2===0;
+  }
+
+  return Array.prototype.reduce.call(args.filter(checkEven), function(a, b) {
+      return (a + b);
+  }, 0);
+
 }
 
 export {
