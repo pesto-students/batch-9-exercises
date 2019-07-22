0af3a2c434545b84977b6dc00f25b3a63e8314fa
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-23 00:36:43 +0530

    pass two test for bind

--- bind/bind.js
+++ bind/bind.js
@@ -1,6 +1,13 @@
 
-function bind(...args) {
-  return args;
+function bind(fn, person, args) {
+  const bindFn = (...curriedArgs) => {
+    if (!args) {
+      // eslint-disable-next-line no-param-reassign
+      args = curriedArgs;
+    }
+    return fn.call(person, args);
+  };
+  return bindFn;
 }
 
 export {
