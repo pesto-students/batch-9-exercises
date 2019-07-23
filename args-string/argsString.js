3b99d3183e137ccbdbe06d26ebc259082a50585d
jayamathan <jayamathanskj03@gmail.com>
2019-07-24 04:53:34 +0530

    WIP argsString

--- args-string/argsString.js
+++ args-string/argsString.js
@@ -1,8 +1,16 @@
 
-function argsString(...args) {
-  return args;
+function argsString(string, array) {
+  let index = 0;
+  let sentence = ' ';
+  const splitString = string.split(' ');
+  for (const str of splitString) {
+    if (str.includes('{}')) {
+      sentence += str.replace('{}', array[index]);
+      index += 1;
+    } else sentence += str;
+  }
+  return sentence;
 }
-
 export {
   argsString,
 };
