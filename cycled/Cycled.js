54e7ec75f2133f7770fe6039578c67d2cfc2880f
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-24 01:00:27 +0530

    [WIP] Partially Solve Cycled and range Iterator.

--- cycled/Cycled.js
+++ cycled/Cycled.js
@@ -1,8 +1,22 @@
 
-function cycled(...args) {
-  return args;
+function Cycled(array) {
+  this.array = array;
+  this.nextIndex = 0;
+  this[Symbol.iterator] = () => this;
 }
+Cycled.prototype.next = () => {
+  const nextValue = this.array[this.nextIndex];
+  this.nextIndex = (this.nextIndex + 1) % this.array.length;
+  return nextValue;
+};
+
+Cycled.prototype.previous = () => {
+  const previousIndex = (this.nextIndex - 1) % this.array.length;
+  const previousValue = this.array[previousIndex];
+  this.nextIndex = previousIndex;
+  return previousValue;
+};
 
 export {
-  cycled,
+  Cycled,
 };
--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,6 +1,13 @@
 
-function rangeIter(...args) {
-  return args;
+function* rangeIter(lowerBound = Number.MIN_SAFE_INTEGER, upperBound = Number.MAX_SAFE_INTEGER) {
+  let copyOfLowerBound = lowerBound;
+  while (true) {
+    const hasASafeNextValue = Number.isSafeInteger(copyOfLowerBound) && copyOfLowerBound < upperBound;
+    if (hasASafeNextValue) {
+      yield copyOfLowerBound;
+      copyOfLowerBound += 1;
+    }
+  }
 }
 
 export {
