29c31d8f87aa170130eab8b4c013e26c6e3a40b7
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-24 01:12:26 +0530

    [WIP] Partially solve range iterator problem.

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,15 +1,23 @@
-
+const checkIfNumber = num => typeof num === 'number';
 function* rangeIter(lowerBound = Number.MIN_SAFE_INTEGER, upperBound = Number.MAX_SAFE_INTEGER) {
+  if (!checkIfNumber(lowerBound)) {
+    throw new Error(`${lowerBound} is not a number`);
+  }
+  if (!checkIfNumber(lowerBound)) {
+    throw new Error(`${lowerBound} is not a number`);
+  }
   let copyOfLowerBound = lowerBound;
   while (true) {
-    const hasASafeNextValue = Number.isSafeInteger(copyOfLowerBound) && copyOfLowerBound < upperBound;
+    const hasASafeNextValue = Number.isSafeInteger(copyOfLowerBound) && copyOfLowerBound <= upperBound;
     if (hasASafeNextValue) {
       yield copyOfLowerBound;
       copyOfLowerBound += 1;
+    } else {
+      return;
     }
   }
 }
-
-export {
-  rangeIter,
-};
+rangeIter();
+// export {
+//   rangeIter,
+// };
