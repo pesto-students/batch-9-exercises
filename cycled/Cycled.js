51fdcafdd4d41fa0ed9c3ce989b128971cd6f102
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-24 01:00:04 +0530

    [WIP] Solve cycled.

--- cycled/Cycled.js
+++ cycled/Cycled.js
@@ -1,8 +1,34 @@
 
-function cycled(...args) {
-  return args;
+function Cycled(array) {
+  let index = 0;
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      index += 1;
+      if (index < 3) {
+        return array[index];
+        // eslint-disable-next-line no-unreachable
+        index += 1;
+      }
+      index = 0;
+      return array[index];
+    },
+    current() {
+      return array[index];
+    },
+    previous() {
+      return array[index];
+    },
+    step(_step) {
+      index += _step;
+      return this.current();
+    },
+    index,
+  };
 }
 
 export {
-  cycled,
+  Cycled,
 };
