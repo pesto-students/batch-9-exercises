2a46f80783bb044612e39222b21ce3c918ca8458
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-24 04:43:15 +0530

    [WIP] Complete Day 7 Segmnet 2

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,5 +1,11 @@
+const isBigger = (a, b) => a > b;
+const sort = compareFn => list => list.sort(compareFn);
+const sortAsc = sort(isBigger);
+const compareAnimalPart = partToCompare => predicate => (animal1, animal2) => predicate(animal1[partToCompare], animal2[partToCompare]);
+const compareNumberOfLegs = compareAnimalPart('numberOfLegs');
+const hasMoreLegs = compareNumberOfLegs(isBigger);
+function animalSort(animals) {
 
-function animalSort(...args) {
   return args;
 }
 
