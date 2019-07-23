4737a7171f59bbcc3b8ce691a1879f6fa08416b8
Kanav Arora <arorakanav11@gmail.com>
2019-07-24 01:20:09 +0530

    Solve Fibonacci Iterator

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,19 @@
-
-function fibonacciIter(...args) {
-  return args;
+function* fibonacci() {
+  let firstNumber = 1;
+  let secondNumber = 2;
+  yield firstNumber;
+  yield secondNumber;
+  while (true) {
+    const next = firstNumber + secondNumber;
+    firstNumber = secondNumber;
+    secondNumber = next;
+    yield next;
+  }
 }
+
+const fibonacciIter = {
+  [Symbol.iterator]: fibonacci,
+};
 
 export {
   fibonacciIter,
