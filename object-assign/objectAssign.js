e65a6b1ff0988768a928be217fba9ab5c3b5e494
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-24 04:49:07 +0530

    [WIP] Solve object-assign.

--- object-assign/objectAssign.js
+++ object-assign/objectAssign.js
@@ -1,6 +1,11 @@
 
-function objectAssign(...args) {
-  return args;
+function objectAssign(firstObject, ...objects) {
+  // eslint-disable-next-line no-param-reassign
+  firstObject = objects.reduce((accumulator, object) => ({
+    ...accumulator,
+    ...object,
+  }), firstObject);
+  return firstObject;
 }
 
 export {
