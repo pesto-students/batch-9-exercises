9c6142d7f06a5a5677ba3426837e3f233a462c9c
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-24 00:52:02 +0530

    [WIP] Partially Complete the fibnocaii iter problem.

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,18 @@
 
-function fibonacciIter(...args) {
-  return args;
-}
+const fibonacciIter = {
+  fibNMinus1: 0,
+  fibN: 1,
+  [Symbol.iterator]() {
+    return this;
+  },
+  next() {
+    const fibOfNPlusOne = this.fibN + this.fibNMinus1;
+    this.fibN = fibOfNPlusOne;
+    this.fibNMinus1 = this.fibN;
+    return { value: fibOfNPlusOne, done: false };
+  },
+
+};
 
 export {
   fibonacciIter,
