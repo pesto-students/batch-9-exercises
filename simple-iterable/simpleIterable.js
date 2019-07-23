8b68b29148e466f613d8392b9f5881e54fd48476
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-24 00:52:49 +0530

    Complete simple-iterable

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,6 +1,26 @@
 
-function simpleIterable(...args) {
-  return args;
+function simpleIterable() {
+  let i = 0;
+  const iterable = {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          i += 1;
+          if (i < 6) {
+            return {
+              value: i,
+              done: false,
+            };
+          }
+          return {
+            value: undefined,
+            done: true,
+          };
+        },
+      };
+    },
+  };
+  return iterable;
 }
 
 export {
