114745aeca113c8f4716f0f8c86d44ffebfe6c70
Rajeet <rajeetdel@gmail.com>
2019-07-24 04:14:10 +0530

    A

--- generator-iterable/generatorIterable.js
+++ generator-iterable/generatorIterable.js
@@ -1,6 +1,22 @@
 
-function generatorIterable(...args) {
+function generatorIterable(args) {
+
+  var myIterable = {
+    *[Symbol.iterator]() {
+        yield 1;
+        yield 2;
+        yield 3;
+        yield 4;
+        yield 5;
+       // yield 6;
+    }
+  }
+  //myIterable().next();
+
+
+  args = myIterable ;
   return args;
+
 }
 
 export {
