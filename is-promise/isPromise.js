e911e9a8c31553860ad39e2770032c8639d99449
Rajeet <rajeetdel@gmail.com>
2019-07-24 00:07:40 +0530

    A

--- is-promise/isPromise.js
+++ is-promise/isPromise.js
@@ -1,8 +1,11 @@
 
-function isPromise(...args) {
-  return args;
-}
+function isPromise(args) {
 
+  //return Promise.resolve(args)===Promise.resolve(1);
+
+  return Promise.resolve(1);
+
+}
 export {
   isPromise,
 };
--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,18 @@
 
-function person(...args) {
-  return args;
+  class Person {
+    constructor(firstName, lastName, dateOfBirth) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.dateOfBirth = dateOfBirth;
+  }
+
+  addDobDigits() {
+    return `${this.name} says hello.`;
+  }
 }
 
+  const empl = new Person('Nishant', 'Salhotra', '5/11/1994');
+
 export {
-  person,
+  Person,
 };
