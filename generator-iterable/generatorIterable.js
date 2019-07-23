8b7ccaa40b7a5a23fa1d98ed72f273fbc3a38a9c
Kanav Arora <arorakanav11@gmail.com>
2019-07-24 00:59:20 +0530

    Solve Generator Iterable

--- generator-iterable/generatorIterable.js
+++ generator-iterable/generatorIterable.js
@@ -1,6 +1,14 @@
 
-function generatorIterable(...args) {
-  return args;
+function* generatorIterable(lb = 1, ub = 5) {
+  let currentCount = lb;
+  while (true) {
+    if (currentCount <= ub) {
+      yield currentCount;
+    } else {
+      return;
+    }
+    currentCount += 1;
+  }
 }
 
 export {
