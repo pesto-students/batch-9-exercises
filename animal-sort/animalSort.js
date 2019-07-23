630ddcdd41af589b3902bc6cd69c73e9fa5d8686
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-24 04:51:07 +0530

    [WIP] Partially complete the animal sort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,12 +1,12 @@
+/* eslint-disable max-len */
 const isBigger = (a, b) => a > b;
 const sort = compareFn => list => list.sort(compareFn);
-const sortAsc = sort(isBigger);
 const compareAnimalPart = partToCompare => predicate => (animal1, animal2) => predicate(animal1[partToCompare], animal2[partToCompare]);
 const compareNumberOfLegs = compareAnimalPart('numberOfLegs');
 const hasMoreLegs = compareNumberOfLegs(isBigger);
+const sortAnimalsWithMoreLegs = sort(hasMoreLegs);
 function animalSort(animals) {
-
-  return args;
+  return sortAnimalsWithMoreLegs(animals);
 }
 
 export {
