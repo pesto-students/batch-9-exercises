b828b583174345329aa85f14fb47569477f333f5
Kanav Arora <arorakanav11@gmail.com>
2019-07-23 09:53:09 +0530

    Solve Sleep

--- sleep-promise/sleepPromise.js
+++ sleep-promise/sleepPromise.js
@@ -1,8 +1,20 @@
 
-function sleepPromise(...args) {
-  return args;
+function sleep(ms) {
+  const sleepPromise = new Promise((response) => {
+    setTimeout(() => {
+      response();
+    }, ms);
+  });
+
+  const promiseFunction = value => sleepPromise.then(() => value);
+
+  // overiging then
+  promiseFunction.then = (...args) => sleepPromise.then(...args);
+
+
+  return promiseFunction;
 }
 
 export {
-  sleepPromise,
+  sleep,
 };
