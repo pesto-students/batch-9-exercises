310ff0ef606414e18010be24691dda6379a72d58
avinash360 <avithakur86@gmail.com>
2019-07-25 00:33:27 +0530

    solve: knownProp test

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,6 +1,15 @@
 
-function knownProp(...args) {
-  return args;
+function knownProp(target) {
+  const proxy = new Proxy(target, {
+    get(obj, prop) {
+      if (obj.hasOwnProperty(prop)) {
+        return obj[prop];
+      }
+      throw TypeError('Unknown property');
+    },
+  });
+
+  return proxy;
 }
 
 export {
