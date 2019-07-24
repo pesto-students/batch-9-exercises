280e9990c816aad9ba00eb2c970e25fcbf85ab6c
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-25 01:17:56 +0530

    [WIP] Complete the negative index problem.

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,24 +1,38 @@
 /* eslint-disable prefer-rest-params */
-
+const isNumber = (value) => {
+  if (typeof value === 'symbol') {
+    return false;
+  }
+  return !Number.isNaN(parseInt(value, 10));
+};
 function negativeIndex(iterableObj) {
   if (!Array.isArray(iterableObj)) {
     throw TypeError(`Only arrays are supported. Received Type:${typeof iterableObj}`);
   }
   const handler = {
     get(target, key) {
-      if (key < 0) {
-        const indexRequested = parseInt(key, 10);
-        const expectedKey = target.length + indexRequested;
-        return target[expectedKey];
+      if (isNumber(key)) {
+        if (key < 0) {
+          const indexRequested = parseInt(key, 10);
+          const expectedKey = target.length + indexRequested;
+          return target[expectedKey];
+        }
+        return target[key];
       }
 
       return target[key];
     },
     // eslint-disable-next-line consistent-return
-    set(target, key) {
-      if (key > -1) {
-        return Reflect.set(...arguments);
+    set(target, key, value) {
+      if (isNumber(key)) {
+        if (key > -1) {
+          return Reflect.set(...arguments);
+        }
+        const indexRequested = parseInt(key, 10);
+        const expectedKey = target.length + indexRequested;
+        return Reflect.set(target, expectedKey, value);
       }
+      return target;
     },
   };
   const proxyIterableObj = new Proxy(iterableObj, handler);
