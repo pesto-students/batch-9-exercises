8def4618e41fb7330e27a0c3facf54056aa5cb42
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-24 09:05:09 +0530

    [WIP] Solve the curry problem.

--- curry/curry.js
+++ curry/curry.js
@@ -1,6 +1,14 @@
 
-function curry(...args) {
-  return args;
+function curry(Fn, ...args) {
+  const allArgumentsSupplied = Fn.length === args.length;
+
+  if (allArgumentsSupplied) {
+    const finalResult = Fn(...args);
+    return finalResult;
+  }
+
+  const curriedFn = (...waitingArgs) => curry(Fn, ...args, ...waitingArgs);
+  return curriedFn;
 }
 
 export {
