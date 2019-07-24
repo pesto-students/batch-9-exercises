9689ef6e71803e403b5a98acf0cae8ec641fe7d8
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-25 01:00:13 +0530

    [WIP] Complete the Values in problem.

--- values-in/valuesIn.js
+++ values-in/valuesIn.js
@@ -1,6 +1,9 @@
 
-function valuesIn(...args) {
-  return args;
+function valuesIn(obj) {
+  const resultMap = Object.getOwnPropertyNames(obj).map(key => obj[key]);
+  const protoTypeObj = Object.getPrototypeOf(obj);
+  Object.keys(protoTypeObj).forEach(value => resultMap.push(protoTypeObj[value]));
+  return resultMap;
 }
 
 export {
