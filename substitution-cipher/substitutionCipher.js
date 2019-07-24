bc3042a24ee697df1b15b91577e466e706c508a9
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-25 05:05:33 +0530

    Solve substitutionCipher

--- substitution-cipher/substitutionCipher.js
+++ substitution-cipher/substitutionCipher.js
@@ -1,6 +1,27 @@
+const isUpperCase = charCode => charCode >= 97 && charCode <= 123;
+const isLowerCase = charCode => charCode >= 65 && charCode <= 91;
 
-function substitutionCipher(...args) {
-  return args;
+const isAlphabet = (char) => {
+  const charCodePoint = char.codePointAt(0);
+  if (isUpperCase(charCodePoint) || isLowerCase(charCodePoint)) {
+    return true;
+  }
+  return false;
+};
+
+function substitutionCipher(cipher) {
+  let flag = 0;
+  return [...cipher].map((char) => {
+    if (isAlphabet(char)) {
+      if (flag === 0) {
+        flag = 1;
+        return String.fromCodePoint(char.codePointAt(0) + 4);
+      }
+      flag = 0;
+      return String.fromCodePoint(char.codePointAt(0) + 6);
+    }
+    return char;
+  }).join('');
 }
 
 export {
