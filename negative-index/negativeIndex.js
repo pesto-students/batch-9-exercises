432b348f03f5549011b2ec91fcde478d4d437c0e
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-25 00:56:51 +0530

    [WIP] Solve negative-index.

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,31 @@
 
-function negativeIndex(...args) {
-  return args;
+/* eslint-disable no-return-assign */
+/* eslint-disable no-param-reassign */
+
+function negativeIndex(array) {
+  if (!Array.isArray(array)) {
+    throw TypeError('Only arrays are supported');
+  }
+  const proxy = new Proxy(array, {
+    get(target, index) {
+      if (index === 'length') {
+        return target.length;
+      }
+      if (index === 'toString') {
+        return Array.toString(target);
+      }
+      index = parseInt(index, 10);
+      if (index < 0) {
+        index += target.length;
+      }
+      return target[index];
+    },
+    set(target, index, value) {
+      target[index] = value;
+      return true;
+    },
+  });
+  return proxy;
 }
 
 export {
