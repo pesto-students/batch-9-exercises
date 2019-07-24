da0e579f4cdf7ca207d8a32c2cd58c5cfd9d6446
Kanav Arora <arorakanav11@gmail.com>
2019-07-25 00:18:08 +0530

    solve Known Prop

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,6 +1,18 @@
 
-function knownProp(...args) {
-  return args;
+function knownProp(targer) {
+  const handler = {
+    get(object, property) {
+      // eslint-disable-next-line no-prototype-builtins
+      if (object.hasOwnProperty(property)) {
+        return object[property];
+      }
+      throw TypeError(`${property} unknown`);
+    },
+  };
+
+  const proxy = new Proxy(targer, handler);
+
+  return proxy;
 }
 
 export {
