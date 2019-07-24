ed4becb37c4c77930034cce97ca5e6cf78a44d96
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-25 04:37:16 +0530

    local commit

--- values/values.js
+++ values/values.js
@@ -1,8 +1,35 @@
-
-function values(...args) {
-  return args;
+function values(object) {
+  const enumerableValues = [];
+  for (let key in object) {
+    if (Array.isArray(object)) {
+      enumerableValues.push(object[key]);
+      return enumerableValues;
+    } else if (object.hasOwnProperty(key)) {
+      enumerableValues.push(object[key]);
+    }
+  }
+  return enumerableValues;
 }
 
-export {
-  values,
+export { values };
+const obj = {
+  a: 100,
+  b: [1, 2, 3],
+  c: { x: 200, y: 300 },
+  d: 'D',
+  e: null,
+  f: undefined
 };
+console.log(values(obj).sort());
+
+class C {
+  constructor() {
+    this.a = 100;
+    this.b = 200;
+  }
+}
+C.prototype.x = () => 'x';
+C.prototype.y = 'y';
+const cobj = new C();
+
+console.log(values(cobj));
