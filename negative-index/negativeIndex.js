0c0856668e22b5653070c221fdd19d27ef84e4ee
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-25 00:43:49 +0530

    [WIP] Partially Solve onchange and negative index problem.

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,28 @@
+/* eslint-disable prefer-rest-params */
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(iterableObj) {
+  if (!Array.isArray(iterableObj)) {
+    throw TypeError(`Only arrays are supported. Received Type:${typeof iterableObj}`);
+  }
+  const handler = {
+    get(target, key) {
+      if (key < 0) {
+        const indexRequested = parseInt(key, 10);
+        const expectedKey = target.length + indexRequested;
+        return target[expectedKey];
+      }
+
+      return target[key];
+    },
+    // eslint-disable-next-line consistent-return
+    set(target, key) {
+      if (key > -1) {
+        return Reflect.set(...arguments);
+      }
+    },
+  };
+  const proxyIterableObj = new Proxy(iterableObj, handler);
+  return proxyIterableObj;
 }
 
 export {
--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,6 +1,24 @@
 
-function onChange(...args) {
-  return args;
+function onChange(obj, onChangeFn) {
+  const handler = {
+    set(target, property, value) {
+      onChangeFn();
+      return Reflect.set(target, property, value);
+    },
+    defineProperty() {
+      onChangeFn();
+      return true;
+    },
+    deleteProperty(target, prop) {
+      if (prop in target) {
+        delete target[prop];
+        onChangeFn();
+        return true;
+      }
+    }
+  };
+  const proxyObj = new Proxy(obj, handler);
+  return proxyObj;
 }
 
 export {
