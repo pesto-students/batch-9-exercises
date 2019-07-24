b51640d281973a0b1153742d59bf8581ae3fad32
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-24 22:17:46 +0530

    Complete animal-sort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,18 @@
-
-function animalSort(...args) {
-  return args;
+function animalSort(animalObjects) {
+  return animalObjects.sort((first, second) => {
+    let diff = 0;
+    diff = first.numberOfLegs - second.numberOfLegs;
+    if (diff === 0) {
+      if (first.name < second.name) {
+        return -1;
+      }
+      if (first.name > second.name) {
+        return 1;
+      }
+      return 0;
+    }
+    return diff;
+  });
 }
 
 export {
