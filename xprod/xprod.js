44229c366b6af5069eb202cf3e1d48df91bc2e9d
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-25 00:00:46 +0530

    Solve xprod.

--- xprod/xprod.js
+++ xprod/xprod.js
@@ -1,6 +1,15 @@
 
-function xprod(...args) {
-  return args;
+const isArrayEmpty = arr => arr.length === 0;
+
+function xprod(...arrays) {
+  if (isArrayEmpty(arrays[0]) || isArrayEmpty(arrays[1])) {
+    return [];
+  }
+  const newArray = [];
+  arrays[0].forEach(element => arrays[1].forEach((innerElement) => {
+    newArray.push([element, innerElement]);
+  }));
+  return newArray;
 }
 
 export {
