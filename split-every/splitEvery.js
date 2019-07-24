6bd2a0467dcedb56cac6b8fc46b8b655664ead3e
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-25 05:20:08 +0530

    [WIP] Solve split-every.

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,6 +1,21 @@
 
-function splitEvery(...args) {
-  return args;
+// const isString = element => typeof element === 'string';
+
+function splitEvery(index, array) {
+  if (index === 0 || index === -1) {
+    throw Error('Expected: index > 1, Recieved: < 1');
+  }
+  if (array.length === 0) {
+    return [];
+  }
+  if (array.length <= index) {
+    return [array];
+  }
+  // array logic
+  // for (let i = 0; i < Math.ceil(array.length / index); i += 1) {
+
+  // }
+  return [];
 }
 
 export {
