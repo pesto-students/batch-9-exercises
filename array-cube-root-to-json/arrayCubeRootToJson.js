f6fb4830215afa065ce36d47703f929dc45505eb
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-24 07:56:16 +0530

    [WIP] Partially solve cube root element of array.

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,6 +1,16 @@
+const checkIfNumberCompatible = num => typeof +num === 'number';
+function getCubeRoot(num) {
+  if (!checkIfNumberCompatible(num)) { throw new Error(`Expected type to be convertible to:number \n Received type:${typeof num}`); }
+  return Math.cbrt(+num);
+}
+const convertArrayToMap = getValueFunc => list => list.reduce((accumulator, currentVal) => {
+  accumulator[currentVal] = getValueFunc(currentVal);
+  return accumulator;
+}, {});
+const convertToCubeRootMap = convertArrayToMap(getCubeRoot);
 
-function arrayCubeRootToJson(...args) {
-  return args;
+function arrayCubeRootToJson(arr) {
+  return convertToCubeRootMap(arr);
 }
 
 export {
