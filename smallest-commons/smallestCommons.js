153bfa788110141bc39359dbbe1e90bee335ecb4
avinash360 <avithakur86@gmail.com>
2019-07-25 23:59:39 +0530

    solve: orbitalPeriod, smallest-commons

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,6 +1,26 @@
 
-function smallestCommons(...args) {
-  return args;
+function smallestCommons(range) {
+  const numbers = range.sort((a, b) => a - b);
+  const min = numbers[0];
+  const max = numbers[numbers.length - 1];
+  let result;
+
+  const gcd = (x, y) => {
+    if (y === 0) {
+      return x;
+    }
+    return gcd(y, x % y);
+  }
+
+  for (let i = min; i < max; i++) {
+    if (i === min) {
+      result = i * (i + 1) / gcd(i, i + 1);
+    } else {
+      result = result * (i + 1) / gcd(result, i + 1);
+    }
+    console.log(result);
+  }
+  return result;
 }
 
 export {
