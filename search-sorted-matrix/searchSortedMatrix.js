508326b4a2ca1a3bd694b8765ed0a3f3cfe70b03
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-25 07:17:15 +0530

    solve searchSortedMatrix

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,8 +1,19 @@
+function searchSortedMatrix({ search, matrix }) {
+  const firstNumberInMatrix = matrix[0][0];
+  const lastRowInMatrix = matrix[matrix.length - 1];
+  const lastNumberInMatrix = lastRowInMatrix[lastRowInMatrix.length - 1];
 
-function searchSortedMatrix(...args) {
-  return args;
+  if (search < firstNumberInMatrix || search > lastNumberInMatrix) {
+    return false;
+  }
+
+  const lastNumberInEveryRow = matrix.map(row => row[row.length - 1]);
+
+  const expectedRowIndex = lastNumberInEveryRow.findIndex(
+    number => search <= number
+  );
+
+  return matrix[expectedRowIndex].includes(search);
 }
 
-export {
-  searchSortedMatrix,
-};
+export { searchSortedMatrix };
