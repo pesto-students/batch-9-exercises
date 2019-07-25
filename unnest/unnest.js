80e22e82d9fe878a09c6cef5de53c1d7de6a973e
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-25 20:52:33 +0530

    solve unnest

--- unnest/unnest.js
+++ unnest/unnest.js
@@ -1,8 +1,16 @@
+import { isNumber } from 'util';
 
-function unnest(...args) {
-  return args;
+function unnest(collection) {
+  if (Array.isArray(collection)) {
+    return collection.flat(1);
+  }
+  const modifiedObject = [];
+  Object.keys(collection).map(key => {
+    if (Number(key) !== NaN) {
+      modifiedObject[Number(key)] = collection[key];
+    }
+  });
+  return modifiedObject.flat();
 }
 
-export {
-  unnest,
-};
+export { unnest };
