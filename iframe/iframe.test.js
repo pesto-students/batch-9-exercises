783874f89a89f4ea16722aa3273b83bc56616285
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-26 00:57:19 +0530

    Complete the navigation menu problem.

--- iframe/iframe.test.js
+++ iframe/iframe.test.js
@@ -5,7 +5,7 @@ describe('checkHtml', () => {
     const htmlDom = new DOMParser().parseFromString(iframe(), 'text/html');
     const iFrameTag = htmlDom.getElementsByTagName('iframe')[0];
     const hasIframeTag = iFrameTag !== undefined;
-    const iframeSrcValue = iFrameTag.getAttribute('src')
+    const iframeSrcValue = iFrameTag.getAttribute('src');
     expect(hasIframeTag).toBe(true);
     expect(iframeSrcValue).toBe('https://pesto.tech');
   });
