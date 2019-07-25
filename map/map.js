60ee5160675a8c0e1dfe38b81bd172a3247f0c34
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-26 05:20:49 +0530

    Complete the map problem.

--- map/map.js
+++ map/map.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+const path = require('path');
 
-function map(...args) {
-  return args;
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+function map() {
+  // eslint-disable-next-line no-undef
+  const docObject = new DOMParser().parseFromString(html, 'text/html');
+  return docObject;
 }
 
 export {
