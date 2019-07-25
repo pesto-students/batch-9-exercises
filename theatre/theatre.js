b2978da1e4f817b7262df1038d3a8f3e85ba5172
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-25 09:47:59 +0530

    solve theatre

--- theatre/theatre.js
+++ theatre/theatre.js
@@ -1,8 +1,42 @@
+function theatre(bills) {
+  const moneyMap = new Map();
+  moneyMap.ten = 0;
+  moneyMap.twenty = 0;
+  moneyMap.fifty = 0;
 
-function theatre(...args) {
-  return args;
+  return bills.reduce((canSellTickets, amount) => {
+    if (amount === 10) {
+      moneyMap.ten += 1;
+      return canSellTickets && true;
+    }
+    if (amount === 20) {
+      if (moneyMap.ten > 0) {
+        moneyMap.ten -= 1;
+        moneyMap.twenty += 1;
+        return true && canSellTickets;
+      }
+      return false && canSellTickets;
+    }
+
+    if (amount === 50) {
+      if (moneyMap.twenty > 1) {
+        moneyMap.twenty -= 2;
+        return true && canSellTickets;
+      }
+
+      if (moneyMap.twenty === 1 && moneyMap.ten > 1) {
+        moneyMap.twenty -= 1;
+        moneyMap.ten -= 2;
+        return true && canSellTickets;
+      }
+
+      if (moneyMap.ten > 3) {
+        moneyMap.ten -= 4;
+        return true && canSellTickets;
+      }
+      return false && canSellTickets;
+    }
+  }, true);
 }
 
-export {
-  theatre,
-};
+export { theatre };
