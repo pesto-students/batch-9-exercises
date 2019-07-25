c79a56498c3320b6eb97cdbdd954c785335360c3
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-25 20:31:26 +0530

    Complete sum-of-perfect-squares

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,6 +1,17 @@
-
-function sumOfPerfectSquares(...args) {
-  return args;
+function sumOfPerfectSquares(number) {
+  if (number <= 3) {
+    return number;
+  }
+  let result = number;
+  for (let i = 1; i < (number + 1); i += 1) {
+    const square = i ** 2;
+    if (square > number) {
+      break;
+    } else {
+      result = Math.min(result, (1 + sumOfPerfectSquares(number - square)));
+    }
+  }
+  return result;
 }
 
 export {
