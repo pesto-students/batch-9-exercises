3c54db3a1b97df5fdfef8f11f5d3feae70d13bcc
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-25 08:49:11 +0530

    solve sudanFn

--- sudan-fn/sudanFn.js
+++ sudan-fn/sudanFn.js
@@ -1,8 +1,15 @@
-
-function sudanFn(...args) {
-  return args;
+function sudanFn(object) {
+  if (object.y === 0 && object.n >= 0) {
+    return object.x;
+  }
+  if (object.n === 0) {
+    return object.x + object.y;
+  }
+  return sudanFn({
+    n: object.n - 1,
+    x: sudanFn({ n: object.n, x: object.x, y: object.y - 1 }),
+    y: sudanFn({ n: object.n, x: object.x, y: object.y - 1 }) + object.y
+  });
 }
 
-export {
-  sudanFn,
-};
+export { sudanFn };
