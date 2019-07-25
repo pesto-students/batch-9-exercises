980344013eda157560b6309d2299bb3a0fbddd6e
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-26 05:04:08 +0530

    Complete the html video problem.

--- html-video/htmlVideo.js
+++ html-video/htmlVideo.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+const path = require('path');
 
-function htmlVideo(...args) {
-  return args;
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+function htmlVideo() {
+  // eslint-disable-next-line no-undef
+  const docObject = new DOMParser().parseFromString(html, 'text/html');
+  return docObject;
 }
 
 export {
