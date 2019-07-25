b70b2929a8b52e30eceb9e6c842249acc3957efa
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-25 08:23:06 +0530

    [WIP] Partially solve the create student problem.

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,6 +1,29 @@
 
-function createStudent(...args) {
-  return args;
+function createStudent(likeObj = { likesES2015: true, likesJavaScript: true }) {
+  const { likesES2015, likesJavaScript } = likeObj;
+  const startingString = 'The student';
+  const likesString = 'likes';
+  const doesNotLikeString = 'does not like much...';
+  const javaScriptString = 'JavaScript';
+  const ES2015String = 'ES2015';
+  const andString = 'and';
+  const spaceString = ' ';
+  let resultString;
+  if (likesES2015 && likesJavaScript) {
+    const likesBothArr = [startingString, likesString, javaScriptString, andString, ES2015String];
+    resultString = likesBothArr.join(spaceString);
+  } else if (!likesES2015 && likesJavaScript) {
+    const likesJS = [startingString, likesString, javaScriptString];
+    resultString = likesJS.join(spaceString);
+  } else if (likesES2015 && !likesJavaScript) {
+    const likesES = [startingString, likesString, ES2015String];
+    resultString = likesES.join(spaceString);
+  } else {
+    const doesNotLikeArray = [startingString, doesNotLikeString];
+    resultString = doesNotLikeArray.join(spaceString);
+  }
+
+  return resultString;
 }
 
 export {
