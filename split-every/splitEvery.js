430150ea1a1582c4a8e84adb298394c75f251984
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-25 07:43:12 +0530

    solve splitEvery

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,8 +1,17 @@
+function splitEvery(splitSize, collection) {
+  if (splitSize <= 0) {
+    throw new Error("First argument can't be a non positive number.");
+  }
+  if (collection.length === 0) {
+    return [];
+  }
+  const chunks = [];
+  for (let i = 0; i < collection.length; i += splitSize) {
+    const chunk = collection.slice(i, i + splitSize);
+    chunks.push(chunk);
+  }
 
-function splitEvery(...args) {
-  return args;
+  return chunks;
 }
 
-export {
-  splitEvery,
-};
+export { splitEvery };
