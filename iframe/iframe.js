d979b28cfe3d3312e49853e3c3cee9c61083ea3f
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-26 00:46:45 +0530

    Solve the iframe problem.

--- iframe/iframe.js
+++ iframe/iframe.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const path = require('path');
 
-function iframe(...args) {
-  return args;
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+function iframe() {
+  return html;
 }
 
 export {
