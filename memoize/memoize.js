a0c04d1bbd1764db69f0782ef18a8451cf89f9e2
avinash360 <avithakur86@gmail.com>
2019-07-26 20:37:45 +0530

    solve memoize and curry test

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,15 @@
 
-function memoize(...args) {
-  return args;
+function memoize(fn) {
+  const cache = {};
+  return (...args) => {
+    const key = args.toString();
+    if (cache[key]) {
+      return cache[key];
+    }
+    const result = fn(...args);
+    cache[key] = result;
+    return result;
+  };
 }
 
 export {
