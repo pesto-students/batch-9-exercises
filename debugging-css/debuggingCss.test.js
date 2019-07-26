07f35989b319f6b987352666fa2d6af595897299
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-27 01:00:41 +0530

    design debuggingCss

---
+++ debugging-css/debuggingCss.test.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+describe('always true', () => {
+  document.documentElement.innerHTML = html.toString();
+  test('test', () => {
+    expect(true).toBe(true);
+  });
+});
