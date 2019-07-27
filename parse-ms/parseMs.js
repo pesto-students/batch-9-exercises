36f5cb905b4c8301b0f1e17f430246ef5ed28425
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-27 22:10:14 +0530

    Solve parse-ms.

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,6 +1,28 @@
+/* eslint-disable no-param-reassign */
 
-function parseMs(...args) {
-  return args;
+function parseMs(milliseconds) {
+  // if (milliseconds < 0) {
+  //   milliseconds *= -1;
+  // }
+  const day = 1000 * 60 * 60 * 24;
+  const days = Math.trunc(milliseconds / day);
+  milliseconds %= day;
+  const hour = 1000 * 60 * 60;
+  const hours = Math.trunc(milliseconds / hour);
+  milliseconds %= hour;
+  const minute = 1000 * 60;
+  const minutes = Math.trunc(milliseconds / minute);
+  milliseconds %= minute;
+  const second = 1000;
+  const seconds = Math.trunc(milliseconds / second);
+  milliseconds %= second;
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+    milliseconds,
+  };
 }
 
 export {
