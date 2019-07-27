5c7276e20eea2b42d6809eb56bd3718cadb0df1b
avinash360 <avithakur86@gmail.com>
2019-07-28 05:12:05 +0530

    solve: mapFilterAndReduce test

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,11 @@
+const filterByFirstName = obj => obj.firstName.length < 5;
+const reduceObj = (acc, element) => {
+  acc[element.firstName] = element.firstName.length;
+  return acc;
+};
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(array) {
+  return array.filter(filterByFirstName).reduce(reduceObj, {});
 }
 
 export {
