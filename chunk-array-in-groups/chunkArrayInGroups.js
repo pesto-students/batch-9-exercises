133220f15d33571c4f73a773b607a65fb1836f15
Rajeet <rajeetdel@gmail.com>
2019-07-27 23:42:25 +0530

    Done Day4-segment-1

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,12 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(argArray, arrayLength) {
+  let chunkedArray = [];
+  let i = 0;
+  while (i < argArray.length) {
+    chunkedArray.push(argArray.slice(i, i + arrayLength));
+    i += arrayLength;
+  }
+  return chunkedArray;
 }
 
 export {
--- object-keys/objectKeys.js
+++ object-keys/objectKeys.js
@@ -1,6 +1,12 @@
 
-function objectKeys(...args) {
-  return args;
+function objectKeys(argObject) {
+  const arrayKey = [];
+  for (const key in argObject) {
+    if (Object.prototype.hasOwnProperty.call(argObject, key)) {
+      arrayKey.push(key);
+    }
+  }
+  return arrayKey;
 }
 
 export {
