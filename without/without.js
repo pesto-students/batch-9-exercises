18776862974f8acad0f40f13e5e1c48f2a409cca
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-27 22:07:50 +0530

    Partially solve the without problem.

--- without/without.js
+++ without/without.js
@@ -1,6 +1,11 @@
-
-function without(...args) {
-  return args;
+const checkEquality = (val1, val2) => {
+  const valueAndSignEqual = val1 === val2 && Math.sign(val1) === Math.sign(val2);
+  return valueAndSignEqual;
+};
+const exists = (arr, val) => arr.findIndex(currentValue => checkEquality(currentValue, val)) > -1;
+function without(arr1, arr2) {
+  const result = arr2.filter(val => !exists(arr1, val));
+  return result;
 }
 
 export {
