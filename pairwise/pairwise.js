f079c754f2ce2a2b84eb4739b9b0c464a485d28c
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-28 02:20:23 +0530

    Solve pairwise

--- pairwise/pairwise.js
+++ pairwise/pairwise.js
@@ -1,6 +1,16 @@
 
-function pairwise(...args) {
-  return args;
+function pairwise(array, sum) {
+  const usedIndices = [];
+  let pairwiseSum = 0;
+  for (let i = 0; i < array.length - 1; i += 1) {
+    for (let j = i + 1; j < array.length; j += 1) {
+      if (array[i] + array[j] === sum && !usedIndices.includes(i) && !usedIndices.includes(j)) {
+        pairwiseSum += i + j;
+        usedIndices.push(i, j);
+      }
+    }
+  }
+  return pairwiseSum;
 }
 
 export {
