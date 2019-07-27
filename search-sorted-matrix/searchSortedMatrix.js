a832b978c54d8a8e838b17d2a5c0320dcbb6139e
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-27 22:25:29 +0530

    Complete the searchSortedMatrix problem.

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,6 +1,27 @@
+const binarySearch = (array, value) => {
+  let guessIndex;
+  let minIndex = 0;
+  let maxIndex = array.length - 1;
 
-function searchSortedMatrix(...args) {
-  return args;
+  while (minIndex <= maxIndex) {
+    guessIndex = Math.floor((minIndex + maxIndex) / 2);
+    if (array[guessIndex] === value) {
+      return guessIndex;
+    }
+    if (array[guessIndex] < value) {
+      minIndex = guessIndex + 1;
+    } else {
+      maxIndex = guessIndex - 1;
+    }
+  }
+
+  return -1;
+};
+function searchSortedMatrix({ matrix, search }) {
+  const sortedFlatArray = Array.prototype.concat(...matrix);
+  const searchItemIndex = binarySearch(sortedFlatArray, search);
+  const searchItemExists = searchItemIndex > -1;
+  return searchItemExists;
 }
 
 export {
