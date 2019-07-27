98daa84bc79634a8f1d8815f89609c66d3a2aefe
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-28 03:39:13 +0530

    SOlve fibonacci iter.

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,18 @@
 
-function fibonacciIter(...args) {
-  return args;
-}
+const fibonacciIter = {
+  [Symbol.iterator]() {
+    let previousNumber = 0;
+    let nextNumber = 1;
+    return {
+      next() {
+        const temp = previousNumber;
+        previousNumber = nextNumber;
+        nextNumber += temp;
+        return { value: nextNumber, done: false };
+      },
+    };
+  },
+};
 
 export {
   fibonacciIter,
