158bb7e320d7875bd8a071d0e3705152adc3dccd
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-28 01:40:18 +0530

    Solve rotatedString

--- rotated-string/rotatedString.js
+++ rotated-string/rotatedString.js
@@ -1,6 +1,10 @@
+const sortString = str => str.split('').sort().join('');
 
-function rotatedString(...args) {
-  return args;
+function rotatedString(inputString, outputString) {
+  if (inputString.length !== outputString.length || inputString === outputString) {
+    return false;
+  }
+  return sortString(inputString) === sortString(outputString);
 }
 
 export {
