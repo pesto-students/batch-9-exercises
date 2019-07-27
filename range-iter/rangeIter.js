0b5b73b8cbda4932f23c7b2c6da42a276fba3f75
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-28 04:34:17 +0530

    WIP range iter.

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,6 +1,22 @@
 
-function rangeIter(...args) {
-  return args;
+function rangeIter(lb, ub) {
+  if (!ub || typeof ub !== 'number') {
+    throw TypeError(`${ub} is not a number`);
+  }
+  let copy = lb;
+  return {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          if (copy <= ub) {
+            copy += 1;
+            return { value: copy - 1, done: false };
+          }
+          return { value: undefined, done: true };
+        },
+      };
+    },
+  };
 }
 
 export {
