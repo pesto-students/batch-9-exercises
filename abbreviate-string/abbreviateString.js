77c880036d63c56d313c6879d398d7a109671ce3
avinash360 <avithakur86@gmail.com>
2019-07-28 02:35:24 +0530

    solve: abbreviateString test

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,10 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(text) {
+  if (!typeof text === 'string') {
+    throw TypeError(`Expected type string, received ${typeof text}`);
+  }
+  const arr = text.split(' ');
+  return arr[0] + ' ' + arr[arr.length - 1][0].toUpperCase() + '.'
 }
 
 export {
