268844d6ab6161ec06def22b7905833ac0bdb43c
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-27 22:46:46 +0530

    Complete the splity every problem.

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -3,24 +3,21 @@ function splitEvery(splitCount, iterableObj) {
   if (splitCount < 1) {
     throw new Error(`Expected split count > 1. Received split count:${splitCount}`);
   }
-  const initialArr = [];
-  const isIterableString = typeof iterableObj === 'string';
-  let tempArray = isIterableString ? '' : [];
-  for (const elem of iterableObj) {
-    if (tempArray.length < splitCount) {
-      if (!isIterableString) {
-        tempArray.push(elem);
-      }
-    } else {
-      initialArr.push([...tempArray]);
-      tempArray = [elem];
-    }
+  const tempArr = [];
+  let splitFrom = 0;
+  let numberOfChunks = Math.ceil(iterableObj.length / splitCount);
+
+  while (numberOfChunks > 0) {
+    const splitUpto = splitFrom + splitCount;
+    const currentChunk = iterableObj.slice(splitFrom, splitUpto);
+    tempArr.push(currentChunk);
+    splitFrom = splitUpto;
+    numberOfChunks -= 1;
   }
-  if (tempArray.length > 0) {
-    initialArr.push(tempArray);
-  }
-  return initialArr;
+
+  return tempArr;
 }
+
 export {
   splitEvery,
 };
