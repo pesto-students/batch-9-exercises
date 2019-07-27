0862777587c1769c594ffabafdaa4f7df26db875
avinash360 <avithakur86@gmail.com>
2019-07-28 02:56:03 +0530

    solve: throwError test

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -3,8 +3,8 @@ function abbreviateString(text) {
   if (!typeof text === 'string') {
     throw TypeError(`Expected type string, received ${typeof text}`);
   }
-  const arr = text.split(' ');
-  return arr[0] + ' ' + arr[arr.length - 1][0].toUpperCase() + '.'
+  let arr = text.split(' ');
+  return arr[0] + ' ' + arr[arr.length - 1][0].toUpperCase() + '.';
 }
 
 export {
