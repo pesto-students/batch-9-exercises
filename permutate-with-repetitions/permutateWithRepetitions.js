af8bb8b01559edeb0c5f272812d36dc03b8ea1c2
avinash360 <avithakur86@gmail.com>
2019-07-28 03:59:07 +0530

    solve: permutateWithRepition

--- permutate-with-repetitions/permutateWithRepetitions.js
+++ permutate-with-repetitions/permutateWithRepetitions.js
@@ -1,6 +1,20 @@
+function permutateWithRepetitions(originalArray, length = originalArray.length) {
+  if (length === 1) {
+    return originalArray.map(element => [element]);
+  }
 
-function permutateWithRepetitions(...args) {
-  return args;
+  const permutations = [];
+
+  const nextPermutation = permutateWithRepetitions(
+    originalArray,
+    length - 1,
+  );
+  for (let i = 0; i < originalArray.length; i += 1) {
+    for (let j = 0; j < nextPermutation.length; j += 1) {
+      permutations.push([originalArray[i]].concat(nextPermutation[j]));
+    }
+  }
+  return permutations;
 }
 
 export {
