b2133df40be80cd40690c95df2c1c64e02941f3e
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-27 23:04:49 +0530

    Complete the sudan Fn implementation and fix typo.

--- sudan-fn/sudanFn.js
+++ sudan-fn/sudanFn.js
@@ -1,6 +1,13 @@
 
-function sudanFn(...args) {
-  return args;
+function sudanFn({ n, x, y }) {
+  if (y === 0 && n >= 0) {
+    return x;
+  }
+  if (n === 0) {
+    return x + y;
+  }
+
+  return sudanFn({ n: n - 1, x: sudanFn({ n, x, y: y - 1 }), y: sudanFn({ n, x, y: y - 1 }) + y });
 }
 
 export {
