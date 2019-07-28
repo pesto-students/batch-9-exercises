b2f893d09dc7361b534363a7f564685465164c62
jayamathan <jayamathanskj03@gmail.com>
2019-07-28 22:22:14 +0530

    solve ackermann

--- ackermann/ackermann.js
+++ ackermann/ackermann.js
@@ -1,6 +1,20 @@
 
-function ackermann(...args) {
-  return args;
+function ackermann({ m, n }) {
+  let result;
+  if (m < 0) {
+    throw Error(`Expected positive interger Received: ${m}`);
+  }
+  if (n < 0) {
+    throw Error(`Expected positive interger Received: ${n}`);
+  }
+  if (m === 0) {
+    result = n + 1;
+  } else if (m > 0 && n === 0) {
+    result = ackermann({ m: m - 1, n: 1 });
+  } else {
+    result = ackermann({ m: m - 1, n: ackermann({ m, n: n - 1 }) });
+  }
+  return result;
 }
 
 export {
