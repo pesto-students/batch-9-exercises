4f64eef2224fd7a23198dcfaf018376c8bc4ba07
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-29 04:41:23 +0530

    solve binarySearch

--- binary-search/binarySearch.js
+++ binary-search/binarySearch.js
@@ -1,8 +1,34 @@
+function binarySearch(collection, find) {
+  let start = 0;
+  let end = collection.length - 1;
 
-function binarySearch(...args) {
-  return args;
+  if (typeof find === 'number') {
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+      if (collection[mid] === find) {
+        return mid;
+      } else if (collection[mid] < find) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (collection[mid].key === find.key) {
+      return mid;
+    } else if (collection[mid].key < find.key) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return -1;
 }
 
-export {
-  binarySearch,
-};
+export { binarySearch };
