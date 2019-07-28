019720bac262fb7d3e4a1fff41729b78ba8096eb
avinash360 <avithakur86@gmail.com>
2019-07-29 02:10:28 +0530

    solve: privateProps test

--- private-props/privateProps.js
+++ private-props/privateProps.js
@@ -1,6 +1,39 @@
 
-function privateProps(...args) {
-  return args;
+function privateProps(target, filter) {
+  const handler = {
+    get(obj, key) {
+      return Reflect.get(obj, key);
+    },
+    set(obj, key, prop) {
+      if (filter(key)) {
+        return false;
+      }
+      return Reflect.set(obj, key, prop);
+    },
+    has(obj, key) {
+      if (filter(key)) {
+        return false;
+      }
+      return key in obj || obj.hasItem(key);
+    },
+    ownKeys(obj) {
+      return Object.keys(obj).filter(key => !filter(key));
+    },
+    getOwnPropertyDescriptors(obj, key) {
+      if (filter(key)) {
+        return false;
+      }
+      const value = obj.getItem(key);
+      return value ? {
+        value,
+        writable: true,
+        enumerable: true,
+        configurable: false,
+      } : undefined;
+    },
+  };
+  const proxy = new Proxy(target, handler);
+  return proxy;
 }
 
 export {
