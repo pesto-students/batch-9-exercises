c0d66b1d68d8d1df7995c9ebe61b2a4beedd0981
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-29 01:26:52 +0530

    Complete binary-agent (+ add tests)

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,3 +1,9 @@
+const areBinaryNumbers = (string) => {
+  const characters = string.split('');
+  return characters.every(character => (parseInt(character, 10) === 0)
+                                    || (parseInt(character, 10) === 1));
+};
+
 function binaryToDecimal(number) {
   let decimal = 0;
   for (let i = 0; i < number.length; i += 1) {
@@ -11,7 +17,12 @@ function binaryAgent(string) {
   const input = string.split(' ');
   let sentence = '';
   for (let i = 0; i < input.length; i += 1) {
-    sentence += String.fromCharCode(binaryToDecimal(input[i]));
+    const number = input[i];
+    if (!areBinaryNumbers(number)) {
+      throw Error(`Expected: String of binary characters, Received: ${number}`);
+    } else {
+      sentence += String.fromCharCode(binaryToDecimal(number));
+    }
   }
   return sentence;
 }
