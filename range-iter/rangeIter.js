3de59b325c6eb5ceb6e45cb012d1a8bdb255ffa7
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-28 19:16:25 +0530

    Solve rangeIter

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,14 +1,25 @@
 
-function* rangeIter(lb, ub) {
-  let copyOflb = lb;
-  while (true) {
-    if (Number.isSafeInteger(copyOflb) && copyOflb <= ub) {
-      yield copyOflb;
-      copyOflb += 1;
-    } else {
-      return;
-    }
+function rangeIter(lb, ub) {
+  if (typeof lb !== 'number') {
+    throw new TypeError(`${lb} is not a number`);
   }
+  if (typeof ub !== 'number') {
+    throw new TypeError(`${ub} is not a number`);
+  }
+  let copyOflb = lb;
+  return {
+    [Symbol.iterator]() {
+      copyOflb = lb;
+      return this;
+    },
+    next() {
+      while (copyOflb <= ub) {
+        // eslint-disable-next-line no-plusplus
+        return { value: copyOflb++, done: false };
+      }
+      return { value: '', done: true };
+    },
+  };
 }
 
 export {
