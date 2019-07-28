80a44e07d913da87905506f3e1c76b536914d2c7
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-28 06:41:11 +0530

    Solve ackermann.

--- ackermann/ackermann.js
+++ ackermann/ackermann.js
@@ -1,14 +1,12 @@
 
-function ackermann(object) {
-  console.log(object);
-  const { m, n } = object;
+function ackermann({ m = 0, n = 0 }) {
   if (m === 0) {
     return n + 1;
   }
   if (m > 0 && n === 0) {
-    return ackermann(m - 1, 1);
+    return ackermann({ m: m - 1, n: 1 });
   }
-  return ackermann(m - 1, ackermann(m, n - 1));
+  return ackermann({ m: m - 1, n: ackermann({ m, n: n - 1 }) });
 }
 
 export {
