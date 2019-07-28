a360e309ae57cc4357a2516daef2ab5b705d4efe
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-28 08:43:16 +0530

    Solve permutateWithRepetitions

--- permutate-with-repetitions/permutateWithRepetitions.js
+++ permutate-with-repetitions/permutateWithRepetitions.js
@@ -1,7 +1,20 @@
 
-function permutateWithRepetitions(...args) {
-  return args;
+function permutateWithRepetitions(array, length = array.length) {
+  if (length === 1) {
+    return array.map(char => [char]);
+  }
+  const permutations = [];
+  const smallerPermutations = permutateWithRepetitions(array, length - 1);
+
+  array.forEach((currentOption) => {
+    smallerPermutations.forEach((smallerPermutation) => {
+      permutations.push([currentOption].concat(smallerPermutation));
+    });
+  });
+
+  return permutations;
 }
+
 
 export {
   permutateWithRepetitions,
