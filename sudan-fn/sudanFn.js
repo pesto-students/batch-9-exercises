c6c16d1368e52cd832e5262169e3c3763d23b5a4
jayamathan <jayamathanskj03@gmail.com>
2019-07-29 00:39:03 +0530

    solve sudanFn

--- sudan-fn/sudanFn.js
+++ sudan-fn/sudanFn.js
@@ -1,6 +1,18 @@
-
-function sudanFn(...args) {
-  return args;
+function sudanFn({ n, x, y }) {
+  if (n === 0) {
+    return x + y;
+  }
+  if (y === 0 && n >= 0) {
+    return x;
+  }
+  if (y >= 0 && n >= 0) {
+    return sudanFn({
+      n: n - 1,
+      x: sudanFn({ n, x, y: y - 1 }),
+      y: sudanFn({ n, x, y: y - 1 }) + y,
+    });
+  }
+  throw new Error('Input is invalid');
 }
 
 export {
