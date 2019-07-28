10a12a87db55ed75ab5be2d2729af9ad2df1bd88
avinash360 <avithakur86@gmail.com>
2019-07-28 08:01:14 +0530

    solve: fibionacciIter test

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,18 @@
 
-function fibonacciIter(...args) {
-  return args;
-}
+const fibonacciIter = {
+  [Symbol.iterator]: () => {
+    let i = 1;
+    let prev = 0;
+    return {
+      next: () => {
+        const temp = prev;
+        prev = i;
+        i = temp + i;
+        return { value: i, done: false };
+      },
+    };
+  },
+};
 
 export {
   fibonacciIter,
