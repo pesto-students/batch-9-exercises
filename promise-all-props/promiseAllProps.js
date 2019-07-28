c842ce40d90c88e953f7cd9a2c788a540ce4f0d7
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-28 10:06:37 +0530

    Solve promiseAllProps

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,10 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(object) {
+  const keys = Object.keys(object);
+  const values = Object.values(object);
+  return Promise.all(values)
+    .then(resolvedValues => keys
+      .reduce((acc, key, index) => ({ ...acc, [key]: resolvedValues[index] }), {}));
 }
 
 export {
