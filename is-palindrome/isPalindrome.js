59c0cebd18389f40b6ae091d832e433850349a45
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-29 01:25:31 +0530

    Fix linting issues in the is Palindrome problem.

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,23 +1,22 @@
 function isPalindrome(randomString) {
-  if (typeof randomString !== "number" && typeof randomString !== "string")
-    {
-      throw new Error(
-      "The value passed (" +
-        randomString +
-        ") is not of valid type, its type is:" +
-        typeof randomString +
-        " but it should be of type either string or number."
+  if (typeof randomString !== 'number' && typeof randomString !== 'string') {
+    throw new Error(
+      `The value passed (${
+        randomString
+      }) is not of valid type, its type is:${
+        typeof randomString
+      } but it should be of type either string or number.`,
     );
   }
 
-  let regExToRemoveUnwanted = /[\W_]/g;
-  let lowerCasedCleanString = String(randomString)
+  const regExToRemoveUnwanted = /[\W_]/g;
+  const lowerCasedCleanString = String(randomString)
     .toLowerCase()
-    .replace(regExToRemoveUnwanted, "");
-  let reversedLowerCasedCleanString = lowerCasedCleanString
-    .split("")
+    .replace(regExToRemoveUnwanted, '');
+  const reversedLowerCasedCleanString = lowerCasedCleanString
+    .split('')
     .reverse()
-    .join("");
+    .join('');
   return lowerCasedCleanString === reversedLowerCasedCleanString;
 }
 
