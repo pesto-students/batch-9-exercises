dfcdf8a5b03bb4ee480ce742e0baa2b07e3b8436
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-28 22:03:23 +0530

    attempt uniq

--- uniq/uniq.js
+++ uniq/uniq.js
@@ -1,20 +1,4 @@
 function uniq(array) {
-  // const map = new Map();
-  // array.forEach(element => {
-  //   console.log(element);
-  //   if (map.get(element) === undefined) {
-  //     console.log(Array.isArray(element));
-  //     map.set(element, true);
-  //   }
-  // });
-  // console.log(map);
-  // const keys = map.keys();
-  // const output = [];
-  // for (let ele of keys) {
-  //   output.push(ele);
-  // }
-  // console.log(output);
-  // return output;
   const stringArray = array.map(ele => {
     if (Array.isArray(ele)) {
       return ele.toString;
