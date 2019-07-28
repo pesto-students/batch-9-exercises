2a1032b75d86265c3b65ad97b8492d28efd7abac
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-29 03:55:30 +0530

    feat + test: Complete reverse-string

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,11 @@
+const reducer = (accumulator, character) => character + accumulator;
 
-function reverseString(...args) {
-  return args;
+function reverseString(string) {
+  if (typeof string !== 'string') {
+    throw new TypeError(`Expected: Argument typeof string, Received: ${typeof string}`);
+  }
+  const characters = string.split('');
+  return characters.reduce(reducer, '');
 }
 
 export {
