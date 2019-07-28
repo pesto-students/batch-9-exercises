6894cd92f0382453b37438eaf00ae6bac5e925a1
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-29 03:25:09 +0530

    feat + test: Complete repeat-string-num-times

--- repeat-string-num-times/repeatStringNumTimes.test.js
+++ repeat-string-num-times/repeatStringNumTimes.test.js
@@ -6,4 +6,13 @@ describe('repeatStringNumTimes', () => {
     expect(repeatStringNumTimes('abc', 3)).toEqual('abcabcabc');
     expect(repeatStringNumTimes('abc', -2)).toEqual('');
   });
+  test('throw Error if first argument not string', () => {
+    expect(() => { repeatStringNumTimes(() => {}, 3); }).toThrow(TypeError);
+    expect(() => { repeatStringNumTimes(12, 3); }).toThrow(TypeError);
+  });
+  test('throw error if second argument not a number', () => {
+    expect(() => { repeatStringNumTimes('*', '3'); }).toThrow(TypeError);
+    expect(() => { repeatStringNumTimes('abc', () => {}); }).toThrow(TypeError);
+    expect(() => { repeatStringNumTimes('abc', null); }).toThrow(TypeError);
+  });
 });
