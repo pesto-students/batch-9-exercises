ecf992e78442e8b6a51f19abd1b48c540a2951e5
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-28 11:06:54 +0530

    solve uniq

--- uniq/uniq.js
+++ uniq/uniq.js
@@ -1,8 +1,32 @@
-
-function uniq(...args) {
-  return args;
+function uniq(array) {
+  // const map = new Map();
+  // array.forEach(element => {
+  //   console.log(element);
+  //   if (map.get(element) === undefined) {
+  //     console.log(Array.isArray(element));
+  //     map.set(element, true);
+  //   }
+  // });
+  // console.log(map);
+  // const keys = map.keys();
+  // const output = [];
+  // for (let ele of keys) {
+  //   output.push(ele);
+  // }
+  // console.log(output);
+  // return output;
+  const stringArray = array.map(ele => {
+    if (Array.isArray(ele)) {
+      return ele.toString;
+    }
+    if (String(ele) === 'NaN') {
+      return 'NaN';
+    }
+    return ele;
+  });
+  return Array.from(new Set(stringArray));
 }
 
-export {
-  uniq,
-};
+export { uniq };
+
+// uniq([[1], [1]]);
