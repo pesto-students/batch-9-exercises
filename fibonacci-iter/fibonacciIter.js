e04c41d0e8aa3a31853daf98e8e146d4116db3a9
jayamathan <jayamathanskj03@gmail.com>
2019-07-28 21:05:37 +0530

    fibonacciiter

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,27 +1,20 @@
-function getfibonacci(ub = Number.MAX_SAFE_INTEGER) {
-  let firstNumber = 0;
-  let secondNumber = 1;
-  let nextNumber = 0;
-
-  const fibIterator = {
-    next() {
-      if (nextNumber < ub) {
-        nextNumber = firstNumber + secondNumber;
-        firstNumber = secondNumber;
-        secondNumber = nextNumber;
-        return { value: nextNumber, done: false };
-      }
-      return { value: nextNumber, done: true };
-    },
-
-  };
-  return fibIterator;
+function* fibonacci() {
+  let firstNumber = 1;
+  let secondNumber = 2;
+  yield firstNumber;
+  yield secondNumber;
+  while (true) {
+    const next = firstNumber + secondNumber;
+    firstNumber = secondNumber;
+    secondNumber = next;
+    yield next;
+  }
 }
 
+const fibonacciIter = {
+  [Symbol.iterator]: fibonacci,
+};
 
-function fibonacciIter() {
-  return getfibonacci.next();
-}
 
 export {
   fibonacciIter,
