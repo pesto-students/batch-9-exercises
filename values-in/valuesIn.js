95726426d406ecad935811eea1ab01704634bf00
avinash360 <avithakur86@gmail.com>
2019-07-29 00:38:46 +0530

    solve: valuesIn test

--- values-in/valuesIn.js
+++ values-in/valuesIn.js
@@ -1,6 +1,10 @@
 
-function valuesIn(...args) {
-  return args;
+function valuesIn(object) {
+  let array = [];
+  for (const prop in object) {
+    array = [...array, object[prop]];
+  }
+  return array;
 }
 
 export {
