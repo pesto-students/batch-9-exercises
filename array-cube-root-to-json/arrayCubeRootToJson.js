5e9c958f76dd606b339dde1430844b39fa27715e
avinash360 <avithakur86@gmail.com>
2019-07-29 04:44:27 +0530

    solve: arrayCubeRootToJson test

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,6 +1,24 @@
+function isNumericArray(array) {
+  return array.every((element) => {
+    if (typeof element === 'string') {
+      return !Number.isNaN(parseInt(element, 10));
+    }
+    return typeof element === 'number';
+  });
+}
 
-function arrayCubeRootToJson(...args) {
-  return args;
+function arrayCubeRootToJson(array) {
+  if (!Array.isArray(array)) {
+    throw Error(`Expected type array, received type ${typeof array}`);
+  }
+  if (!isNumericArray(array)) {
+    throw Error('Each element of the array should be a number');
+  }
+
+  return array.reduce((acc, element) => {
+    acc = { ...acc, [element]: Math.cbrt(element) };
+    return acc;
+  }, {});
 }
 
 export {
