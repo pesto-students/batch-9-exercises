ef180378be54f6c38408d0de8f032922e15aa4d7
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-28 07:40:42 +0530

    Solve arg-string.

--- args-string/argsString.js
+++ args-string/argsString.js
@@ -1,6 +1,13 @@
 
-function argsString(...args) {
-  return args;
+const replacer = text => searchValue => newValue => text.replace(searchValue, newValue);
+
+function argsString(string, array) {
+  let result = string;
+  const braces = '{}';
+  array.forEach((element) => {
+    result = replacer(result)(braces)(element);
+  });
+  return result;
 }
 
 export {
