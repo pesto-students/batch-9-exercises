fd1307543f4781539400c63eff614476603ba662
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-29 01:36:43 +0530

    Fix linting issues in find largest and longest word.

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,10 +1,10 @@
-import { findLargest } from "../large-of-four/largeOfFour";
-function longestWordInString(randomString) {
+import { findLargest } from '../large-of-four/largeOfFour';
 
-  const wordsLengthList = randomString.split(" ").map(word => word.length);
+function longestWordInString(randomString) {
+  const wordsLengthList = randomString.split(' ').map(word => word.length);
 
   const longestWordSize = findLargest(wordsLengthList);
-  
+
   return longestWordSize;
 }
 
--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,14 +1,12 @@
 function reverseString(string) {
-  
-  let outputString = ''
-  let iterator = string.length -1 
-  
-  while(iterator >= 0){
-    outputString+=string[iterator]
-    iterator--;
-  }
-  return outputString
+  let outputString = '';
+  let iterator = string.length - 1;
 
+  while (iterator >= 0) {
+    outputString += string[iterator];
+    iterator -= 1;
+  }
+  return outputString;
 }
 
 export { reverseString };
