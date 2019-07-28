c0a0fd0bb80c042be6f87994ab5d133bc3bfa6da
Rajeet <rajeetdel@gmail.com>
2019-07-28 05:48:13 +0530

    WIP Day-7-segment-1

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,6 +1,23 @@
 
-function simpleIterable(...args) {
-  return args;
+function simpleIterable() {
+  let i = 0;
+  return {
+    [Symbol.iterator]: () => ({
+      next() {
+        i += 1;
+        if (i <= 5) {
+          return {
+            value: i,
+            done: false,
+          };
+        }
+        return {
+          value: undefined,
+          done: true,
+        };
+      },
+    }),
+  };
 }
 
 export {
