0f1fcf76b145d35755d6ea3c422f63f0d5ec3599
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-29 01:35:10 +0530

    Fix linting issues in sum all problem.

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,14 +1,16 @@
-function sumAll(range) {
 
+function sumOneToN(n) {
+  return (n * (n + 1)) / 2;
+}
+
+function sumAll(range) {
   const upperBound = range[0] > range[1] ? range[0] : range[1];
   const lowerBound = range[0] < range[1] ? range[0] : range[1];
-  
-  const requiredSum = sumOneToN(upperBound)-sumOneToN(lowerBound-1) 
 
-  return requiredSum
+  const requiredSum = sumOneToN(upperBound) - sumOneToN(lowerBound - 1);
+
+  return requiredSum;
 }
 
-function sumOneToN (n) {
-  return (n * (n+1)) / 2;
-}
+
 export { sumAll };
