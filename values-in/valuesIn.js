2aac98b87ec6cee7b5d184346004b886ff9c622b
jayamathan <jayamathanskj03@gmail.com>
2019-07-28 23:45:27 +0530

    solve values-in

--- values-in/valuesIn.js
+++ values-in/valuesIn.js
@@ -1,6 +1,12 @@
 
-function valuesIn(...args) {
-  return args;
+function valuesIn(object) {
+  let prop;
+  const values = [];
+  // eslint-disable-next-line guard-for-in
+  for (prop in object) {
+    values[values.length] = object[prop];
+  }
+  return values;
 }
 
 export {
