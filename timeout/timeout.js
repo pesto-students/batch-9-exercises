ab050352fcc58ff126463b295233fd0ee122d6ec
Sumit Nihalani <nihalanisumit@gmail.com>
2019-07-29 03:22:46 +0530

    solve timeout

--- timeout/timeout.js
+++ timeout/timeout.js
@@ -1,8 +1,6 @@
-
-function timeout(...args) {
-  return args;
+async function timeout(name) {
+  const greeting = await setTimeout(() => `Hello ${name}`, 300);
+  return greeting;
 }
 
-export {
-  timeout,
-};
+export { timeout };
