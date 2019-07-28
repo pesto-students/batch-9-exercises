70d1fc1d8e7d5fe1a3447523c9ec806139c432fc
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-28 08:51:12 +0530

    Solve bind

--- bind/bind.js
+++ bind/bind.js
@@ -1,13 +1,6 @@
 
-function bind(fn, person, args) {
-  const bindFn = (...curriedArgs) => {
-    if (!args) {
-      // eslint-disable-next-line no-param-reassign
-      args = curriedArgs;
-    }
-    return fn.call(person, args);
-  };
-  return bindFn;
+function bind(fn, person, ...args) {
+  return fn.bind(person, ...args);
 }
 
 export {
