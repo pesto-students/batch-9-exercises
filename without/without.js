6e1d1c6a060ca83fa20e4c335075cb6efcc1c68d
jayamathan <jayamathanskj03@gmail.com>
2019-07-28 23:34:23 +0530

    WIP without

--- without/without.js
+++ without/without.js
@@ -1,6 +1,11 @@
 
-function without(...args) {
-  return args;
+function without(array1, array2) {
+  return array2.reduce((el) => {
+    if (array1.indexOf(el) === -1) {
+      array2.shift();
+    }
+    return array2;
+  }, []);
 }
 
 export {
