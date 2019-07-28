6bac74dc7e59ff601e2318f5cb8e0c47e0896a89
sumeet rohra <sumeetrohra1@gmail.com>
2019-07-28 06:29:19 +0530

    Solve aperture.

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,14 @@
 
-function aperture(...args) {
-  return args;
+function aperture(nestCount, array) {
+  if (nestCount > array.length) {
+    return [];
+  }
+  const iterations = array.length - nestCount;
+  const newArray = [];
+  for (let i = 0; i <= iterations; i += 1) {
+    newArray.push(array.slice(i, i + nestCount));
+  }
+  return newArray;
 }
 
 export {
