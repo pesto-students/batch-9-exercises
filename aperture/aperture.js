d7a046b8c34dd3df757b3a2283a5e88ea861cf70
avinash360 <avithakur86@gmail.com>
2019-07-29 05:10:45 +0530

    solve: aperture test

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,22 @@
 
-function aperture(...args) {
-  return args;
+function aperture(chunkSize, array) {
+  if (chunkSize > array.length) {
+    return [];
+  }
+  let newArr = [];
+  let start = 0;
+  let end = chunkSize;
+  while (start !== null) {
+    const tempArr = array.slice(start, end);
+    if (tempArr.length < chunkSize) {
+      start = null;
+    } else {
+      newArr = [...newArr, tempArr];
+      start += 1;
+      end = start + chunkSize;
+    }
+  }
+  return newArr;
 }
 
 export {
