387e09c79a6e0a2c31d7bf995b744f9602c92599
avinash360 <avithakur86@gmail.com>
2019-07-29 03:27:00 +0530

    solve: splitEvery test

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,6 +1,15 @@
 
-function splitEvery(...args) {
-  return args;
+function splitEvery(chunkSize, array) {
+  if (chunkSize < 1) {
+    throw Error('chunkSize should be atleast 1');
+  }
+  const chunkedArr = [];
+  let index = 0;
+  while (index < array.length) {
+    chunkedArr.push(array.slice(index, chunkSize + index));
+    index += chunkSize;
+  }
+  return chunkedArr;
 }
 
 export {
