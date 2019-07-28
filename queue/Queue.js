81ef3c2c9b1374b510c2ad183f942b6fe96be235
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-28 09:46:07 +0530

    Solve Queue

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,36 @@
 
-function queue(...args) {
-  return args;
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(data) {
+    return this.queue.push(data);
+  }
+
+  dequeue() {
+    if (!this.isEmpty()) {
+      return this.queue.shift();
+    }
+    return null;
+  }
+
+  toString(fn) {
+    if (fn) {
+      return this.queue.reduce((acc, val) => `${acc + fn(val)},`, '').slice(0, -1);
+    }
+    return this.queue.toString();
+  }
+
+  peek() {
+    return this.queue[0] === undefined ? null : this.queue[0];
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
 }
 
 export {
-  queue,
+  Queue,
 };
