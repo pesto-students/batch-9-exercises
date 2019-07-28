5ad9c09785eefa352547496b448fc9809e058a41
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-29 04:12:07 +0530

    Complete the queue problem.

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,43 @@
 
-function queue(...args) {
-  return args;
-}
+class Queue {
+  constructor() {
+    this.linkedList = [];
+  }
 
+  enqueue(value) {
+    this.linkedList.push(value);
+  }
+
+  dequeue() {
+    const dequeuedValue = this.linkedList.shift();
+    if (!dequeuedValue) {
+      return null;
+    }
+    return dequeuedValue;
+  }
+
+  toString(randomFn) {
+    let stringifiedString;
+    if (randomFn) {
+      const stringifiedPair = this.linkedList.map(randomFn);
+      stringifiedString = stringifiedPair.join(',');
+    } else {
+      stringifiedString = this.linkedList.join(',');
+    }
+    return stringifiedString;
+  }
+
+  isEmpty() {
+    return this.linkedList.length === 0;
+  }
+
+  peek() {
+    if (!this.linkedList[0]) {
+      return null;
+    }
+    return this.linkedList[0];
+  }
+}
 export {
-  queue,
+  Queue,
 };
