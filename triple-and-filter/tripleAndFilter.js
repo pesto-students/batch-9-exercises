8e86aa598189b5ab75ffaee09e44238e50e51e68
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-29 03:17:50 +0530

    Complete the triple and filter problem.

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,6 +1,8 @@
 
-function tripleAndFilter(...args) {
-  return args;
+function tripleAndFilter(arr) {
+  const numbersTriced = arr.map(num => num * 3);
+  const numbersDivisibleByFive = numbersTriced.filter(num => (num % 5 === 0));
+  return numbersDivisibleByFive;
 }
 
 export {
