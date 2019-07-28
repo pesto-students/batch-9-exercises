832bcbb00ada5def35c94da315b48d54e0fb3028
avinash360 <avithakur86@gmail.com>
2019-07-28 23:40:35 +0530

    solve: onChange Test

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -3,7 +3,7 @@ function knownProp(target) {
   const proxy = new Proxy(target, {
     get(obj, prop) {
       if (obj.hasOwnProperty(prop)) {
-        return obj[prop];
+        return Reflect.get(obj, prop);
       }
       throw TypeError('Unknown property');
     },
