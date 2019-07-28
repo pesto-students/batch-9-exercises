e657705ddc009210b3c0c4de0617dd73cc72c5c6
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-28 08:54:11 +0530

    Solve allOfConditions

--- all-of-conditions/allOfConditions.js
+++ all-of-conditions/allOfConditions.js
@@ -1,18 +1,6 @@
 
 function allOfConditions(...callbacks) {
-  const [firstFunction, secondFunction] = callbacks;
-  return (...args) => {
-    new Promise((resolve, reject) => {
-      const result = firstFunction(args);
-      if (result) {
-        resolve();
-      } else {
-        reject(new Error('Promise rejected.'));
-      }
-    }).then(() => secondFunction(...args))
-      // eslint-disable-next-line no-console
-      .catch(err => console.error(err));
-  };
+  return (...input) => callbacks.every(cb => cb(...input));
 }
 
 export {
