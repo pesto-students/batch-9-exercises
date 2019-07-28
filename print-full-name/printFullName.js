5c69515c9afb371ff6e9e189a1afc198696d2836
jayamathan <jayamathanskj03@gmail.com>
2019-07-28 10:52:36 +0530

    solve template-literals

--- print-full-name/printFullName.js
+++ print-full-name/printFullName.js
@@ -1,13 +1,4 @@
-function get(attr) {
-  return function (object) {
-    return object[attr];
-  };
-}
-
-
-function printFullName(obj) {
-  const first = get('first')(obj);
-  const last = get('second')(obj);
+function printFullName(first, last) {
   return `My name is ${first} ${last}`;
 }
 
--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,23 @@
 
-function templateLiterals(...args) {
-  return args;
+function templateLiterals() {
+  const teamName = 'football';
+  const people = [{
+    name: 'Rooney',
+    role: 'CF',
+  },
+  {
+    name: 'Ronaldo',
+    role: 'LW',
+  },
+  {
+    name: 'Messi',
+    role: 'RW',
+  },
+  {
+    name: 'Pogba',
+    role: 'CM',
+  }];
+  return `There are 4 people on the ${teamName} team. Their names are ${people[0].name}, ${people[1].name}, ${people[2].name}, ${people[3].name}.`;
 }
 
 export {
