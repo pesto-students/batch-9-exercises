4e00f0f4e080e56dc9fdc8b5c3aaab8a85717d2e
sumeetrohra <sumeetrohra1@gmail.com>
2019-07-30 04:46:19 +0530

    Solve question 10.

--- question-10/question10.js
+++ question-10/question10.js
@@ -1,8 +1,19 @@
-
-function question10(...args) {
-  return args;
-}
-
-export {
-  question10,
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-undef */
+const firstChild = document.getElementById('page').firstElementChild;
+const lastChild = document.getElementById('header').lastElementChild;
+const next = document.querySelector('#header').nextElementSibling;
+const previous = document.querySelector('#content').previousElementSibling;
+const parent = document.querySelector('#page').parentElement;
+const children = document.querySelector('body').childNodes;
+const object = {
+  firstChildOfPageId: firstChild,
+  lastChildOfHeader: lastChild,
+  nextSiblingOfHeader: next,
+  previousSiblingOfContent: previous,
+  parentNodeOfPage: parent,
+  childNodesOfBody: children,
 };
+
+console.log(object);
