7afdb12d3619ad4f7f832c6c6a1e9c1050818303
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-29 06:18:00 +0530

    feat + test: Complete rotated-string

--- rotated-string/rotatedString.js
+++ rotated-string/rotatedString.js
@@ -1,6 +1,25 @@
+const rotateArray = array => (pivot) => {
+  const firstPart = array.slice(array.indexOf(pivot));
+  const secondPart = array.slice(0, array.indexOf(pivot));
+  const rotatedArray = firstPart.concat(secondPart);
+  return rotatedArray;
+};
 
-function rotatedString(...args) {
-  return args;
+function rotatedString(firstString, secondString) {
+  if (typeof firstString !== 'string' || typeof secondString !== 'string') {
+    throw new TypeError(`Expected: both inputs should be strings, Received: ${typeof firstString} ${typeof secondString}`);
+  }
+  if (firstString === secondString) {
+    return false;
+  }
+  const originalCharacters = firstString.split('');
+  const rotateFromPivot = rotateArray(originalCharacters);
+  const rotatedArray = rotateFromPivot(secondString[0]);
+  const stringAfterRotation = rotatedArray.join('');
+  if (stringAfterRotation === secondString) {
+    return true;
+  }
+  return false;
 }
 
 export {
