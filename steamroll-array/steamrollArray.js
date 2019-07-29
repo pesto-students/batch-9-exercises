f5c4fa3d53fbbce46c6d0820701165a0fdaeca22
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-29 07:03:54 +0530

    feat: Complete steamroll-array

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,12 @@
-
-function steamrollArray(...args) {
-  return args;
+function steamrollArray(array) {
+  if (!Array.isArray(array)) {
+    return [array];
+  }
+  let newArray = [];
+  for (const value of array) {
+    newArray = newArray.concat(steamrollArray(value));
+  }
+  return newArray;
 }
 
 export {
