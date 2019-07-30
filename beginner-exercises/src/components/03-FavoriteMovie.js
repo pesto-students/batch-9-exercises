4a661c8affce1fdf87431fd611d4548c1083e094
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-31 01:25:24 +0530

    Changes for the favourite movie problem.

--- beginner-exercises/src/components/03-FavoriteMovie.js
+++ beginner-exercises/src/components/03-FavoriteMovie.js
@@ -4,23 +4,22 @@ import React, { Component } from 'react';
 class FavoriteMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = { movie: 'Hey there. Enter your favorite movie.' };
+    this.state = { movie: '' };
     this.onMovieChange = this.onMovieChange.bind(this);
   }
 
   /* eslint-disable no-unused-vars, react/no-unused-state */
   onMovieChange(event) {
-    let movieName = event.target.value;
-    if (movieName.length === 0) {
-      movieName = 'Hey there. Enter your favorite movie.';
-    }
+    const movieName = event.target.value;
     this.setState({ movie: movieName });
   }
 
   render() {
+    const { movie } = this.state;
     return (
       <div>
-        <p>My favorite movie is <span style={{ color: 'blue' }}>{this.state.movie}</span></p>
+        {movie.length > 0 ? (<p>My favorite movie is <span style={{ color: 'blue' }}>{movie}</span></p>) : (<p>Hey there. Enter your favorite movie.</p>)
+        }
         <input type="text" name="name" onChange={this.onMovieChange} />
       </div>
     );
