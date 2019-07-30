6b261ac7e288e59252e8b2f607fe8e82a420dce1
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-31 00:34:22 +0530

    Solve HelloWorld

--- beginner-exercises/src/components/01-HelloWorld.js
+++ beginner-exercises/src/components/01-HelloWorld.js
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 // Task: Render "Hello World" text.
-class HelloWorld extends Component {
-  // All components *must* have a `render` method defined.
-
-  // To define a component's render method, we use syntax called JSX. As you
-  // can see it looks similar to HTML. JSX gets converted to
-  // JavaScript code. It is here just for readability purposes.
-
+class HelloWorld extends PureComponent {
   render() {
-    return (
-      <h1>Fill this h1 tag with your heading!</h1>
-    );
+    // eslint-disable-next-line react/jsx-filename-extension
+    return <h1>Hello World</h1>;
   }
 }
 
