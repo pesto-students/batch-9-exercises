a96f865e4f7c3f02e72251b158c99fd0eb8406a5
piyushagwl007 <piyushagwl007@gmail.com>
2019-07-31 04:54:04 +0530

    Complete the lifecycle probelm.

--- intermediate-exercises/src/components/01-LifeCycle.js
+++ intermediate-exercises/src/components/01-LifeCycle.js
@@ -1,44 +1,54 @@
 import React from 'react';
 
-/*
-  Tasks for this exercise are in comments inside the component class code.
-  In this exercise lifecycle methods will be used to provide convenient debug
-  messages in developer's console.
-  There are more lifecycle methods available.
-  Those three presented are commonly used.
- */
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 class LifeCycle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: 'Pesto' };
+    this.state = {
+      name: 'Pesto',
+      options: ['Pesto', 'Piyush', 'Arfat'],
+    };
+    this.onClickhandle = this.onClickhandle.bind(this);
   }
 
-  // This code will be called when the component finishes mounting
-  // (so it is visible for a user).
   componentDidMount() {
-    // Task 1: Display a message "I'm mounted!" in developer's console when the
-    //         component finishes mounting.
-    //         Use `console.log` function for it.
+    console.log('I am mounted');
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // Task 2: Display a message "Updated!" in developer's console
-    //         when the component updates.
-    //         Here you also need to use the console.log function.
-    //         Notice that in this lifecycle method you have an access
-    //         to previous values of properties and state.
-    console.log({ prevProps, prevState });
+    const { name } = this.state;
+    if (prevState.name !== name) {
+      console.log('Updated!');
+    }
   }
 
   componentWillUnmount() {
-    // Task 3: Display a message "Good night, Pesto!" in developer's
-    //         console when the component unmounts.
+    const { name } = this.state;
+    console.log(`Good night,${name}`);
   }
 
-  // Task 4: Create a button which triggers ComponentDidUpdate lifecycle method on click.
+  onClickhandle() {
+    const { options } = this.state;
+    const newNameIndex = getRandomInt(3);
+    const newName = options[newNameIndex];
+    this.setState({ name: newName });
+  }
+
   render() {
-    return (<p>Good morning, {this.state.name}!</p>);
+    const { name } = this.state;
+    return (
+      <div>
+        <p>
+          Good morning,
+          {' '}
+          {name}
+          !
+        </p>
+        <button type="button" onClick={this.onClickhandle}>Try Changing Name</button>
+      </div>
+    );
   }
 }
 
