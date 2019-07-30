81da38c8eef751d5e49b63c50a68feed66bed774
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-31 00:32:04 +0530

    feat: Complete 02-MoiveList

--- beginner-exercises/src/components/01-HelloWorld.jsx
+++ beginner-exercises/src/components/01-HelloWorld.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 function HelloWorld() {
-    return (
-      <h1>Hello World</h1>
-    );
+  return (
+    <h1>Hello World</h1>
+  );
 }
 
 export default HelloWorld;
