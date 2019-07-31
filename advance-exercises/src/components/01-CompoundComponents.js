83c93c84681809d7b9b21baf2f1f2163a66f186c
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-01 01:21:29 +0530

    Partially Solve Compound Component problem.

--- advance-exercises/src/components/01-CompoundComponents.js
+++ advance-exercises/src/components/01-CompoundComponents.js
@@ -20,27 +20,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class RadioGroup extends React.Component {
+  state = {selectedValue:this.props.defaultValue || null}
+  changeSelectedOption = this.changeSelectedOption.bind(this)
   static propTypes = {
-    // defaultValue: PropTypes.string,                UN-COMMENT THIS LINE
+    defaultValue: PropTypes.string,
     children: PropTypes.shape().isRequired,
   };
+  changeSelectedOption(value){
+    this.setState({selectedValue:value})
+  }
   render() {
+    const { name } = this.props;
+    const { selectedValue } = this.state;
+    const renderedChildren = React.Children.map(this.props.children, (child) => {
+      const { value } = child.props;
+      return React.cloneElement(child,{name, selectFunction:this.changeSelectedOption, isSelected: value === selectedValue})
+    })
     return (
-      <div>{this.props.children}</div>
+      <div tabIndex='0'>{renderedChildren}</div>
     );
   }
 }
+const RadioOptionContext = React.createContext();
 
 class RadioOption extends React.Component {
   static propTypes = {
-    // value: PropTypes.string,                       UN-COMMENT THIS LINE
+    value: PropTypes.string.isRequired,                       
     children: PropTypes.shape().isRequired,
+    isSelected:PropTypes.bool.isRequired,
   };
+  static RadioIcon2 = () => {
+return (
+  <RadioOptionContext.Consumer  >
+    {({name, value, isSelected}) => {
+      return (<RadioIcon isSelected={isSelected} name={name} value={value} />)
+      }} 
+  </RadioOptionContext.Consumer>
 
+ )
+  }
   render() {
+    const { name, value, isSelected, selectFunction } = this.props;
     return (
-      <div>
-        <RadioIcon isSelected={false} /> {this.props.children}
+      <div onClick={()=>{selectFunction(value)}}>
+       <RadioOptionContext.Provider name={name} value={value} isSelected={isSelected}>
+          {this.props.children}
+       </RadioOptionContext.Provider>
       </div>
     );
   }
@@ -75,11 +100,11 @@ class CompoundComponents extends React.Component {
       <div>
         <h1>♬ It is about time that we all turned off the radio ♫</h1>
 
-        <RadioGroup defaultValue="fm">
-          <RadioOption value="am">AM</RadioOption>
-          <RadioOption value="fm">FM</RadioOption>
-          <RadioOption value="tape">Tape</RadioOption>
-          <RadioOption value="aux">Aux</RadioOption>
+        <RadioGroup defaultValue="fm" name="radio" >
+          <RadioOption value="am">AM<RadioOption.RadioIcon2/></RadioOption>
+          <RadioOption value="fm"><RadioOption.RadioIcon2/>FM</RadioOption>
+          <RadioOption value="tape"><RadioOption.RadioIcon2/>Tape</RadioOption>
+          <RadioOption value="aux"><RadioOption.RadioIcon2/>Aux</RadioOption>
         </RadioGroup>
       </div>
     );
