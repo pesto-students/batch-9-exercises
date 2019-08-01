9afd81e4a40fa6b1828a1e5400b9d619996dc5ee
Sumit Nihalani <nihalanisumit@gmail.com>
2019-08-02 01:24:54 +0530

    solve setup Router

--- setup-router/src/components/Navbar.js
+++ setup-router/src/components/Navbar.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import '../styles/App.css';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
+      <ul className='nav'>
+        <li>
+          <NavLink to='/' exact activeClassName='active'>
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to='/about' exact activeClassName='active'>
+            About Us
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/contact' exact activeClassName='active'>
+            Contact
+          </NavLink>
+        </li>
       </ul>
     );
   }
---
+++ setup-router/src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class NotFound extends Component {
+  render() {
+    return <div>ERROR 404, webpage not found.</div>;
+  }
+}
