9a79461e4ea43e6ac3904c405370367ddbd57e47
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-02 01:45:02 +0530

    feat: Add Navbar to setup-router

--- setup-router/src/components/Navbar.js
+++ setup-router/src/components/Navbar.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import NotFound from './NotFound';
 import '../styles/App.css';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
-      </ul>
+      <Router>
+        <div>
+          <ul className="nav">
+            <li>
+              <NavLink to="/home/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about/">About Us</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact/">Contact</NavLink>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/home/" exact component={Home} />
+            <Route path="/about/" exact component={About} />
+            <Route path="/contact/" exact component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
