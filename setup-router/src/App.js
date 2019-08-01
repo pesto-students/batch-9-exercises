4935e46e92f34dbdfa0c4b012f99bde4a110fd6e
piyush@crive.me <piyush@crive.me>
2019-08-02 01:28:15 +0530

    Complete router problem

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { routes } from './routes';
 import './styles/App.css';
@@ -14,6 +14,7 @@ class App extends Component {
         <Router>
           <Navbar />
           {renderedRoutes}
+          <Redirect to="/404" />
         </Router>
       </Fragment>
     );
--- setup-router/src/routes.js
+++ setup-router/src/routes.js
@@ -1,11 +1,13 @@
 import About from './components/About';
 import Contact from './components/Contact';
 import Home from './components/Home';
+import FourOhFour from './components/404';
 
 const paths = {
   contactPath: '/contact',
   homePath: '/home',
   aboutPath: '/about',
+  fourOhFour: '/404',
 };
 const routes = [
   {
@@ -19,6 +21,11 @@ const routes = [
   {
     component: Home,
     link: paths.homePath,
+  },
+  {
+    component: FourOhFour,
+    link: paths.fourOhFour,
+
   },
 ];
 
