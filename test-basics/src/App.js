0365458c72fd5209748a2f4d05c10081458954ae
piyush@crive.me <piyush@crive.me>
2019-08-03 01:05:50 +0530

    Complete tests for app of test basics

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -9,10 +9,36 @@ class App extends Component {
     input: '',
     mainColor: 'blue',
   }
+
+  toggleButtonState = this.toggleButtonState.bind(this)
+  handleOnChange = this.handleOnChange.bind(this)
+  handleStrings = this.handleStrings.bind(this)
+  toggleButtonState() {
+    this.setState(({ on }) => ({ on: !on }));
+  }
+
+  handleOnChange({ currentTarget: { value } }) {
+    this.setState(({ input: value }));
+  }
+
+  handleStrings(string) {
+    const isStringAndNotEmpty = typeof string === 'string' && string.length > 0;
+    return isStringAndNotEmpty;
+  }
   render() {
+    const { mainColor, on, input } = this.state;
     return (
       <div className="App">
-        Welcome to React
+        <h1 className={mainColor} >Welcome to React</h1>
+        <h2>{input}</h2>
+        <button type="button" onClick={this.toggleButtonState}>
+          <p className="button-state">
+            {
+              on ? 'Yes!' : 'No!'
+            }
+          </p>
+        </button>
+        <input type="text" name="heading" onChange={this.handleOnChange} value={input} />
       </div>
     );
   }
