541a2da0857b90350dd0202389f86e1e44969340
avinash360 <avithakur86@gmail.com>
2019-08-03 01:11:07 +0530

    solve: Jest test cases

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -8,19 +8,47 @@ class App extends Component {
     on: false,
     input: '',
     mainColor: 'blue',
+    paragraphText: 'No!',
+    h2Text: ''
+  }
+
+  handleClick() {
+    const nextText = this.state.paragraphText === 'No!' ? 'Yes!' : 'No!';
+    this.setState({ paragraphText: nextText });
+  }
+
+  handleChange(event) {
+    const { value } = event.currentTarget;
+    this.setState({ h2Text: value });
   }
   render() {
     return (
       <div className="App">
-        Welcome to React
+        <h1>Welcome to React</h1>
+        <h2>{this.state.h2Text}</h2>
+        <div className={this.state.mainColor}></div>
+        <input onChange={(e) => this.handleChange(e)}></input>
+        <p className="button-state">{this.state.paragraphText}</p>
+        <Link hide={false} address="www.google.com"></Link>
+        <button onClick={this.handleClick.bind(this)}>Click Me!</button>
       </div>
     );
   }
 }
 
 export class Link extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return null;
+    if(this.props.display === false){
+      return null;
+    }
+
+    
+    return (
+      <a style={{ display: 'block' }} href={this.props.address}></a>
+    );
   }
 }
 
