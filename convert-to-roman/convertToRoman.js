acb19aa6eb67f8ba751f8fd17223b0a032f49876
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-03 15:24:36 +0530

    feat + test: Complete convert-to-roman

--- convert-to-roman/convertToRoman.js
+++ convert-to-roman/convertToRoman.js
@@ -1,6 +1,32 @@
-
-function convertToRoman(...args) {
-  return args;
+function convertToRoman(input) {
+  let decimalNumber = input;
+  if (!Number.isFinite(decimalNumber)) {
+    throw TypeError('Input is not a countable number');
+  }
+  const romanKeys = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let romanNumber = '';
+  const keysArray = Object.keys(romanKeys);
+  keysArray.forEach((key) => {
+    while (decimalNumber >= romanKeys[key]) {
+      romanNumber += key;
+      decimalNumber -= romanKeys[key];
+    }
+  });
+  return romanNumber;
 }
 
 export {
