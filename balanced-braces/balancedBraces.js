d4fd9be4d73a78b759adf3089c714323781bb17f
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-04 05:24:04 +0530

    Complete the balanced braces probelm.

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,6 +1,25 @@
 
-function balancedBraces(...args) {
-  return args;
+function balancedBraces(string) {
+  const brackets = { open: '[', close: ']' };
+  const curlyBraces = { open: '{', close: '}' };
+  const parentheses = { open: '(', close: ')' };
+  const bracesToCheck = [brackets, curlyBraces, parentheses];
+  const stackForBrackets = [];
+  for (const char of string) {
+    const currentBraces = bracesToCheck.find(val => (val.open === char) || (val.close === char));
+    if (currentBraces) {
+      const isOpenBraces = char === currentBraces.open;
+      if (isOpenBraces) {
+        stackForBrackets.push(currentBraces.open);
+      } else {
+        const lastOpenBracesInStack = stackForBrackets.pop();
+        if (lastOpenBracesInStack !== currentBraces.open) {
+          return false;
+        }
+      }
+    }
+  }
+  return stackForBrackets.length === 0;
 }
 
 export {
