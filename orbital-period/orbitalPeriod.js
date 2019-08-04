55eba6557344452ac084f6c75b2621d9289c44d4
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-04 07:54:14 +0530

    feat: Complete orbital-period

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,6 +1,23 @@
+const calculateOrbitalPeriod = (avgAlt) => {
+  const GM = 398600.4418; // in km3s-2
+  const earthRadius = 6367.4447; // in km
+  const distanceFromCenter = avgAlt + earthRadius;
+  const orbitalPeriodValue = 2 * Math.PI * Math.sqrt((distanceFromCenter ** 3) / GM);
+  return orbitalPeriodValue;
+};
 
-function orbitalPeriod(...args) {
-  return args;
+const returnOrbitalObject = (object) => {
+  if (typeof object.avgAlt !== 'number') {
+    throw TypeError(`Expected: avgAlt to be a number, Received: ${typeof object.avgAlt}`);
+  }
+  return {
+    name: object.name,
+    orbitalPeriod: Math.round(calculateOrbitalPeriod(object.avgAlt)),
+  };
+};
+
+function orbitalPeriod(args) {
+  return args.map(returnOrbitalObject);
 }
 
 export {
