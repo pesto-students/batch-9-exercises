b24eb3f54f32ae5b2aa8be777e973006e68ec39b
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-05 04:26:25 +0530

    feat + test: Complete object-values

--- object-values/objectValues.js
+++ object-values/objectValues.js
@@ -1,6 +1,11 @@
+const strictTypeCheck = value => Object.prototype.toString.call(value)
+  .slice(8, -1);
 
-function objectValues(...args) {
-  return args;
+function objectValues(object) {
+  if (strictTypeCheck(object) !== 'Object') {
+    throw TypeError(`Expected: object argument, Received: ${strictTypeCheck(object)}`);
+  }
+  return Object.values(object);
 }
 
 export {
