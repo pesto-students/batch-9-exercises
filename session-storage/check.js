e08ff44624c7b71956d4876266398bea148a3f24
Sumit Nihalani <nihalanisumit@gmail.com>
2019-08-06 05:04:04 +0530

    solve session storage

--- session-storage/check.js
+++ session-storage/check.js
@@ -5,7 +5,15 @@ window.setInterval(() => {
   if (value === '' || value === null) {
     value = sessionStorage.getItem('world');
   } else {
-    document.getElementsByClassName('status')[0].innerText = 'Value add successful';
+    document.getElementsByClassName('status')[0].innerText =
+      'Value add successful';
   }
 }, 200);
 
+function setSessionStorage(event) {
+  event.preventDefault();
+  const key = document.getElementById('key').value;
+  const val = document.getElementById('value').value;
+  console.log(key + val);
+  sessionStorage.setItem(key, val);
+}
