d0a0befd9160a40cbb29686dd8af66e710837e95
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-05 10:47:55 +0530

    feat + test: Complete turth-check

--- truth-check/truthCheck.js
+++ truth-check/truthCheck.js
@@ -1,6 +1,9 @@
-
-function truthCheck(...args) {
-  return args;
+function truthCheck(collection, predicate) {
+  if (typeof predicate !== 'string') {
+    throw TypeError(`Expected: Second argument of type string, Received: ${typeof predicate}`);
+  }
+  const truthy = collection.every(member => member[predicate]);
+  return truthy;
 }
 
 export {
