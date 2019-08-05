c7ada80de821a3bf384209c1a8f466c881121207
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-05 07:53:16 +0530

    remove comments

--- intermediate-exercises/src/components/01-LifeCycle.js
+++ intermediate-exercises/src/components/01-LifeCycle.js
@@ -2,14 +2,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 
-/*
-  Tasks for this exercise are in comments inside the component class code.
-  In this exercise lifecycle methods will be used to provide convenient debug
-  messages in developer's console.
-  There are more lifecycle methods available.
-  Those three presented are commonly used.
- */
-
 class LifeCycle extends React.Component {
   constructor(props) {
     super(props);
@@ -19,32 +11,19 @@ class LifeCycle extends React.Component {
     };
   }
 
-  // This code will be called when the component finishes mounting
-  // (so it is visible for a user).
   componentDidMount() {
     console.log('I\'m mounted!');
-    // Task 1: Display a message "" in developer's console when the
-    //         component finishes mounting.
-    //         Use `console.log` function for it.
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // Task 2: Display a message "Updated!" in developer's console
-    //         when the component updates.
-    //         Here you also need to use the console.log function.
-    //         Notice that in this lifecycle method you have an access
-    //         to previous values of properties and state.
     console.log('Updated!');
     console.log({ prevProps, prevState });
   }
 
   componentWillUnmount() {
-    // Task 3: Display a message "Good night, Pesto!" in developer's
-    //         console when the component unmounts.
     console.log('Good night, Pesto!');
   }
 
-  // Task 4: Create a button which triggers ComponentDidUpdate lifecycle method on click.
   render() {
     const { name } = this.state;
     return (
--- intermediate-exercises/src/components/03-FetchGithubData.js
+++ intermediate-exercises/src/components/03-FetchGithubData.js
@@ -5,34 +5,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
-/**
- * Axios is a promise based HTTP client for the browser and node.js.
- * Refer to their github page to see how to use it to make HTTP requests.
- * Axios: https://github.com/axios/axios
- *
- * It is best not to fetch data from a server in the `render` method.
- * Any change to the state of a component can cause a re-render of the
- * component. This will likely happen more often than we want.
- * Use the appropriate lifecycle method to make the axios request.
- *
- * Exercise:
- *
- *  Create a `GithubRepos` component that lists all the GitHub repos for a user.
- *  Allow the repos to be provided as a prop.
- *
- *  https://api.github.com/users/{username}/repos
- */
-/* eslint-disable react/no-unused-state */
 const GithubRepos = ({ repos }) => (
   <ul>
     {repos.map(element => (<li key={element}>{element}</li>))}
   </ul>
 );
 
-
-// Task: Open the console in the browser. There will be a warning
-// about incorrect prop type for user.
-// Define the correct prop type for the prop `repos`
 GithubRepos.propTypes = {
   repos: PropTypes.array,
 };
