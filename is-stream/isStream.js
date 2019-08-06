de6f83962cc0625e63fc7a8c7717fd036f08e601
avinash360 <avithakur86@gmail.com>
2019-08-07 01:20:09 +0530

    solve: isStream

--- is-stream/isStream.js
+++ is-stream/isStream.js
@@ -1,6 +1,25 @@
+import { Stream } from "stream";
 
-function isStream(...args) {
-  return args;
+const isStream = (stream) => {
+  return stream !== null && typeof stream === 'object' && typeof stream.pipe == 'function';
+}
+isStream.writable = (stream) => {
+  return isStream(stream) && stream.writable !== false && typeof stream._write == 'function'
+    && typeof stream._writableState === 'object';
+}
+
+isStream.readable = (stream) => {
+  return isStream(stream) && stream.readable !== false && typeof stream._read == 'function'
+    && typeof stream._readableState === 'object';
+}
+
+isStream.duplex = (stream) => {
+  return isStream.writable(stream) && isStream.readable(stream);
+}
+
+isStream.transform = (stream) => {
+  return isStream.duplex(stream) && typeof stream._transform === 'function' &&
+    typeof stream._transformState === 'object';
 }
 
 export {
