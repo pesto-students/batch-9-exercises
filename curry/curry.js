e7334cf31d8dd14e3dc3ed228367c4207e7d9f8a
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-06 18:31:39 +0530

    feat: Complete memoize

--- curry/curry.js
+++ curry/curry.js
@@ -1,6 +1,8 @@
-
-function curry(...args) {
-  return args;
+function curry(func, ...args) {
+  if (func.length === args.length) {
+    return func(args);
+  }
+  return (...moreArgs) => curry(func, ...args, ...moreArgs);
 }
 
 export {
