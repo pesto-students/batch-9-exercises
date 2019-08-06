64442b068371da5037422e4ba8526c7f4f68f543
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-07 04:47:43 +0530

    Solve bitcoin-ticker

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,6 +1,18 @@
+const http = require('https');
 
-function bitcoinTicker(...args) {
-  return args;
+function bitcoinTicker() {
+  return new Promise((resolve) => {
+    http.get('https://api.coinmarketcap.com/v2/ticker/', (response) => {
+      let result = '';
+      response.on('data', (data) => {
+        result = `${result}${data}`;
+      });
+      response.on('end', () => {
+        console.log(JSON.parse(result));
+        resolve(JSON.parse(result));
+      });
+    });
+  });
 }
 
 export {
