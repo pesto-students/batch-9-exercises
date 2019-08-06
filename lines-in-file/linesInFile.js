3a3bb6ab655f75639fbf2ad1a0e7439b164f7390
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-07 01:03:00 +0530

    [WIP] lines in file

--- lines-in-file/linesInFile.js
+++ lines-in-file/linesInFile.js
@@ -1,6 +1,19 @@
+/* eslint-disable no-unused-vars */
+const readline = require('readline');
+const fs = require('fs');
 
-function linesInFile(...args) {
-  return args;
+function linesInFile(filePath) {
+  console.log(filePath);
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(filePath),
+  });
+  let count = 0;
+  readInterface.on('line', (line) => {
+    count += 1;
+  });
+  readInterface.on('close', (line) => {
+  });
+  return count;
 }
 
 export {
