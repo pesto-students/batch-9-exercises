7f98a00ca43496767b52151439b11f6c324b8fa5
avinash360 <avithakur86@gmail.com>
2019-08-07 00:58:17 +0530

    solve: httpServer

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
 
-function filteredNames(...args) {
-  return args;
+function filteredNames(path, fileType) {
+  fs.readdir(path, function (err, files) {
+    if (err) {
+      console.log('the error is : ', err);
+    }
+    const filesByType = files.filter(file => {
+      console.log(file);
+    });
+  });
 }
 
 export {
