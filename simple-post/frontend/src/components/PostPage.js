3b793433017d9a9a0db1b95b099246711ac3ee34
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-06 09:32:52 +0530

    Add the Post page for displaying body.

--- simple-post/frontend/src/components/PostPage.js
+++ simple-post/frontend/src/components/PostPage.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PostPage = (props) => {
-  const data = JSON.stringify(props);
+const PostPage = ({
+  post, show, handleClose, loading,
+}) => {
+  const showHideClassName = show ? 'modal display-block' : 'modal display-none';
   return (
-      <div>
-          <p>{data}</p>
-        </div>
+    <div className={showHideClassName} key={post.id}>
+      <div className="modal-main" >
+        {loading ? (<h4> Loading... </h4>) :
+        (<div>
+          <h4>{post.id}</h4>
+          <div>Title: {post.title}</div>
+          <div>Body: {post.body} </div>
+         </div>
+        )}
+        <button onClick={handleClose}>close</button>
+      </div>
+
+    </div>
   );
 };
 
+PostPage.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  show: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+  loading: PropTypes.func.isRequired,
+};
+
 export default PostPage;
+
