ec6889662ee04fd539916ec7d6d400f75d8c0093
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-06 20:24:53 +0530

    Complete the path satisfies problem.

--- path-satisfies/pathSatisfies.js
+++ path-satisfies/pathSatisfies.js
@@ -1,6 +1,9 @@
 
-function pathSatisfies(...args) {
-  return args;
+function pathSatisfies(predicate, objectPath, object) {
+  const valueAtGivenPath = objectPath.reduce((valueAtCurrentPath, nextObjectPath) => {
+    return valueAtCurrentPath[nextObjectPath];
+  }, object);
+  return predicate(valueAtGivenPath);
 }
 
 export {
