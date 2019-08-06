2b52c0e7d4a62dcb12f795669cc4a070029aea03
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-06 08:04:24 +0530

    Solve sum-fibs

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,20 @@
+const getFibonacciNumbers = (limit, array) => {
+  const [secondLastNumber, lastNumber] = array.slice(array.length - 2);
+  const nextNumber = secondLastNumber + lastNumber;
+  if (nextNumber <= limit) {
+    return getFibonacciNumbers(limit, [...array, nextNumber]);
+  }
+  return array;
+};
 
-function sumFibs(...args) {
-  return args;
+const sum = numbers => numbers.reduce((accumulator, current) => accumulator + current, 0);
+
+const isOdd = number => number % 2 === 1;
+
+function sumFibs(limit) {
+  const fibonacciArray = [1, 1];
+  const oddFibonacciNumbers = getFibonacciNumbers(limit, fibonacciArray).filter(isOdd);
+  return sum(oddFibonacciNumbers);
 }
 
 export {
