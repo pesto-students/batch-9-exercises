b2b780d9e25ba053992afdf15a88d644a63751f1
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-06 09:39:19 +0530

    Solve diff array

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,9 @@
+const uniqueElements = (array1, array2) => array1.filter(item => !array2.includes(item));
 
-function diffArray(...args) {
-  return args;
+function diffArray(array1, array2) {
+  const array1Unique = uniqueElements(array1, array2);
+  const array2Unique = uniqueElements(array2, array1);
+  return [...array1Unique, ...array2Unique];
 }
 
 export {
