b9aa077c8e4c8f25c68e258dc364678f3946ee51
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-07 01:02:01 +0530

    feat: Complete filtered-names

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+const path = require('path');
 
-function filteredNames(...args) {
-  return args;
+function filteredNames(directory, fileType) {
+  const members = fs.readdirSync(directory);
+  const fileExtension = `.${fileType}`;
+  const result = members.filter(file => path.extname(file) === fileExtension);
+  return result;
 }
 
 export {
