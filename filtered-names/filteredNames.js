75b6a09bad1a6f53285a91e7d32a5c4fa7970490
Sumit Nihalani <nihalanisumit@gmail.com>
2019-08-07 04:05:56 +0530

    attempt

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,8 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 
-function filteredNames(...args) {
-  return args;
+async function filteredNames(directory, extension) {
+  const filteredNames = [];
+  await fs.readdirSync(directory, (err, files) => {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(file => {
+      const splitNames = file.split('.');
+      console.log('SN = ' + splitNames);
+      const extn = splitNames[splitNames.length - 1];
+      if (extn === extension) {
+        filteredNames.push(file);
+      }
+    });
+  });
+  console.log('filtered names ' + filteredNames);
+  return filteredNames;
 }
 
-export {
-  filteredNames,
-};
+filteredNames(path.join(__dirname, 'Directory'), 'json');
+
+// export { filteredNames };
