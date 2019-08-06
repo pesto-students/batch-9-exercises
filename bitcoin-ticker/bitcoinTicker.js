1182863b6098bcecb07836a48a4e114ebe3b1b8d
Sumit Nihalani <nihalanisumit@gmail.com>
2019-08-07 04:29:44 +0530

    solve bitcoinTicker

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,8 +1,23 @@
+const https = require('https');
+function bitcoinTicker() {
+  let data = '';
+  https
+    .get('https://api.coinmarketcap.com/v2/ticker/', response => {
+      response.on('data', chunk => {
+        data += chunk;
+      });
 
-function bitcoinTicker(...args) {
-  return args;
+      response.on('end', () => {
+        const json = JSON.parse(data);
+      });
+    })
+    .on('error', err => {
+      console.log('Error: ' + err.message);
+    });
+
+  return { data: data };
 }
 
-export {
-  bitcoinTicker,
-};
+// bitcoinTicker();
+
+export { bitcoinTicker };
