b03467292822dc4cdf7b49194432cd7039ee5edf
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-07 20:23:11 +0530

    Complete the filtered names problem

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,6 +1,12 @@
 
-function filteredNames(...args) {
-  return args;
+const fs = require('fs');
+const path = require('path');
+
+function filteredNames(directory, fileType) {
+  const directoryList = fs.readdirSync(directory, { encoding: 'utf-8' });
+  const extension = `.${fileType}`;
+  const filesWithRequiredFileType = directoryList.filter(file => path.extname(file) === extension);
+  return filesWithRequiredFileType;
 }
 
 export {
