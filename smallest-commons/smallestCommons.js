2935097cbb0513679db5a17ce319275d97629884
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-07 08:59:10 +0530

    Solve smallest-commons

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,6 +1,21 @@
+const smallestCommon = (low, high) => {
+  const scm = (l, h) => {
+    if (h % l === 0) {
+      return h;
+    }
+    return scm(l, h + high);
+  };
+  return scm(low, high);
+};
 
-function smallestCommons(...args) {
-  return args;
+function smallestCommons(args) {
+  const min = Math.min(...args);
+  const max = Math.max(...args);
+  let result = max;
+  for (let i = min; i <= max; i += 1) {
+    result = smallestCommon(i, result);
+  }
+  return result;
 }
 
 export {
