5f145f068bcafa25e2c6090d879b00728a2d8c05
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-07 09:11:03 +0530

    Adding custom error class to the parse json problem

--- parse-json/parseJson.js
+++ parse-json/parseJson.js
@@ -1,10 +1,16 @@
-
+class CustomParseJsonError extends Error {
+  constructor(message, fileName) {
+    super(message);
+    this.name = this.constructor.name;
+    this.fileName = fileName;
+  }
+}
 function parseJson(jsonString, fileName = '') {
   try {
     const requiredJsonObj = JSON.parse(jsonString);
     return requiredJsonObj;
   } catch (error) {
-    throw new Error(error, fileName);
+    throw new CustomParseJsonError(error, fileName);
   }
 }
 
