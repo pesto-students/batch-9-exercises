0e4f8602e9112f4071fc31cc1c87820c1aeffd3c
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-08 00:43:07 +0530

    Complete: Template engine problem

--- templating-engine/src/index.js
+++ templating-engine/src/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
 const app = express();
-
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 const languages = ['JavaScript', 'Haskell', 'Python'];
 
 app.get('/', (req, res) => {
-  return res.render('index', { languages }); // Use res.render() to render the ejs file instead of sending text response
+  return res.render('index', { languages });
 });
 
-app.listen(3001, () => {
+app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
