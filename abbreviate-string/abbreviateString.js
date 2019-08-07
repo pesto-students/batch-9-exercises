81e0e314d33a0c7f75dcf75cf7a09ca8557f12e1
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-07 11:20:18 +0530

    Solve abbreviate-string

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,12 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(value) {
+  if (typeof value !== 'string') {
+    throw TypeError(`Expected: String Received: ${typeof value}`);
+  }
+  const wordArray = value.split(' ');
+  const firstWord = wordArray[0];
+  const lastWord = wordArray[wordArray.length - 1];
+  return `${firstWord} ${lastWord[0].toUpperCase()}.`;
 }
 
 export {
