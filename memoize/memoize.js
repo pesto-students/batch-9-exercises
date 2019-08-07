4c1cb68dd04fb4ef897234782d32b6207858f200
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-07 10:28:03 +0530

    Solve memoize.

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,14 @@
 
 function memoize(cb) {
-  return cb;
+  const cache = {};
+  return (...args) => {
+    const key = args.toString();
+    if (key in cache) {
+      return cache[key];
+    }
+    cache[key] = cb(...args);
+    return cache[key];
+  };
 }
 
 export {
