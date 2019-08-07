2e12cbabdba1fd45ab6a8e40be96ea9841fbd719
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-07 08:16:00 +0530

    Solve chunk-array-in-groups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,9 +1,12 @@
 
-function chunkArrayInGroups(flatArray, numberOfChunks) {
-  // for (let i = 0; i < numberOfChunks; i += 1) {
-
-  // }
-  return [flatArray, numberOfChunks];
+function chunkArrayInGroups(flatArray, chunkSize) {
+  const newArray = [];
+  let index = 0;
+  while (index < flatArray.length) {
+    newArray.push(flatArray.slice(index, index + chunkSize));
+    index += chunkSize;
+  }
+  return newArray;
 }
 
 export {
