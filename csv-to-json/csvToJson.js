d18bdc5aa2cabfd7204256617adbe366a105d118
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-07 05:31:07 +0530

    Complete the csv to json problem.

--- csv-to-json/csvToJson.js
+++ csv-to-json/csvToJson.js
@@ -1,6 +1,32 @@
+const fs = require('fs');
 
-function csvToJson(...args) {
-  return args;
+function getObjectFromLine(lineString) {
+  const values = lineString.split(',');
+  return {
+    name: values[0],
+    superheroName: values[1],
+    power: values[2],
+  };
+}
+function csvToJson(filePath) {
+  return new Promise((resolve) => {
+    const myFileStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+    let dataInFile = '';
+
+    myFileStream.on('data', (currentChunk) => {
+      dataInFile = `${dataInFile}${currentChunk}`;
+    });
+
+    myFileStream.on('end', () => {
+      const linesInFileWithoutHeading = dataInFile.split('\n').slice(1);
+      const requiredObject = linesInFileWithoutHeading.reduce((accumulatedArray, currentLine) => {
+        const objectToPush = getObjectFromLine(currentLine);
+        accumulatedArray.push(objectToPush);
+        return accumulatedArray;
+      }, []);
+      resolve(requiredObject);
+    });
+  });
 }
 
 export {
