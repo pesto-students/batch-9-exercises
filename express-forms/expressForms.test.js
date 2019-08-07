dc4ba74d3c7aac31f8e9867d1579fc3bd4217cca
Navneet Sahota <navneet.sahota@outlook.com>
2019-08-08 00:45:04 +0530

    Solve expressForms

---
+++ express-forms/expressForms.test.js
@@ -0,0 +1,5 @@
+describe('Express Form', () => {
+  test('return true', () => {
+    expect(true).toBe(true);
+  });
+});
--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.redirect('/form-with-get');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.redirect('/form-with-get'));
+
+app.get('/form-with-get', (req, res) => res.render('form-with-get'));
+
+app.get('/form-with-post', (req, res) => res.render('form-with-post'));
+
+app.get('/submit-form-with-get', (req, res) => {
+  res.json(req.query);
 });
 
-app.get('/form-with-get', (req, res) => {
-  return res.render('form-with-get');
-});
-
-app.get('/form-with-post', (req, res) => {
-  return res.render('form-with-post');
+app.post('/submit-form-with-post', (req, res) => {
+  res.json(req.body);
 });
 
 app.listen(3000, () => {
