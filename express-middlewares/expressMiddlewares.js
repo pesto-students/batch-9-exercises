3704397533588ecf02c764edeaf09e2a621a137f
Sumit Nihalani <nihalanisumit@gmail.com>
2019-08-08 04:29:35 +0530

    solve expressMiddleware

--- express-middlewares/expressMiddlewares.js
+++ express-middlewares/expressMiddlewares.js
@@ -1,8 +1,30 @@
+const express = require('express');
 
-function expressMiddlewares(...args) {
-  return args;
-}
+const app = express();
+app.use((req, res, next) => {
+  res.set('middleware-header', req.path.slice(1));
+  next();
+});
 
-export {
-  expressMiddlewares,
-};
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.get('/languages', (req, res) => {
+  const languages = ['Javascript', 'Java', 'Swift', 'Node', 'C++'];
+
+  res.json({ languages });
+});
+
+app.get('/packages', (req, res) => {
+  const packages = [
+    'Express',
+    'body-parser',
+    'nodemon',
+    'create-react-app',
+    'bcryt'
+  ];
+
+  res.json({ packages });
+});
+app.listen(3000);
