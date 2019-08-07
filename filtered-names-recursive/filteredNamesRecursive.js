112a78bc7598a151d2f7a90a1bb3746586944601
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-07 20:45:17 +0530

    Complete the filtered names recursive

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,6 +1,24 @@
 
-function filteredNamesRecursive(...args) {
-  return args;
+const fs = require('fs');
+const path = require('path');
+
+function filteredNamesRecursive(directory, fileType) {
+  const directoryList = fs.readdirSync(directory, { encoding: 'utf-8' });
+  const extension = `.${fileType}`;
+  const filesWithRequiredFileType = directoryList.reduce((currentFileList, fileOrFolder) => {
+    const fileOrFolderPath = path.join(directory, fileOrFolder);
+    const fileStats = fs.statSync(fileOrFolderPath);
+    if (fileStats.isDirectory()) {
+      const filesInDirectory = filteredNamesRecursive(fileOrFolderPath, fileType);
+      if (filesInDirectory) {
+        currentFileList = [...currentFileList, ...filesInDirectory];
+      }
+    } else if (path.extname(fileOrFolder) === extension) {
+      currentFileList.push(fileOrFolder);
+    }
+    return currentFileList;
+  }, []);
+  return filesWithRequiredFileType;
 }
 
 export {
