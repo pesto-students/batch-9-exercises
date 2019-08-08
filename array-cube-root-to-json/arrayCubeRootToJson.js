d59ea24c0b5f13adc9d839bb39398dd3de1d571d
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-08 10:53:38 +0530

    Solve array-cube-root-to-json

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,6 +1,23 @@
+function isNumericArray(array) {
+  return array.every((element) => {
+    if (typeof element === 'string') {
+      return Number.isSafeInteger(parseInt(element, 10));
+    }
+    return typeof element === 'number';
+  });
+}
 
-function arrayCubeRootToJson(...args) {
-  return args;
+function arrayCubeRootToJson(array) {
+  if (!Array.isArray(array)) {
+    throw Error(`Expected type array, received type ${typeof array}`);
+  }
+  if (!isNumericArray(array)) {
+    throw Error('Each element of the array should be a number');
+  }
+  return array.reduce((acc, element) => ({
+    ...acc,
+    [element]: Math.cbrt(element),
+  }), {});
 }
 
 export {
