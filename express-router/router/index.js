b3a3832a06c9b9cfa4f20f82bfdc0574db4dd8dc
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-08 10:13:32 +0530

    Complete: The express router problem

--- express-router/router/index.js
+++ express-router/router/index.js
@@ -8,6 +8,13 @@ router.get('/', (req, res) => {
   res.render('index', { languages });
 });
 
+router.post('/', (req, res) => {
+  const newLanguageId = languages.length + 1;
+  const newLanguage = { name: req.body.name, id: newLanguageId };
+  languages.push(newLanguage);
+  res.redirect('/');
+});
+
 router.get('/new', (req, res) => {
   res.render('new');
 });
@@ -30,7 +37,23 @@ router.get('/:id/show', (req, res) => {
   res.render('show', { language: languageToEdit });
 });
 
-router.post('/:id/', (req, res) => {
+router.patch('/:id/', (req, res) => {
+  const languageId = parseInt(req.params.id, 10);
+  const languageIndex = languages.findIndex(language => language.id === languageId);
+  if (languageIndex < 0) {
+    return res.status(404).send('Invalid language id');
+  }
+  languages[languageIndex].name = req.body.name;
+  return res.redirect('/');
+});
 
+router.delete('/:id/', (req, res) => {
+  const languageId = parseInt(req.params.id, 10);
+  const languageIndex = languages.findIndex(language => language.id === languageId);
+  if (languageIndex < 0) {
+    return res.status(404).send('Invalid language id');
+  }
+  languages.splice(languageIndex, 1);
+  return res.redirect('/');
 });
 module.exports = router;
