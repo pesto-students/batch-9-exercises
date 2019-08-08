2cbb73757daf473efea64764faf5295e1e488ee7
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-08 16:29:45 +0530

    feat: Complete alternating-characters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,15 @@
+const checkForContinuous = (charactersArray) => {
+  const charactersToDelete = charactersArray
+    .filter((character, index) => character === charactersArray[index - 1]);
+  return charactersToDelete.length;
+};
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(stringsArray) {
+  const deletionsAray = stringsArray.map((string) => {
+    const numberOfDeletions = checkForContinuous(string.split(''));
+    return numberOfDeletions;
+  });
+  return deletionsAray;
 }
 
 export {
