5be59113b2dd7f4f8778e5d0c34e0d9569945601
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-08 10:32:14 +0530

    Solve sequential-promises

--- sequential-promise/sequentialPromise.js
+++ sequential-promise/sequentialPromise.js
@@ -1,6 +1,11 @@
+/* eslint-disable arrow-body-style */
 
-function sequentialPromise(...args) {
-  return args;
+function sequentialPromise(promises) {
+  return promises.reduce((promise, next) => {
+    return promise.then((res) => {
+      return next(res);
+    });
+  }, Promise.resolve());
 }
 
 export {
