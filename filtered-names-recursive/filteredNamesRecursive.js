70fd00b85049c29b59f83929eb68e66712817f38
Sumit Nihalani <nihalanisumit@gmail.com>
2019-08-08 09:31:02 +0530

    solve filteredNamesRecursive

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 
-function filteredNamesRecursive(...args) {
-  return args;
+function filteredNamesRecursive(directory, extension) {
+  const files = fs.readdirSync(directory);
+  return files.reduce((currentFileList, file) => {
+    const filePath = path.join(directory, file);
+    const fileStats = fs.statSync(filePath);
+    if (fileStats.isDirectory()) {
+      const filesInDirectory = filteredNamesRecursive(filePath, extension);
+      if (filesInDirectory) {
+        currentFileList = [...currentFileList, ...filesInDirectory];
+      }
+    } else if (path.extname(file) === `.${extension}`) {
+      currentFileList.push(file);
+    }
+    return currentFileList;
+  }, []);
 }
 
-export {
-  filteredNamesRecursive,
-};
+export { filteredNamesRecursive };
--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -2,22 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 function filteredNames(directory, extension) {
-  console.log(extension);
   const filteredFiles = [];
-
-  //   });
   const files = fs.readdirSync(directory);
-  // console.log(files);
   files.forEach(file => {
-    // console.log(path.extname(file));
     if (path.extname(file) === '.' + extension) {
       filteredFiles.push(file);
-      // console.log(file);
     }
   });
   return filteredFiles;
 }
-
-// console.log(filteredNames(path.join(__dirname, 'Directory'), 'json'));
 
 export { filteredNames };
