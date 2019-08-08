d5dfcff802db75b66ce335e28649ec38abddc0a6
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-08 10:27:25 +0530

    WIP: Partially solve the node event problem

--- node-event/nodeEvent.js
+++ node-event/nodeEvent.js
@@ -1,6 +1,17 @@
+const EventEmitter = require('events');
 
-function nodeEvent(...args) {
-  return args;
+class GreetEmitter extends EventEmitter {}
+
+const greetEmitter = new GreetEmitter();
+greetEmitter.on('greeting', (message) => {
+  const isStringMessage = typeof message === 'string';
+  if (!isStringMessage) {
+    throw new Error(`Error: Enter a string please! Received value's type: ${typeof message}`);
+  }
+  console.log(`Hello ${message}`);
+});
+function nodeEvent(message) {
+  greetEmitter.emit('greeting', message);
 }
 
 export {
