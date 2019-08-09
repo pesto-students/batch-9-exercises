1121147aaa8ec00536461ea77e1f6a9f6ef259a1
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-10 00:59:17 +0530

    WIP Setup progress of the express

--- simple-todo-tdd/server/index.js
+++ simple-todo-tdd/server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(cors());
 
