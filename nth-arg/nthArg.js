ace2b5f70f1c0d1132683d99d73567d101545a5b
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-10 22:56:10 +0530

    WIP nth argument problem.

--- nth-arg/nthArg.js
+++ nth-arg/nthArg.js
@@ -1,6 +1,14 @@
 
-function nthArg(...args) {
-  return args;
+function nthArg(argumentIndex) {
+  const initialNumberOfArgs = 1;
+  let dynamicArgs = initialNumberOfArgs;
+  if (argumentIndex >= 0) {
+    dynamicArgs += argumentIndex;
+  }
+  const args = Array(dynamicArgs).fill('');
+  const functionExecution = 'return arguments[argumentIndex];';
+  const requiredFn = new Function(...args, functionExecution);
+  return requiredFn;
 }
 
 export {
