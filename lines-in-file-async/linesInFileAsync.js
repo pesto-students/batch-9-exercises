0f1d0da23dbc9b9c41abf2f49f9dc736ca7dbc3c
sumeetrohra <sumeetrohra1@gmail.com>
2019-08-11 04:56:31 +0530

    Solve lines-in-file-async

--- lines-in-file-async/linesInFileAsync.js
+++ lines-in-file-async/linesInFileAsync.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
 
-function linesInFileAsync(...args) {
-  return args;
+function linesInFileAsync(filePath) {
+  return new Promise((resolve) => {
+    fs.readFile(filePath, { encoding: 'utf8' }, (error, fileString) => {
+      const lines = fileString.split('\n');
+      resolve(lines.length - 1);
+    });
+  });
 }
 
 export {
