6904743af581aa8ee40b16469db36ed5861cb5ea
piyushagwl007 <piyushagwl007@gmail.com>
2019-08-12 04:00:36 +0530

    Complete the modify object keys problem.

--- modify-object-keys/modifyObjectKeys.js
+++ modify-object-keys/modifyObjectKeys.js
@@ -1,6 +1,11 @@
 
-function modifyObjectKeys(...args) {
-  return args;
+function modifyObjectKeys(object, operationToPerform) {
+  const objectKeys = Object.getOwnPropertyNames(object);
+  const resultingValues = objectKeys.reduce((accumulatedObj, key) => ({
+    ...accumulatedObj,
+    [operationToPerform(key)]: object[key],
+  }), {});
+  return resultingValues;
 }
 
 export {
