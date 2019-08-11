9125ff7f977e9eaba3030f35e448877de2e80793
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-11 06:23:00 +0530

    feat: Complete round-to

--- round-to/roundTo.js
+++ round-to/roundTo.js
@@ -1,7 +1,21 @@
-
-function roundTo(...args) {
-  return args;
+function roundTo(number, decimalPlaces) {
+  const multiplicationFactor = 10 ** (decimalPlaces + 1);
+  const roundingNumber = (number * multiplicationFactor) % 10;
+  if (roundingNumber >= 4.9999999999) {
+    return roundTo.up(number, decimalPlaces);
+  }
+  return roundTo.down(number, decimalPlaces);
 }
+
+roundTo.up = (number, decimalPlaces) => {
+  const multiplicationFactor = 10 ** decimalPlaces;
+  return Math.ceil(number * multiplicationFactor) / multiplicationFactor;
+};
+
+roundTo.down = (number, decimalPlaces) => {
+  const multiplicationFactor = 10 ** decimalPlaces;
+  return Math.floor(number * multiplicationFactor) / multiplicationFactor;
+};
 
 export {
   roundTo,
--- time-was/timeWas.js
+++ time-was/timeWas.js
@@ -1,6 +1,15 @@
 
 function timeWas(...args) {
-  return args;
+  const NOW = 1516816290;
+  if (args.length === 1) {
+    let timeDifference = (NOW - args) / 1000;
+    if (timeDifference === 0) {
+      timeDifference = 'just';
+    }
+    return `${timeDifference} seconds ago`;
+  }
+  const value = (args[1] - args[0]) / 1000;
+  return `${value} seconds ago`;
 }
 
 export {
