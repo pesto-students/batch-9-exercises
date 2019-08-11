0b74f7a3a1a657d00ca9d9267db1eab5c5d4cd99
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-11 16:13:40 +0530

    feat + test: Complete time-was

--- time-was/timeWas.js
+++ time-was/timeWas.js
@@ -1,15 +1,36 @@
+const timeDifferenceString = (timeDifference) => {
+  const SECONDS = 1000;
+  const MINUTES = SECONDS * 60;
+  const HOURS = MINUTES * 60;
+  const DAYS = HOURS * 24;
+  const WEEKS = DAYS * 7;
+  const MONTHS = DAYS * 30;
+  const YEARS = MONTHS * 12;
 
-function timeWas(...args) {
-  const NOW = 1516816290;
-  if (args.length === 1) {
-    let timeDifference = (NOW - args) / 1000;
-    if (timeDifference === 0) {
-      timeDifference = 'just';
-    }
-    return `${timeDifference} seconds ago`;
+  const timeUnits = {
+    [YEARS]: 'year',
+    [MONTHS]: 'month',
+    [WEEKS]: 'week',
+    [DAYS]: 'day',
+    [HOURS]: 'hour',
+    [MINUTES]: 'minute',
+    [SECONDS]: 'second',
+  };
+  const probableUnits = Object.keys(timeUnits).filter(key => key <= timeDifference);
+  const timeUnitToDisplay = probableUnits[probableUnits.length - 1];
+  const unitCount = timeDifference / timeUnitToDisplay;
+  const forPlural = unitCount > 1 ? 's' : '';
+  const displayValue = `${unitCount} ${timeUnits[timeUnitToDisplay]}${forPlural} ago`;
+  return displayValue;
+};
+
+function timeWas(earlierTime, givenTime) {
+  const currentTime = givenTime || Date.now();
+  const timeDifference = (currentTime - earlierTime);
+  if (timeDifference < 1000) {
+    return 'just now';
   }
-  const value = (args[1] - args[0]) / 1000;
-  return `${value} seconds ago`;
+  return timeDifferenceString(timeDifference);
 }
 
 export {
