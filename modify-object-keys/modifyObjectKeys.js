8b0eb9972288a46f06ed866968fbc211a2699166
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-12 14:38:06 +0530

    feat: Complete modify-object-keys

--- modify-object-keys/modifyObjectKeys.js
+++ modify-object-keys/modifyObjectKeys.js
@@ -1,6 +1,10 @@
 
-function modifyObjectKeys(...args) {
-  return args;
+function modifyObjectKeys(object, modifierCallback) {
+  const keys = Object.keys(object);
+  const modifiedObject = keys.reduce((newObject, key) => ({
+    ...newObject, [modifierCallback(key)]: object[key],
+  }), {});
+  return modifiedObject;
 }
 
 export {
