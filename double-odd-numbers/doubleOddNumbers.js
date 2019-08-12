a33db4b21a199f378f5e946e5ef3ca3d8ea8f982
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-12 10:08:13 +0530

    feat: Complete double-odd-numbers

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,9 @@
-
-function doubleOddNumbers(...args) {
-  return args;
+function doubleOddNumbers(numbers) {
+  const isOdd = number => number % 2 !== 0;
+  const double = number => number * 2;
+  const oddNumbers = numbers.filter(isOdd);
+  const doubleAllOddNumbers = oddNumbers.map(double);
+  return doubleAllOddNumbers;
 }
 
 export {
