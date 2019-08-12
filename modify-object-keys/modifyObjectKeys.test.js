2a0de2e3ebbc776489503dc2f750a7882f9e0ef2
Karan Sapolia <sapoliakaran@gmail.com>
2019-08-12 14:49:36 +0530

    test: add test to modify-object-keys

--- modify-object-keys/modifyObjectKeys.test.js
+++ modify-object-keys/modifyObjectKeys.test.js
@@ -3,5 +3,16 @@ import { modifyObjectKeys } from './modifyObjectKeys';
 describe('modifyObjectKeys', () => {
   test('should modify object keys according to callback function', () => {
     expect(modifyObjectKeys({ FOO: true }, x => x.toLowerCase()).foo).toBe(true);
+    const object = {
+      ABC: true,
+      DEF: true,
+      GHI: true,
+      JKL: true,
+    };
+    const modifiedObject = modifyObjectKeys(object, x => x.toLowerCase());
+    const keys = Object.keys(modifiedObject);
+    for (const key of keys) {
+      expect(modifiedObject[key]).toBe(true);
+    }
   });
 });
